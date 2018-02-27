3c452340bdceaeaecd4121046df4432e381b85e8
Carson Full <carson_full@tsco.org>
2018-02-26 18:40:08 -0600

    Added and configured HMR.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { hmrBootstrap } from './hmr';
+
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule);
 
 if (environment.production) {
   enableProdMode();
+  document.addEventListener('DOMContentLoaded', bootstrap);
+} else if (module['hot']) {
+  hmrBootstrap(module, bootstrap);
+} else {
+  console.error('HMR is not enabled for webpack-dev-server!');
+  console.log('Are you using the --hmr flag for ng serve?');
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
