17a48bd6649cc41e88ce74c4d6d45c0a04b7dd70
Carson Full <carson_full@tsco.org>
2018-03-12 17:56:06 -0500

    CF2-116 Implement more of search input in header, including animation and autofocus. Service and results view not done.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material.module';
+import { AutofocusDirective } from './directives/autofocus.directive';
+import { HeaderSearchComponent } from './header-search/header-search.component';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -11,14 +14,17 @@ import { WelcomeComponent } from './welcome/welcome.component';
     CommonModule,
     RouterModule,
     MaterialModule,
+    FormsModule,
   ],
   exports: [
     HeaderComponent,
   ],
   declarations: [
+    AutofocusDirective,
     WelcomeComponent,
     NotFoundPageComponent,
     HeaderComponent,
+    HeaderSearchComponent,
   ],
 })
 export class CoreModule {
