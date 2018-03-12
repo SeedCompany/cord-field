5e7246e3b805b6cc2e69ae3af9db8a6d1d8f8fe8
Carson Full <carson_full@tsco.org>
2018-03-12 17:45:17 -0500

    CF2-116 Implemented shell of main header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material.module';
+import { HeaderComponent } from './header/header.component';
 import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
@@ -11,9 +12,13 @@ import { WelcomeComponent } from './welcome/welcome.component';
     RouterModule,
     MaterialModule,
   ],
+  exports: [
+    HeaderComponent,
+  ],
   declarations: [
     WelcomeComponent,
     NotFoundPageComponent,
+    HeaderComponent,
   ],
 })
 export class CoreModule {
