66418ce5365f1ddb0c0956357dd667a0d86c853a
Carson Full <carson_full@tsco.org>
2018-03-21 18:01:39 -0500

    CF2-116 Move cord icon to external file and registering it in icon registry.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,6 +18,7 @@ import { MaterialModule } from './material.module';
     BrowserModule.withServerTransition({appId: 'cord-field'}),
     BrowserAnimationsModule,
     TransferHttpCacheModule,
+    HttpClientModule,
     AppRoutingModule,
     MaterialModule,
     CoreModule,
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-header',
@@ -6,4 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon('cord', sanitizer.bypassSecurityTrustResourceUrl('assets/images/cord-icon.svg'));
+  }
 }
