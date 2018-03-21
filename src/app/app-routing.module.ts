507fa3103e8857729fb772f52037209985724e19
Carson Full <carson_full@tsco.org>
2018-03-21 11:27:39 -0500

    Added a NotFoundPage component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundPageComponent } from './core/not-found-page/not-found-page.component';
 import { WelcomeComponent } from './core/welcome/welcome.component';
 
 const routes: Routes = [
   {path: '', component: WelcomeComponent, pathMatch: 'full'},
+  {path: '**', component: NotFoundPageComponent},
 ];
 
 @NgModule({
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material.module';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
@@ -12,6 +13,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
   ],
   declarations: [
     WelcomeComponent,
+    NotFoundPageComponent,
   ],
 })
 export class CoreModule {
