f3aec4ccf375d3ab690b232f12526026959ab348
Daniel Chen <daniel@olivetech.com>
2018-03-30 01:47:21 +0000

    Merge pull request #13 in SC/cord-field from feature/cf2-103 to develop
    Squashed commit of the following:
    
    commit 96edc0fefb71a45e301c74c83a300d14ee24d45a
    Author: Daniel Chen <daniel@olivetech.com>
    Date:   Thu Mar 29 20:44:20 2018 -0500
    
        CF2-103 use a different development profile server

--- src/app/core/services/logger.service.spec.ts
+++ src/app/core/services/logger.service.spec.ts
@@ -1,6 +1,5 @@
 import { inject, TestBed } from '@angular/core/testing';
 import { GoogleAnalyticsService } from './google-analytics.service';
-
 import { LoggerService } from './logger.service';
 
 describe('LoggerService', () => {
