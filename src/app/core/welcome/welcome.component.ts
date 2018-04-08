0708dda3f0ccbe34d7374fb964fa070565b01994
Gowtham Kumar Rodda <gowtham@olivetech.net>
2018-04-08 15:48:39 +0530

    CF2-137 removes unwanted console logs

--- src/app/core/welcome/welcome.component.ts
+++ src/app/core/welcome/welcome.component.ts
@@ -1,13 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent implements OnInit {
-
-  ngOnInit() {
-    console.log('welocme');
-  }
+export class WelcomeComponent {
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../material.module';
 import { SearchComponent } from './components/search/search.component';
 import { AutofocusDirective } from './directives/autofocus.directive';
-
 
 @NgModule({
   imports: [
