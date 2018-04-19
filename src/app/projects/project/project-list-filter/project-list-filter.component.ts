c01b0a0f132d793f44c683c29d27a5a1b2192b08
Carson Full <carson_full@tsco.org>
2018-04-19 12:36:46 -0500

    CF2-149 Stubbed filter fields

--- src/app/projects/project/project-list-filter/project-list-filter.component.ts
+++ src/app/projects/project/project-list-filter/project-list-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material';
 
 @Component({
   selector: 'app-project-list-filter',
@@ -6,4 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./project-list-filter.component.scss']
 })
 export class ProjectListFilterComponent {
+
+  languages = [];
+
+  addLanguage(event: MatChipInputEvent) {
+    const {input, value} = event;
+
+    if ((value || '').trim()) {
+      this.languages.push({name: value.trim()});
+    }
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  removeLanguage(language: any) {
+    const index = this.languages.indexOf(language);
+    if (index >= 0) {
+      this.languages.splice(index, 1);
+    }
+  }
 }
