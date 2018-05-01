7cecce82fa5c2dc383bf25f805474996a9c82e35
Carson Full <carson_full@tsco.org>
2018-05-01 13:36:30 -0500

    Updated project and location model

--- src/app/core/models/location.ts
+++ src/app/core/models/location.ts
@@ -9,4 +9,25 @@ export class Location {
     id: string;
     name: string;
   };
+
+  static fromJson(json: any): Location {
+    const location = new Location();
+
+    location.id = json.id;
+    location.country = json.country || '';
+
+    const region = json.region || {};
+    location.region = {
+      id: region.id,
+      name: region.name
+    };
+
+    const area = json.area || {};
+    location.area = {
+      id: area.id,
+      name: area.name
+    };
+
+    return location;
+  }
 }
