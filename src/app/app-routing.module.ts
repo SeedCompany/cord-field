7fc3abbb71ac47c928b0203f1eb184a5e3125bc7
Carson Full <carson_full@tsco.org>
2018-05-08 16:36:01 -0500

    Disable lazy loading for projects module in a hacky way so HMR works better.
    This will help our dev time. But we'll need to address a way that works for dev and prod or a way to switch between definitions before we go live.
    This should be addressed with CF2-258

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './core/header/header.component';
 import { NotFoundPageComponent } from './core/not-found-page/not-found-page.component';
 import { AuthenticationGuard } from './core/route-guards/authentication-guard';
+import { ProjectsModule } from './projects/projects.module';
+
+// Load ProjectsModule directly instead of lazily, because it makes HMR a lot slower.
+// A more permanent fix should be determined with CF2-258
+export function loadProjects() {
+  return ProjectsModule;
+}
 
 const routes: Routes = [
   {
@@ -12,7 +19,7 @@ const routes: Routes = [
       {path: '', redirectTo: 'projects', pathMatch: 'full'},
       {path: 'languages', loadChildren: 'app/languages/languages.module#LanguagesModule'},
       {path: 'organizations', loadChildren: 'app/organizations/organizations.module#OrganizationsModule'},
-      {path: 'projects', loadChildren: 'app/projects/projects.module#ProjectsModule'},
+      {path: 'projects', loadChildren: loadProjects},
       {path: 'tasks', loadChildren: 'app/tasks/tasks.module#TasksModule'}
     ],
     canActivate: [AuthenticationGuard]
