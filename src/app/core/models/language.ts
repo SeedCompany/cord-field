5dc1a2ec057f947ef35369ee605575a92c0425d8
Carson Full <carson_full@tsco.org>
2018-05-09 11:22:37 -0500

    Update language model to handle redacted name and display name

--- src/app/core/models/language.ts
+++ src/app/core/models/language.ts
@@ -1,27 +1,34 @@
+import { REDACTED } from './util';
+
 export class Language {
 
   id: string;
+  name: string | null;
+  displayName: string;
   beginFiscalYear: number;
-  ethnologueCode: string;
-  ethnologueName: string;
-  ethnologuePopulation: number;
-  organizationPopulation: number;
-  rodNumber: number;
-  name: string;
+  ethnologueCode: string | null;
+  ethnologueName: string | null;
+  ethnologuePopulation: number | null;
+  organizationPopulation: number | null;
+  rodNumber: number | null;
 
   static fromJson(json: any): Language {
-
     const language = new Language();
 
-    language.id = json.id || '';
-    language.beginFiscalYear = json.beginFiscalYear || 0;
-    language.ethnologueCode = json.ethnologueCode || '';
-    language.ethnologueName = json.ethnologueName || '';
-    language.ethnologuePopulation = json.ethnologuePopulation || 0;
-    language.organizationPopulation = json.organizationPopulation || 0;
-    language.rodNumber = json.rodNumber || 0;
-    language.name = json.name || '';
+    language.id = json.id;
+    language.name = json.name && json.name !== REDACTED ? json.name : null;
+    language.displayName = json.displayName;
+    language.beginFiscalYear = json.beginFiscalYear;
+    language.ethnologueCode = json.ethnologueCode || null;
+    language.ethnologueName = json.ethnologueName || null;
+    language.ethnologuePopulation = json.ethnologuePopulation || null;
+    language.organizationPopulation = json.organizationPopulation || null;
+    language.rodNumber = json.rodNumber || null;
 
     return language;
+  }
+
+  get nameOrDisplayName(): string {
+    return this.name || this.displayName;
   }
 }
---
+++ src/app/core/models/util.ts
@@ -0,0 +1,1 @@
+export const REDACTED = '🙈';
