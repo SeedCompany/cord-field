05a08dc199ccde7722702073c715183b638439b7
Carson Full <carson_full@tsco.org>
2018-05-16 17:46:52 -0500

    Updated User model to match backend

--- src/app/core/models/user.ts
+++ src/app/core/models/user.ts
@@ -1,6 +1,6 @@
 import { Location } from './location';
-import { ObjectId } from './object-id';
 import { ProjectRole } from './project-role';
+import { REDACTED } from './util';
 
 
 export interface IUserRequestAccess {
@@ -13,76 +13,40 @@ export interface IUserRequestAccess {
 
 export class User {
 
-  id: ObjectId;
-  firstName: string;
-  lastName: string;
-  email: string;
-  phone: string;
-  mailingAddress?: Address;
+  id: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  displayFirstName: string;
+  displayLastName: string;
+  email: string | null;
   assignableRoles: AssignableRole[];
-  authRoles: string[];
-  imgSrc: string;
 
   static fromJson(json: any): User {
     json = json || {};
 
     const obj = new User();
 
-    obj.id = json.id;
-    obj.firstName = json.firstName;
-    obj.lastName = json.lastName;
-    obj.email = json.email;
-    obj.phone = json.phone;
-    obj.mailingAddress = Address.fromJson(json.mailingAddress);
-    obj.assignableRoles = AssignableRole.fromJsonArray(json.roles);
-    obj.authRoles = json.authRoles || [];
-    obj.imgSrc = json.imgSrc || '';
+    obj.id = json.id !== REDACTED ? json.id : null;
+    obj.firstName = json.firstName !== REDACTED ? json.firstName : null;
+    obj.displayFirstName = json.displayFirstName;
+    obj.lastName = json.lastName !== REDACTED ? json.lastName : null;
+    obj.displayLastName = json.displayLastName;
+    obj.email = json.email !== REDACTED ? json.email : null;
+    obj.assignableRoles = json.assignableRoles.map(AssignableRole.fromJson);
 
     return obj;
   }
 
-  static fromJsonArray(jsons: any[]): User[] {
-    return jsons.map(json => User.fromJson(json));
+  get publicFirstName(): string {
+    return this.firstName || this.displayFirstName;
+  }
+
+  get publicLastName(): string {
+    return this.lastName || this.displayLastName;
   }
 
   get fullName(): string {
-    return `${this.firstName || ''}${this.lastName && this.lastName !== '' ? ' ' : ''}${this.lastName || ''}`;
-  }
-
-  get created(): Date {
-    return (this.id || {} as any).timeStamp || null;
-  }
-}
-
-export class Address {
-
-  address1: string;
-  address2: string;
-  city: string;
-  country: string;
-  state: string;
-  zip: string;
-
-  static fromJson(json: any): Address {
-    json = json || {};
-
-    const obj = new Address();
-
-    obj.address1 = json.address1 || json.addressLine1 || '';
-    obj.address2 = json.address2 || json.addressLine2 || '';
-    obj.city = json.city || json.addressCity || '';
-    obj.country = json.country || '';
-    obj.state = json.state || json.addressState || '';
-    obj.zip = json.zip || json.addressZip || '';
-
-    return obj;
-  }
-
-  static fromJsonArray(jsons: any[]): Address[] {
-    return jsons.map(json => Address.fromJson(json));
-  }
-
-  constructor() {
+    return `${this.publicFirstName} ${this.publicLastName}`.trim();
   }
 }
 
@@ -91,16 +55,11 @@ export class AssignableRole {
   location: Location;
 
   static fromJson(json: any): AssignableRole {
-    json = json || {};
-
     const assignableRole = new AssignableRole();
-    assignableRole.role = json.role || null;
+
+    assignableRole.role = json.role;
     assignableRole.location = Location.fromJson(json.location);
 
     return assignableRole;
-  }
-
-  static fromJsonArray(jsons: any[]): AssignableRole[] {
-    return jsons.map(json => AssignableRole.fromJson(json));
   }
 }
