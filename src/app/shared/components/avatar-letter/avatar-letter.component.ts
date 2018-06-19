cbf59c7581fac58b35aecffd923b4ecd6a4d5c12
Carson Full <carson_full@tsco.org>
2018-06-19 18:00:40 -0500

    Revert "CF2-425 : added change detection need to be tested."
    This reverts commit 5dd38ef

--- src/app/shared/components/avatar-letter/avatar-letter.component.ts
+++ src/app/shared/components/avatar-letter/avatar-letter.component.ts
@@ -1,25 +1,12 @@
-import { ChangeDetectorRef, Component, HostBinding, Input, OnChanges } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'app-avatar-letter',
   templateUrl: './avatar-letter.component.html',
   styleUrls: ['./avatar-letter.component.scss']
 })
-export class AvatarLetterComponent implements OnChanges {
+export class AvatarLetterComponent {
   @Input() color: 'primary' | 'accent';
-
-  @HostBinding('class.mat-primary') get isPrimary() {
-    return this.color === 'primary';
-  }
-
-  @HostBinding('class.mat-accent') get isAccent() {
-    return this.color === 'accent';
-  }
-
-  constructor(private changeDetectorRef: ChangeDetectorRef) {
-  }
-
-  ngOnChanges(): void {
-    this.changeDetectorRef.detectChanges();
-  }
+  @HostBinding('class.mat-primary') get isPrimary() { return this.color === 'primary'; }
+  @HostBinding('class.mat-accent') get isAccent() { return this.color === 'accent'; }
 }
