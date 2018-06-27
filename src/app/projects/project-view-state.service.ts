c76dcf530c1e386ee89cd07b34fdc7a3cd1a5489
Carson Full <carson_full@tsco.org>
2018-06-27 10:31:00 -0500

    Abstracted ProjectViewState. Added tests for AbstractViewState. Updated ProjectViewState tests to test config and not logic covered by base class.

--- src/app/projects/project-view-state.service.ts
+++ src/app/projects/project-view-state.service.ts
@@ -1,116 +1,82 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { DateTime } from 'luxon';
 import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
-import {
-  accessDates,
-  ChangeConfig,
-  ChangeEngine,
-  Changes,
-  mapChangeList,
-  Modified,
-  returnId
-} from '../core/change-engine';
+import { AbstractViewState } from '../core/abstract-view-state';
+import { accessDates, ChangeConfig, mapChangeList, returnId } from '../core/change-engine';
 import { Language } from '../core/models/language';
 import { Partnership, PartnershipForSaveAPI } from '../core/models/partnership';
 import { Project } from '../core/models/project';
 import { TeamMember, TeamMemberForSaveAPI } from '../core/models/team-member';
 import { ProjectService } from '../core/services/project.service';
 
-export type ModifiedProject = Modified<Project>;
+export interface ModifiedProject {
+  mouStart?: DateTime;
+  mouEnd?: DateTime;
+  locationId?: string;
+  languages?: {
+    add?: string[];
+    remove?: string[];
+  };
+  partnerships?: {
+    add?: PartnershipForSaveAPI[];
+    update?: PartnershipForSaveAPI[];
+    remove?: string[];
+  };
+  team?: {
+    add?: TeamMemberForSaveAPI[];
+    update?: TeamMemberForSaveAPI[];
+    remove?: string[];
+  };
+}
+
+const config: ChangeConfig<Project> = {
+  mouStart: {
+    accessor: accessDates
+  },
+  mouEnd: {
+    accessor: accessDates
+  },
+  location: {
+    accessor: returnId,
+    toServer: returnId,
+    key: 'locationId',
+    forceRefresh: true
+  },
+  languages: {
+    accessor: returnId,
+    toServer: mapChangeList<Language, string, string>(returnId, returnId)
+  },
+  partnerships: {
+    accessor: returnId,
+    toServer: mapChangeList<Partnership, PartnershipForSaveAPI, string>(Partnership.forSaveAPI, returnId)
+  },
+  team: {
+    accessor: returnId,
+    toServer: mapChangeList<TeamMember, TeamMemberForSaveAPI, string>(TeamMember.forSaveAPI, returnId)
+  }
+};
 
 @Injectable()
-export class ProjectViewStateService {
-
-  private _project = new BehaviorSubject<Project>(Project.fromJson({}));
-
-  private submitting = new BehaviorSubject<boolean>(false);
-  private _loadError = new Subject<Error>();
-
-  private config: ChangeConfig = {
-    mouStart: {
-      accessor: accessDates
-    },
-    mouEnd: {
-      accessor: accessDates
-    },
-    location: {
-      accessor: returnId,
-      toServer: returnId,
-      key: 'locationId',
-      forceRefresh: true
-    },
-    languages: {
-      accessor: returnId,
-      toServer: mapChangeList<Language, string, string>(returnId, returnId)
-    },
-    partnerships: {
-      accessor: returnId,
-      toServer: mapChangeList<Partnership, PartnershipForSaveAPI, string>(Partnership.forSaveAPI, returnId)
-    },
-    team: {
-      accessor: returnId,
-      toServer: mapChangeList<TeamMember, TeamMemberForSaveAPI, string>(TeamMember.forSaveAPI, returnId)
-    }
-  };
-  private changeEngine: ChangeEngine;
+export class ProjectViewStateService extends AbstractViewState<Project> {
 
   constructor(private projectService: ProjectService) {
-    this.changeEngine = new ChangeEngine(this.config);
+    super(config, Project.fromJson({}));
   }
 
   get project(): Observable<Project> {
-    return this._project.asObservable();
-  }
-
-  get isDirty(): Observable<boolean> {
-    return this.changeEngine.isDirty;
-  }
-
-  get isSubmitting(): Observable<boolean> {
-    return this.submitting.asObservable();
-  }
-
-  get loadError(): Observable<Error> {
-    return this._loadError.asObservable();
+    return this.subject;
   }
 
   onNewId(id: string): void {
     this.projectService.getProject(id)
-      .subscribe(
-        this.onNewProject.bind(this),
-        (err: Error) => this._loadError.next(err)
-      );
+      .subscribe(this.onLoad);
   }
 
-  change(changes: Changes<Project>): void {
-    this.changeEngine.change(changes, this._project.value);
+  protected async onSave(project: Project, changes: ModifiedProject): Promise<void> {
+    await this.projectService.save(project.id, changes);
   }
 
-  async save(): Promise<void> {
-    this.submitting.next(true);
-    try {
-      await this.projectService.save(this._project.value.id, this.changeEngine.getModifiedForServer());
-    } finally {
-      this.submitting.next(false);
-    }
-
-    const project = this.changeEngine.getModified(this._project.value);
-    const needsRefresh = this.changeEngine.needsRefresh;
-
-    this.onNewProject(project);
-
-    if (needsRefresh) {
-      this.onNewId(project.id);
-    }
-  }
-
-  discard(): void {
-    this.onNewProject(this._project.value);
-  }
-
-  private onNewProject(project: Project): void {
-    this.changeEngine.reset();
-    this._project.next(project);
+  protected refresh(project: Project): void {
+    this.onNewId(project.id);
   }
 }
