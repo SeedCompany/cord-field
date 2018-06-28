945ddc6adbfeb5a1caab3069395f491b0327b8ed
Carson Full <carson_full@tsco.org>
2018-06-28 17:30:23 -0500

    Added UserProfile and related models

--- src/app/core/models/user.ts
+++ src/app/core/models/user.ts
@@ -1,3 +1,7 @@
+import { DateTime } from 'luxon';
+import { buildEnum } from './enum';
+import { Language } from './language';
+import { Location } from './location';
 import { Organization } from './organization';
 import { ProjectRole } from './project-role';
 import { maybeRedacted } from './util';
@@ -67,4 +71,131 @@ export class UserListItem extends User {
 export interface UsersWithTotal {
   users: UserListItem[];
   total: number;
+}
+
+export class UserProfile extends User {
+  // basic info tab
+  roles: UserRole[];
+  organizations: Organization[];
+  phone: string | null;
+  timeZone: string; // TODO what's easiest on FE?
+  unavailabilities: Unavailability[];
+
+  // about tab
+  bio: string;
+  education: Education[];
+  skills: string[]; // predefined chip list
+  customSkills: string[]; // free form chip list
+  knownLanguages: KnownLanguage[];
+
+  // Authorization
+  isSelf: boolean;
+
+  static fromJson(json: Partial<UserProfile>): UserProfile {
+    const user = Object.assign(new UserProfile(), super.fromJson(json));
+
+    user.roles = (json.roles || []).map(UserRole.fromJson);
+    user.organizations = (json.organizations || []).map(Organization.fromJson);
+    user.phone = json.phone || null;
+    user.timeZone = json.timeZone || '';
+    user.unavailabilities = (json.unavailabilities || []).map(Unavailability.fromJson);
+    user.bio = json.bio || '';
+    user.education = (json.education || []).map(Education.fromJson);
+    user.skills = json.skills || [];
+    user.customSkills = json.customSkills || [];
+    user.knownLanguages = (json.knownLanguages || []).map(KnownLanguage.fromJson);
+    user.isSelf = json.isSelf || false;
+
+    return user;
+  }
+
+  get available(): boolean {
+    return false; // TODO
+  }
+}
+
+export class UserRole {
+  role: ProjectRole;
+  locations: Location[];
+
+  static fromJson(json: Partial<UserRole>): UserRole {
+    const role = new UserRole();
+
+    role.role = json.role!;
+    role.locations = (json.locations || []).map(Location.fromJson);
+
+    return role;
+  }
+}
+
+export class KnownLanguage {
+  language: Language;
+  proficiency: LanguageProficiency;
+
+  static fromJson(json: Partial<KnownLanguage>): KnownLanguage {
+    const kl = new KnownLanguage();
+
+    kl.language = Language.fromJson(json.language || {});
+    kl.proficiency = json.proficiency || LanguageProficiency.Beginner;
+
+    return kl;
+  }
+}
+
+export enum LanguageProficiency {
+  Beginner = 'b',
+  Conversational = 'c',
+  Fluent = 'f'
+}
+export namespace LanguageProficiency {
+  export const {entries, forUI, values, trackEntryBy, trackValueBy} = buildEnum(LanguageProficiency, {
+    [LanguageProficiency.Beginner]: 'Beginner',
+    [LanguageProficiency.Conversational]: 'Conversational',
+    [LanguageProficiency.Fluent]: 'Fluent'
+  });
+}
+
+export class Education {
+  degree: Degree;
+  major: string;
+  institution: string;
+
+  static fromJson(json: Education): Education {
+    return Object.assign(new Education(), json);
+  }
+}
+
+export enum Degree {
+  Primary = 'p',
+  Secondary = 's',
+  Associates = 'a',
+  Bachelors = 'b',
+  Masters = 'm',
+  Doctorate = 'd'
+}
+export namespace Degree {
+  export const {entries, forUI, values, trackEntryBy, trackValueBy} = buildEnum(Degree, {
+    [Degree.Primary]: 'Primary',
+    [Degree.Secondary]: 'Secondary',
+    [Degree.Associates]: 'Associates',
+    [Degree.Bachelors]: 'Bachelors',
+    [Degree.Masters]: 'Masters',
+    [Degree.Doctorate]: 'Doctorate'
+  });
+}
+
+export class Unavailability {
+  description: string;
+  start: DateTime;
+  end: DateTime;
+
+  static fromJson(json: Partial<Record<keyof Unavailability, any>>): Unavailability {
+    const obj = new Unavailability();
+
+    obj.description = json.description || '';
+    obj.start = json.start ? DateTime.fromISO(json.start) : DateTime.invalid('Missing');
+    obj.end = json.end ? DateTime.fromISO(json.end) : DateTime.invalid('Missing');
+
+    return obj;
+  }
 }
