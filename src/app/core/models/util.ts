13e706a86ab593ec752dfd004939b2ad53a66eb1
Carson Full <carson_full@tsco.org>
2018-07-02 12:21:18 -0500

    Added mapEntries helper function to make it easier to map object values to something else

--- src/app/core/models/util.ts
+++ src/app/core/models/util.ts
@@ -66,6 +66,24 @@ export function filterEntries<T>(obj: T, predicate: (key: keyof T, value: T[keyo
   return filtered;
 }
 
+// Shortcut for a mapping of keys of object {T} to values {V}
+export type ObjMap<T, V> = {[key in keyof T]: V};
+
+/**
+ * A helper to map object values by their key/values via a mapper function
+ *
+ * mapEntries(obj, (key, value) => value + 1)
+ */
+export function mapEntries<T extends ObjMap<T, V>, V, U>(obj: T, mapper: (key: keyof T, value: V) => U): ObjMap<T, U> {
+  const mapped = {} as ObjMap<T, U>;
+
+  for (const [key, value] of Object.entries(obj) as Array<[keyof T, V]>) {
+    mapped[key] = mapper(key, value);
+  }
+
+  return mapped;
+}
+
 /**
  * An RXJs pipeable operator that filters form values to only ones that are valid.
  */
