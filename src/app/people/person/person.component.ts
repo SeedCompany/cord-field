e786241e2d773b03867df8bc42d633fd0501a8a5
Carson Full <carson_full@tsco.org>
2018-07-16 17:07:16 -0500

    CF2-563 Show snackbar message when loading user fails and redirect to list

--- src/app/people/person/person.component.ts
+++ src/app/people/person/person.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Subject } from 'rxjs/Subject';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SubscriptionComponent } from '../../shared/components/subscription.component';
 import { UserViewStateService } from '../user-view-state.service';
 
 @Component({
@@ -9,23 +11,29 @@ import { UserViewStateService } from '../user-view-state.service';
   styleUrls: ['./person.component.scss'],
   providers: [UserViewStateService]
 })
-export class PersonComponent implements OnInit, OnDestroy {
-  private unsubscribe = new Subject<void>();
-
+export class PersonComponent extends SubscriptionComponent implements OnInit {
   constructor(
     private userViewState: UserViewStateService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {
+    super();
   }
 
   async ngOnInit() {
     this.route.params.subscribe(params => {
       this.userViewState.next(params.id);
     });
-  }
 
-  ngOnDestroy() {
-    this.unsubscribe.next();
-    this.unsubscribe.complete();
+    this.userViewState.loadError
+      .takeUntil(this.unsubscribe)
+      .subscribe(err => {
+        const message = (err instanceof HttpErrorResponse && err.status === 404)
+          ? 'Could not find person'
+          : 'Failed to fetch person details';
+        this.snackBar.open(message, undefined, {duration: 5000});
+        this.router.navigate(['..'], {replaceUrl: true, relativeTo: this.route});
+      });
   }
 }
