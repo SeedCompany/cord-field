c31032124476a2d4e42beda6cbd48302798b131c
Carson Full <carson_full@tsco.org>
2018-07-16 17:31:04 -0500

    Update project component tabs to use relative routes, remove unsubscribe from activated route (it doesn't need it), and add unsubscribe to router events (I'm not sure but I think it does need it).

--- src/app/projects/project/project.component.ts
+++ src/app/projects/project/project.component.ts
@@ -1,10 +1,10 @@
 import { HttpErrorResponse } from '@angular/common/http';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material';
 import { ActivatedRoute, NavigationEnd, Router, UrlSegment } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
 import { Project } from '../../core/models/project';
 import { popInOut } from '../../shared/animations';
+import { SubscriptionComponent } from '../../shared/components/subscription.component';
 import { ProjectViewStateService } from '../project-view-state.service';
 
 interface TabConfig {
@@ -22,36 +22,35 @@ interface TabConfig {
     ProjectViewStateService
   ]
 })
-export class ProjectComponent implements OnInit, OnDestroy {
-  id: string;
-  project: Project;
+export class ProjectComponent extends SubscriptionComponent implements OnInit {
 
+  readonly tabs: TabConfig[] = [
+    {path: 'overview', label: 'Overview', saveFab: true},
+    {path: 'plan', label: 'Plan', saveFab: true},
+    {path: 'budget', label: 'Budget', saveFab: true},
+    {path: 'files', label: 'Files'},
+    {path: 'team', label: 'Team'},
+    {path: 'updates', label: 'Updates'}
+  ];
+
+  project: Project;
   dirty = false;
   submitting = false;
   private shouldCurrentTabShowSaveFab: boolean;
 
-  private idSub = Subscription.EMPTY;
   private snackBarRef: MatSnackBarRef<SimpleSnackBar> | null;
-
-  readonly tabs: TabConfig[] = [
-    {path: '/overview', label: 'Overview', saveFab: true},
-    {path: '/plan', label: 'Plan', saveFab: true},
-    {path: '/budget', label: 'Budget', saveFab: true},
-    {path: '/files', label: 'Files'},
-    {path: '/team', label: 'Team'},
-    {path: '/updates', label: 'Updates'}
-  ];
 
   constructor(
     private projectViewState: ProjectViewStateService,
     private route: ActivatedRoute,
     private router: Router,
     private snackBar: MatSnackBar
-  ) {}
+  ) {
+    super();
+  }
 
   ngOnInit() {
-    this.idSub = this.route.params.subscribe(params => {
-      this.id = params.id;
+    this.route.params.subscribe(params => {
       this.projectViewState.onNewId(params.id);
     });
     this.projectViewState.loadError.subscribe(err => {
@@ -59,22 +58,19 @@ export class ProjectComponent implements OnInit, OnDestroy {
         ? 'Could not find project'
         : 'Failed to fetch project details';
       this.snackBar.open(message, undefined, {duration: 5000});
-      this.router.navigateByUrl('/projects', {replaceUrl: true});
+      this.router.navigate(['..'], {replaceUrl: true, relativeTo: this.route});
     });
     this.projectViewState.project.subscribe(project => this.project = project);
     this.projectViewState.isDirty.subscribe(dirty => this.dirty = dirty);
     this.projectViewState.isSubmitting.subscribe(submitting => this.submitting = submitting);
 
     this.router.events
+      .takeUntil(this.unsubscribe)
       .filter(event => event instanceof NavigationEnd)
       .startWith({}) // for first load
       .switchMap(() => this.route.firstChild ? this.route.firstChild.snapshot.url : [])
-      .map((segment: UrlSegment) => this.tabs.find((tab) => tab.path === '/' + segment.path)!)
+      .map((segment: UrlSegment) => this.tabs.find((tab) => tab.path === segment.path)!)
       .subscribe(tab => this.shouldCurrentTabShowSaveFab = !!tab.saveFab);
-  }
-
-  ngOnDestroy() {
-    this.idSub.unsubscribe();
   }
 
   get showSaveFab() {
