4c7f856d55f7ae4eee26e350e4e315185f64ec51
Carson Full <carson_full@tsco.org>
2018-07-19 16:09:50 -0500

    Declaring `/languages`, `/organizations`, and `/tasks` as not implemented.

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { notImplementedRoute } from '../core/route-guards/not-implemented.guard';
 
 const routes: Routes = [
+  notImplementedRoute('Task views have not been implemented yet')
 ];
 
 @NgModule({
