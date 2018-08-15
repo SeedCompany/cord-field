c3caa8ed36f082e960bd8300b7508f2399387062
Carson Full <carson_full@tsco.org>
2018-08-14 22:12:32 -0500

    Update @TitleAware to handle children titles without needing callbacks on router outlet.
    This is what I wanted to do in the first place, but couldn't figure out how, and
    fixes AOT compilation where the compiler thinks that the component doesn't have the callbacks defined.

--- src/app/core/decorators/title-aware.ts
+++ src/app/core/decorators/title-aware.ts
@@ -1,11 +1,16 @@
-import { BehaviorSubject, combineLatest, EMPTY, Observable, of as observableOf } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { ViewChild } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { BehaviorSubject, combineLatest, EMPTY, Observable, of as observableOf, Unsubscribable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 type Title = string | string[] | Observable<string> | Observable<string[]>;
 
 export interface TitleProp {
   title: Title;
 }
+
+// From @angular/core/src/util/decorators
+const PROP_METADATA = '__prop__metadata__';
 
 /**
  * Component decorator to handle the current page title. The title is handled in pieces so each component on defines it's title.
@@ -32,26 +37,10 @@ export interface TitleProp {
  * It may make sense to be a transparent pass-through, like a header component,
  * or it may make sense to add a piece to the title, like for an object details component that has sub-routes.
  *
- * The router outlet events need to be passed along as well:
- * ```
- * <router-outlet
- *   (activate)="onRouterActivate($event)"
- *   (deactivate)="onRouterDeactivate($event)"
- * ></router-outlet>
- * ```
- *
- * Note: It is important that all components with router outlets do this in order for their children to be able to define their titles.
+ * Note: All parent components need to be decorated in order for their children to be able to define their titles.
  */
 export function TitleAware(title?: Title): ClassDecorator {
   return function TitleAwareDecorator(target) {
-    function getTitle$(this: any) {
-      if (!this.title$) {
-        this.title$ = new BehaviorSubject<string[]>([]);
-      }
-
-      return this.title$;
-    }
-
     const orig = Object.getOwnPropertyDescriptor(target.prototype, 'title');
     if (orig && title) {
       throw new Error(
@@ -59,32 +48,69 @@ export function TitleAware(title?: Title): ClassDecorator {
       );
     }
 
+    const childTitles = new BehaviorSubject<string[]>([]);
+
+    // Define title property which is this component's title and all children titles merged together
     Object.defineProperty(target.prototype, 'title', {
       get: function (this: any) {
-        const result = orig ? orig.get!.apply(this) : title;
+        const result: Title = orig ? orig.get!.apply(this) : title;
         const title$ = (result instanceof Observable ? result : observableOf(result))
           .pipe(map(t => Array.isArray(t) ? t : [t]));
 
-        return combineLatest(getTitle$.call(this) as Observable<string[]>, title$)
+        return combineLatest(childTitles, title$)
           .pipe(map(([list, titles]) => list.concat(titles)));
       }
     });
 
-    target.prototype.onRouterActivate = function (component: Partial<TitleProp>) {
-      this.titleSub = observeComponentTitle(component)
-        .subscribe(titles => getTitle$.apply(this).next(titles));
-    };
+    // Add RouterOutlet as a view child
+    // See PropDecorator function in @angular/core/src/util/decorators
+    const metaProps = target.hasOwnProperty(PROP_METADATA)
+      ? (target as any)[PROP_METADATA]
+      : Object.defineProperty(target, PROP_METADATA, {value: {}})[PROP_METADATA];
+    metaProps.__routerOutlet = new ViewChild(RouterOutlet);
 
-    target.prototype.onRouterDeactivate = function () {
-      getTitle$.apply(this).next(''); // Remove piece when deactivating.
-      if (this.titleSub) {
-        this.titleSub.unsubscribe();
+    // Listen to router outlet for child component and apply child titles to our subject here
+    let activateSub: Unsubscribable;
+    let deactivateSub: Unsubscribable;
+    let childTitleSub: Unsubscribable;
+    const origAfterViewInit = target.prototype.ngAfterViewInit;
+    target.prototype.ngAfterViewInit = function () {
+      const outlet: RouterOutlet | undefined = this.__routerOutlet;
+      if (outlet) {
+        activateSub = outlet.activateEvents
+          .pipe(startWith(outlet.component))
+          .subscribe((component: Partial<TitleProp>) => {
+            if (childTitleSub) { // Just in case?
+              childTitleSub.unsubscribe();
+            }
+            childTitleSub = observeComponentTitle(component)
+              .subscribe(titles => childTitles.next(titles));
+          });
+        deactivateSub = outlet.deactivateEvents
+          .subscribe(() => {
+            childTitles.next([]);
+            if (childTitleSub) {
+              childTitleSub.unsubscribe();
+            }
+          });
+      }
+
+      if (origAfterViewInit) {
+        origAfterViewInit.apply(this);
       }
     };
 
     const origDestroy = target.prototype.ngOnDestroy;
     target.prototype.ngOnDestroy = function () {
-      this.onRouterDeactivate();
+      if (activateSub) {
+        activateSub.unsubscribe();
+      }
+      if (deactivateSub) {
+        deactivateSub.unsubscribe();
+      }
+      if (childTitleSub) {
+        childTitleSub.unsubscribe();
+      }
       if (origDestroy) {
         origDestroy.apply(this);
       }
