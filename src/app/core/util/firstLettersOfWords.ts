29866cce5ddaa751c9ea78dae8088d17153fd15d
Phani kumar Vadlamani <phani@olivetech.net>
2018-10-09 19:37:19 +0530

    CF2-796 Fix initials for lowercase names

--- src/app/core/util/firstLettersOfWords.ts
+++ src/app/core/util/firstLettersOfWords.ts
@@ -1,12 +1,22 @@
 import * as XRegExp from 'xregexp';
 
-// https://regex101.com/r/63P0Hw (make sure you are viewing the latest version)
 // Using XRegExp for unicode property support
-const lettersPattern = XRegExp(
+const hasUppercaseLettersPattern = XRegExp('\\p{Lu}', 'g');
+
+// https://regex101.com/r/63P0Hw (make sure you are viewing the latest version)
+const uppercasePattern = XRegExp(
   // Match word boundary - \b fails in some cases, so also match start of string, and space before letter
   '(?:^|\\b|\\s)' +
   // Capture uppercase unicode letter or number
   '([\\p{Lu}\\p{N}])',
+  'g'
+);
+
+const lowercasePattern = XRegExp(
+  // Match word boundary - \b fails in some cases, so also match start of string, and space before letter
+  '(?:^|\\b|\\s)' +
+  // Capture lower case unicode letter or number
+  '([\\p{Ll}\\p{N}])',
   'g'
 );
 
@@ -22,6 +32,7 @@ function matchAll(str: string, pattern: RegExp, captureGroup = 1): string[] {
 }
 
 export function firstLettersOfWords(words: string, limit: number | null = 3): string {
-  const letters = matchAll(words, lettersPattern).join('');
+  const pattern = words.match(hasUppercaseLettersPattern) ? uppercasePattern : lowercasePattern;
+  const letters = matchAll(words, pattern).join('');
   return limit === Infinity || limit == null ? letters : letters.substr(0, limit);
 }
