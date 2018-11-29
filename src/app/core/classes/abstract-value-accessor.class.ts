313f2aeb7bb2a604040e99fe8060051d96eb71b9
Carson Full <carson_full@tsco.org>
2018-11-29 16:52:11 -0600

    [AbstractValueAccessor] Only expose `valueChange` as an observable and handle unsubscription on destroy.

--- src/app/core/classes/abstract-value-accessor.class.ts
+++ src/app/core/classes/abstract-value-accessor.class.ts
@@ -1,6 +1,8 @@
 import { forwardRef } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { SubscriptionComponent } from '@app/shared/components/subscription.component';
 import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 export function ValueAccessorProvider(type: any) {
   return {
@@ -11,9 +13,9 @@ export function ValueAccessorProvider(type: any) {
   };
 }
 
-export abstract class AbstractValueAccessor<T> implements ControlValueAccessor {
-  valueChange: Subject<T> = new Subject<T>();
-
+export abstract class AbstractValueAccessor<T> extends SubscriptionComponent implements ControlValueAccessor {
+  private _valueChange: Subject<T> = new Subject<T>();
+  readonly valueChange = this._valueChange.asObservable().pipe(takeUntil(this.unsubscribe));
   private _innerValue: T;
 
   /**
@@ -24,7 +26,7 @@ export abstract class AbstractValueAccessor<T> implements ControlValueAccessor {
       this._innerValue = value;
       this.onChange(value);
       this.onTouched();
-      this.valueChange.next(value);
+      this._valueChange.next(value);
     }
   }
   get value(): T {
@@ -37,7 +39,7 @@ export abstract class AbstractValueAccessor<T> implements ControlValueAccessor {
   writeValue(value: T) {
     if (this._innerValue !== value) {
       this._innerValue = value;
-      this.valueChange.next(value);
+      this._valueChange.next(value);
     }
   }
 
