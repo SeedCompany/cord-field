fd6a2d2bb962a2b4f53b1b9b015f321a40c3ba96
Carson Full <carson_full@tsco.org>
2018-12-10 19:12:12 -0600

    Added "log" rxjs operator as a shortcut to tap stream and log values with optional prefix.

--- src/app/core/util/rxjs-operators.ts
+++ src/app/core/util/rxjs-operators.ts
@@ -1,7 +1,7 @@
 import { FormGroup } from '@angular/forms';
 import { EMPTY, from, Observable, of } from 'rxjs';
 import { isInteropObservable } from 'rxjs/internal/util/isInteropObservable';
-import { filter, map } from 'rxjs/operators';
+import { filter, map, tap } from 'rxjs/operators';
 import { filterEntries } from './array-object-helpers';
 
 export type MaybeObservable<T> = Observable<T> | T;
@@ -23,3 +23,12 @@ export const filterRequired = <T>() => filter<T | null | undefined, T>(required)
 function required<T>(val: T | null | undefined): val is T {
   return val != null;
 }
+
+export const log = <T>(prefix?: string) => (source: Observable<T>): Observable<T> => source.pipe(tap(value => {
+  const args: any[] = [value];
+  if (prefix) {
+    args.unshift(prefix);
+  }
+  // tslint:disable-next-line:no-console
+  console.log(...args);
+}));
