4c32e927fb460655143cd102a70bff5a449b130c
Carson Full <carson_full@tsco.org>
2018-12-21 14:43:39 -0600

    Use public location in project sidebar

--- src/app/projects/project-overview-sidebar/project-overview-sidebar.component.ts
+++ src/app/projects/project-overview-sidebar/project-overview-sidebar.component.ts
@@ -1,11 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Project, ProjectSensitivity } from '@app/core/models/project';
 
-interface LinkConfig {
-  path: string;
-  label: string;
-}
-
 @Component({
   selector: 'app-project-overview-sidebar',
   templateUrl: './project-overview-sidebar.component.html',
@@ -15,8 +10,4 @@ export class ProjectOverviewSidebarComponent {
   @Input() project: Project | null;
 
   readonly ProjectSensitivity = ProjectSensitivity;
-
-  trackLinkBy(index: number, link: LinkConfig) {
-    return link.label;
-  }
 }
