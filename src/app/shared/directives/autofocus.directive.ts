44d4386a60a14bab3a196f9f7f639801e4cb74ad
Carson Full <carson_full@tsco.org>
2018-12-27 15:30:09 -0600

    Fix autofocus directive to wait for rendering complete before focusing element. Previously we were just guessing when it would be done and it was wrong sometimes.

--- src/app/shared/directives/autofocus.directive.ts
+++ src/app/shared/directives/autofocus.directive.ts
@@ -1,15 +1,16 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, NgZone, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
 
 @Directive({
   selector: '[autofocus]',
 })
 export class AutofocusDirective implements OnInit {
 
-  constructor(private el: ElementRef) {
+  constructor(private el: ElementRef, private zone: NgZone) {
   }
 
   ngOnInit() {
-    window.setTimeout(() => {
+    this.zone.onStable.pipe(first()).subscribe(() => {
       this.el.nativeElement.focus();
     });
   }
