8b3e69c87eed2fa1c047cc5cf998dd5f93c3c229
Phani kumar Vadlamani <phani@olivetech.net>
2018-12-29 03:32:23 +0530

    CF2-939: Checking project name availability (WIP)

--- src/app/projects/project/project-name/project-name.component.ts
+++ src/app/projects/project/project-name/project-name.component.ts
@@ -1,9 +1,12 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material';
+import { ErrorStateMatcher, MatSnackBar, MatSnackBarRef, ShowOnDirtyErrorStateMatcher, SimpleSnackBar } from '@angular/material';
+import { ProjectService } from '@app/core/services/project.service';
 import { ProjectViewStateService } from '@app/projects/project-view-state.service';
 import { SubscriptionComponent } from '@app/shared/components/subscription.component';
-import { takeUntil } from 'rxjs/operators';
+import { from as observableFrom } from 'rxjs';
+import { catchError, debounceTime, distinctUntilChanged, filter, map, switchMap, takeUntil, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-project-name',
@@ -19,10 +22,12 @@ export class ProjectNameComponent extends SubscriptionComponent implements OnIni
   editingTitle = false;
   private original: string;
   private openedWithEnter = false;
+  private snackBarRef?: MatSnackBarRef<SimpleSnackBar>;
 
   constructor(
     private projectViewState: ProjectViewStateService,
-  ) {
+    private projectService: ProjectService,
+    private snackBar: MatSnackBar) {
     super();
   }
 
@@ -36,17 +41,43 @@ export class ProjectNameComponent extends SubscriptionComponent implements OnIni
         this.name.reset(project.name, { emitEvent: false });
       });
 
-    this.name.valueChanges
+    this.name
+      .valueChanges
       .pipe(
+        map(name => name.trim()),
+        filter(name => name.length > 1),
+        debounceTime(500),
+        distinctUntilChanged(),
+        filter(() => !this.name.hasError('required')), // Don't continue if user has already cleared the text
+        tap(() => this.name.markAsPending()),
+        switchMap(name => {
+          if (this.name.valid) {
+            this.projectViewState.change({name});
+          } else {
+            this.projectViewState.revert('name');
+          }
+          return observableFrom(this.projectService.isProjectNameTaken(name))
+            .pipe(catchError<boolean, HttpErrorResponse>(err => err));
+        }),
         takeUntil(this.unsubscribe),
       )
-      .subscribe(name => {
-        if (this.name.valid) {
-          this.projectViewState.change({ name });
-        } else {
-          this.projectViewState.revert('name');
+      .subscribe((taken: boolean | HttpErrorResponse) => {
+        if (this.name.hasError('required')) {
+          // If we are here the response has been returned after the user cleared the field
+          // Even if the name is available, we don't want to remove the required error.
+          return;
         }
+        this.name.markAsTouched(); // Be sure first error shows immediately instead of waiting for field to blur
+
+        if (taken instanceof HttpErrorResponse) {
+          this.showSnackBar('Failed to check project name availability');
+          return;
+        }
+
+        this.name.setErrors(taken ? {duplicate: true} : null);
       });
+
+
   }
 
   edit(event: MouseEvent) {
@@ -69,5 +100,11 @@ export class ProjectNameComponent extends SubscriptionComponent implements OnIni
       return;
     }
     this.done();
+  }
+
+  private showSnackBar(message: string) {
+    this.snackBarRef = this.snackBar.open(message, undefined, {
+      duration: 3000,
+    });
   }
 }
