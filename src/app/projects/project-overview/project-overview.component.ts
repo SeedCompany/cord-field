3b32a2aabeeda2eb65eae6b66aa3d56c11778692
Carson Full <carson_full@tsco.org>
2019-01-17 13:14:25 -0600

    Remove check for project languages since projects will never be internships

--- src/app/projects/project-overview/project-overview.component.ts
+++ src/app/projects/project-overview/project-overview.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { TitleAware } from '@app/core/decorators';
-import { ProjectType } from '@app/core/models/project/type';
 import { ProjectTabComponent } from '../abstract-project-tab';
 import { ProjectViewStateService } from '../project-view-state.service';
 
@@ -11,8 +10,6 @@ import { ProjectViewStateService } from '../project-view-state.service';
 })
 @TitleAware('')
 export class ProjectOverviewComponent extends ProjectTabComponent {
-
-  readonly ProjectType = ProjectType;
 
   constructor(projectViewState: ProjectViewStateService) {
     super(projectViewState);
