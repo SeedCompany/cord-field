1e1b939f0f034b6da2d856c1d29e4c9354d778ee
Carson Full <carson_full@tsco.org>
2020-04-10 13:52:50 -0500

    Setup material-ui & theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
-import React from 'react';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import React, { cloneElement, FC, ReactElement } from 'react';
+import { createTheme } from './theme';
 
-export const App = () => <div>hello world</div>;
+const theme = createTheme();
+
+/**
+ * Register all providers here in a flat list
+ * This prevents git diff churning
+ */
+const providers = [
+  <ThemeProvider theme={theme} children={<></>} />,
+  // more
+];
+
+export const App = () => (
+  <Nest elements={providers}>
+    <CssBaseline />
+    <div>hello world</div>
+  </Nest>
+);
+
+const Nest: FC<{ elements: ReactElement[] }> = ({ elements, children }) =>
+  elements.reduceRight(
+    (out, element) => cloneElement(element, {}, out),
+    <>{children}</>
+  );
---
+++ src/theme/index.ts
@@ -0,0 +1,41 @@
+import { createMuiTheme } from '@material-ui/core';
+// eslint-disable-next-line no-restricted-imports
+import createBreakpoints from '@material-ui/core/styles/createBreakpoints';
+// eslint-disable-next-line no-restricted-imports
+import createPalette from '@material-ui/core/styles/createPalette';
+// eslint-disable-next-line no-restricted-imports
+import createSpacing from '@material-ui/core/styles/createSpacing';
+
+const palette = createPalette({
+  primary: {
+    main: '#64b145',
+    contrastText: '#ffffff',
+  },
+  secondary: {
+    main: '#9dcdd0',
+  },
+});
+
+// Just so we don't have to keep digging for it
+// sm: 600
+// md: 960
+// lg: 1280
+// xl: 1920
+const breakpoints = createBreakpoints({});
+
+const spacing = createSpacing(8); // default
+
+export const createTheme = () =>
+  createMuiTheme({
+    breakpoints,
+    palette,
+    spacing,
+  });
+
+// Augment Material UI's theme (if/when needed)
+declare module '@material-ui/core/styles/createMuiTheme' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface Theme {
+    // extra things here
+  }
+}
