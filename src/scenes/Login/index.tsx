60baf6772a1c33c9fbd172f808f94cdbe2ac47be
Carson Full <carson_full@tsco.org>
2020-04-14 14:00:36 -0500

    Split login form from future api connection

--- src/scenes/Login/index.tsx
+++ src/scenes/Login/index.tsx
@@ -1,44 +1,14 @@
-import { Card, CardActions, CardContent, makeStyles } from '@material-ui/core';
 import React from 'react';
-import { Form } from 'react-final-form';
-import { EmailField, PasswordField, SubmitButton } from '../../components/form';
 import { sleep } from '../../util';
-
-const useStyles = makeStyles({
-  card: {
-    maxWidth: 400,
-    margin: 'auto',
-  },
-});
-
-interface FormValues {
-  email: string;
-  password: string;
-}
+import { LoginForm } from './LoginForm';
 
 export const Login = () => {
-  const classes = useStyles();
-
-  const submit = async (data: FormValues) => {
-    await sleep(1000);
-    alert(JSON.stringify(data, undefined, 2));
-  };
-
   return (
-    <Form<FormValues> onSubmit={submit}>
-      {({ handleSubmit }) => (
-        <form onSubmit={handleSubmit}>
-          <Card className={classes.card}>
-            <CardContent>
-              <EmailField fullWidth />
-              <PasswordField fullWidth />
-            </CardContent>
-            <CardActions>
-              <SubmitButton />
-            </CardActions>
-          </Card>
-        </form>
-      )}
-    </Form>
+    <LoginForm
+      onSubmit={async (data) => {
+        await sleep(1000);
+        alert(JSON.stringify(data, undefined, 2));
+      }}
+    />
   );
 };
