c6dd6f7fd741ec717e46ca7028f11ff16c47aae5
Carson Full <carson_full@tsco.org>
2020-04-16 11:34:58 -0500

    Copy over CalenderDate from API

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 
 if (process.env.NODE_ENV !== 'production') {
+  /* eslint-disable @typescript-eslint/no-floating-promises */
   // Add lodash to dev console
-  // eslint-disable-next-line @typescript-eslint/no-floating-promises
   import('lodash').then((_) => ((window as any)._ = _));
   // Add luxon to dev console
-  // eslint-disable-next-line @typescript-eslint/no-floating-promises
   import('luxon').then((l) => Object.assign(window as any, l));
+  import('./util/CalenderDate').then((m) => Object.assign(window as any, m));
+  /* eslint-enable @typescript-eslint/no-floating-promises */
 }
 
 const root = document.getElementById('root');
