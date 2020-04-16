5b60d03e7e5adf0c5c77478db2c4e52fc29559fc
Carson Full <carson_full@tsco.org>
2020-04-16 17:13:10 -0500

    Use knobs to define component options, update button stories

--- src/components/ErrorButton/ErrorButton.stories.tsx
+++ src/components/ErrorButton/ErrorButton.stories.tsx
@@ -1,11 +1,18 @@
 import { action } from '@storybook/addon-actions';
+import { boolean, select, text } from '@storybook/addon-knobs';
 import React from 'react';
-import { ErrorButton } from './ErrorButton';
+import { ErrorButton as EB } from './ErrorButton';
 
-export default { title: 'Components/Buttons/ErrorButton' };
+export default { title: 'Components/Buttons' };
 
-export const Base = () => (
-  <ErrorButton onClick={action('button click')}>
-    This is an Error Button
-  </ErrorButton>
+export const ErrorButton = () => (
+  <EB
+    variant={select('Variant', ['contained', 'text', 'outlined'], 'contained')}
+    size={select('Size', ['small', 'medium', 'large'], 'medium')}
+    disabled={boolean('Disabled', false)}
+    fullWidth={boolean('Full Width', false)}
+    onClick={action('click')}
+  >
+    {text('Label', `Delete`)}
+  </EB>
 );
