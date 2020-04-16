e96bd004777d5e87b299224a8fef919b36afb8d8
Carson Full <carson_full@tsco.org>
2020-04-15 20:06:38 -0500

    Inject lodash and luxon into window for dev

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+if (process.env.NODE_ENV !== 'production') {
+  // Add lodash to dev console
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  import('lodash').then((_) => ((window as any)._ = _));
+  // Add luxon to dev console
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  import('luxon').then((l) => Object.assign(window as any, l));
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
