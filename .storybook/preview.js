5775b4fdd31e913b1d3206d4c403d40677806ec5
Carson Full <carson_full@tsco.org>
2020-04-16 15:41:58 -0500

    Use app theme in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import { addDecorator } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import React, { createElement } from 'react';
+import { createTheme } from '../src/theme';
 
 addDecorator(withInfo);
+
+const theme = createTheme();
+addDecorator(story => createElement(
+  ThemeProvider,
+  { theme },
+  createElement(CssBaseline),
+  story()
+));
