5fb7981fde2869c50c8c551b4f953da7bcaacae0
Carson Full <carson_full@tsco.org>
2020-04-20 17:42:03 -0500

    Disable SubmitButton if there are fields with submit errors that have not been changed
    The special FORM_ERROR key doesn't count as it doesn't go to a field.
    We'll assume that the form _can_ be re-submitted in this case.

--- src/components/form/SubmitButton.tsx
+++ src/components/form/SubmitButton.tsx
@@ -1,3 +1,5 @@
+import { FORM_ERROR } from 'final-form';
+import { difference } from 'lodash';
 import { FC } from 'react';
 import * as React from 'react';
 import { useForm, useFormState } from 'react-final-form';
@@ -18,16 +20,28 @@ export const SubmitButton: FC<SubmitButtonProps> = ({
   ...rest
 }) => {
   const form = useForm('SubmitButton');
-  const { hasValidationErrors, submitting, touched, validating } = useFormState(
-    {
-      subscription: {
-        hasValidationErrors: true,
-        submitting: true,
-        touched: true,
-        validating: true,
-      },
-    }
-  );
+  const {
+    hasValidationErrors,
+    submitting,
+    touched,
+    validating,
+    submitErrors,
+    dirtyFieldsSinceLastSubmit,
+  } = useFormState({
+    subscription: {
+      submitErrors: true,
+      dirtyFieldsSinceLastSubmit: true,
+      hasValidationErrors: true,
+      submitting: true,
+      touched: true,
+      validating: true,
+    },
+  });
+  // Ignore FORM_ERROR since it doesn't count as it doesn't go to a field.
+  // We'll assume that the form _can_ be re-submitted with these errors.
+  // It could be a server error, connection error, etc.
+  const { [FORM_ERROR]: _, ...fieldSubmitErrors } = submitErrors ?? {};
+
   const allFieldsTouched = touched
     ? Object.values(touched).every((field) => field)
     : false;
@@ -48,7 +62,16 @@ export const SubmitButton: FC<SubmitButtonProps> = ({
       }}
       type="submit"
       disabled={
-        submitting || validating || (allFieldsTouched && hasValidationErrors)
+        submitting ||
+        validating ||
+        (allFieldsTouched && hasValidationErrors) ||
+        // disable if there are submit/server errors for specific fields
+        // and they have not been changed since last submit
+        (submitErrors &&
+          difference(
+            Object.keys(fieldSubmitErrors),
+            Object.keys(dirtyFieldsSinceLastSubmit)
+          ).length > 0)
       }
       progress={spinner && submitting}
     >
