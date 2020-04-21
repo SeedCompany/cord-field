96ca1890301a5960c8e4a489e73e10aa5ce94e75
Carson Full <carson_full@tsco.org>
2020-04-21 16:53:22 -0500

    Implement MenuItemLink

---
+++ src/components/Routing/MenuItemLink.tsx
@@ -0,0 +1,49 @@
+import { MenuItem, MenuItemProps } from '@material-ui/core';
+import React, { forwardRef, Ref } from 'react';
+import { Link, LinkProps } from 'react-router-dom';
+import { assert } from 'ts-essentials';
+import { Merge } from 'type-fest';
+
+export type MenuItemLinkProps = InternalProps | ExternalProps;
+
+type BaseProps = Omit<MenuItemProps<'a'>, 'button' | 'component' | 'href'>;
+
+interface InternalProps
+  extends Merge<BaseProps, Omit<LinkProps, 'as' | 'href'>> {
+  external?: false;
+}
+
+interface ExternalProps extends BaseProps {
+  external: true;
+  to: string;
+}
+
+/**
+ * A MenuItem linking to somewhere.
+ *
+ * Uses `Link` for internal routing and `<a>` for external routing.
+ */
+export const MenuItemLink = forwardRef<HTMLAnchorElement, MenuItemLinkProps>(
+  ({ external, to, children, ...props }, ref) => {
+    if (external) {
+      assert(typeof to === 'string');
+      return (
+        <MenuItem {...props} href={to} button ref={ref} component="a">
+          {children}
+        </MenuItem>
+      );
+    }
+
+    return (
+      <MenuItem
+        {...props}
+        to={to}
+        button
+        ref={ref as Ref<HTMLLIElement>}
+        component={Link as any}
+      >
+        {children}
+      </MenuItem>
+    );
+  }
+);
--- src/components/Routing/index.ts
+++ src/components/Routing/index.ts
@@ -1,2 +1,3 @@
 export * from './Link';
 export * from './ListItemLink';
+export * from './MenuItemLink';
