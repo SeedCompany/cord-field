391fd8cf15691f2e4ef1478a243594963c6af1ff
Carson Full <carson_full@tsco.org>
2020-04-21 20:20:06 -0500

    Use apollo cache instead of management state ourselves

--- src/components/Session/Session.tsx
+++ src/components/Session/Session.tsx
@@ -1,26 +1,26 @@
 import constate from 'constate';
-import { useEffect, useState } from 'react';
-import { LoggedInUserFragment, useSessionQuery } from '../../api';
+import {
+  LoggedInUserFragment,
+  SessionDocument,
+  SessionQuery,
+  useSessionQuery,
+} from '../../api';
 
 export type SessionUser = LoggedInUserFragment;
 
 function Session() {
-  const [userSession, setUserSession] = useState<SessionUser | null>(null);
-  const [loading, setLoading] = useState(true);
-  const { loading: loadingSession, error, data } = useSessionQuery();
+  const { loading, data, client } = useSessionQuery();
+  const setSession = (user: LoggedInUserFragment) =>
+    client.writeQuery<SessionQuery>({
+      query: SessionDocument,
+      data: {
+        session: {
+          user,
+        },
+      },
+    });
 
-  useEffect(() => {
-    if (!loadingSession || error) {
-      if (data?.session.user) {
-        setUserSession(data?.session.user);
-      }
-
-      setLoading(false);
-    }
-    setUserSession(data?.session.user || null);
-  }, [loadingSession, error, data, setUserSession, setLoading]);
-
-  return [userSession, loading, setUserSession] as const;
+  return [data?.session.user, loading, setSession] as const;
 }
 
 export const [SessionProvider, useSession] = constate(Session);
