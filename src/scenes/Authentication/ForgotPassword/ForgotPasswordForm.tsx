786874b9a2646369b9da89e96a9f1730eb234b84
Carson Full <carson_full@tsco.org>
2020-04-28 00:49:03 -0500

    Update forgot password page to match mock ups

--- src/scenes/Authentication/ForgotPassword/ForgotPasswordForm.tsx
+++ src/scenes/Authentication/ForgotPassword/ForgotPasswordForm.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from '@material-ui/core';
+import { makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 import { Form, FormProps } from 'react-final-form';
 import { UserEmailInput } from '../../../api';
@@ -7,6 +7,23 @@ import {
   SubmitButton,
   SubmitError,
 } from '../../../components/form';
+import { Link } from '../../../components/Routing';
+
+const useStyles = makeStyles(({ spacing }) => ({
+  title: {
+    marginBottom: spacing(3),
+  },
+  formError: {
+    margin: spacing(2, 0),
+  },
+  submit: {
+    marginTop: spacing(1),
+  },
+  loginLink: {
+    display: 'block',
+    marginTop: spacing(1),
+  },
+}));
 
 export type ForgotPasswordFormProps = Pick<
   FormProps<UserEmailInput>,
@@ -16,16 +33,31 @@ export type ForgotPasswordFormProps = Pick<
 export const ForgotPasswordForm = ({
   className,
   ...props
-}: ForgotPasswordFormProps) => (
-  <Form {...props}>
-    {({ handleSubmit }) => (
-      <Card component="form" onSubmit={handleSubmit} className={className}>
-        <CardContent>
-          <SubmitError />
-          <EmailField placeholder="Enter Email Address" />
-          <SubmitButton />
-        </CardContent>
-      </Card>
-    )}
-  </Form>
-);
+}: ForgotPasswordFormProps) => {
+  const classes = useStyles();
+  return (
+    <div className={className}>
+      <Typography variant="h3" align="center" className={classes.title}>
+        Reset Your Password
+      </Typography>
+      <Typography align="center">
+        Enter your email address to your account
+        <br /> to reset your password.
+      </Typography>
+      <Form {...props}>
+        {({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <SubmitError className={classes.formError} />
+            <EmailField autoFocus />
+            <SubmitButton className={classes.submit}>
+              Reset Password
+            </SubmitButton>
+          </form>
+        )}
+      </Form>
+      <Link to="/login" className={classes.loginLink}>
+        Login
+      </Link>
+    </div>
+  );
+};
