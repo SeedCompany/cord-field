89bea678a6e499272b2db3a32b22d5c47ee32fd2
aaronkchsu <aaronkchsu@gmail.com>
2020-04-30 16:43:38 -0300

    updated typograhpy story

--- src/components/MUITypography/Typography.stories.tsx
+++ src/components/MUITypography/Typography.stories.tsx
@@ -1,10 +1,18 @@
 import { createMuiTheme, ThemeProvider, Typography } from '@material-ui/core';
 import { select, text } from '@storybook/addon-knobs';
 import React from 'react';
+import { ProgressButton } from '../ProgressButton';
 
 export default { title: 'Components/Typography' };
 
-const theme = createMuiTheme();
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Arial',
+    button: {
+      fontSize: 10,
+    },
+  },
+});
 
 export const Story = () => (
   <ThemeProvider theme={theme}>
@@ -12,7 +20,7 @@ export const Story = () => (
       variant={select(
         'Variant',
         ['body1', 'caption', 'h1', 'h5', 'subtitle1'],
-        'h1'
+        'body1'
       )}
       color={select(
         'Color',
@@ -27,5 +35,8 @@ export const Story = () => (
     >
       {text('Children', 'Children')}
     </Typography>
+    <ProgressButton color="secondary" variant="contained">
+      ProgressButton
+    </ProgressButton>
   </ThemeProvider>
 );
