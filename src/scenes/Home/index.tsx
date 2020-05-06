358f73d637c2ccbbadf1dc93ad306c6850ccbd6c
Carson Full <carson_full@tsco.org>
2020-05-06 14:11:00 -0500

    Move create org connection to sidebar create menu

--- src/scenes/Home/index.tsx
+++ src/scenes/Home/index.tsx
@@ -1,22 +1,14 @@
-import { Button } from '@material-ui/core';
 import React from 'react';
-import { useDialog } from '../../components/Dialog';
 import { useSession } from '../../components/Session';
 import { useTitle } from '../../components/title';
-import { CreateOrganization } from '../Organizations/Create';
 
 export const Home = () => {
   useTitle('Home');
   const [session] = useSession();
-  const [orgDialog, createOrg] = useDialog();
 
   return (
     <div>
       <div>Welcome, {session?.displayFirstName.value ?? 'Friend'}</div>
-      <Button onClick={createOrg} color="primary" variant="contained">
-        Create Organization
-      </Button>
-      <CreateOrganization {...orgDialog} />
     </div>
   );
 };
