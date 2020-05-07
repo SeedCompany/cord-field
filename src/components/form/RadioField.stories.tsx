7c979fd5fa99b6471cb81e5aa8a88a21d1073a96
Carson Full <carson_full@tsco.org>
2020-05-06 20:07:35 -0500

    Add options to storybook example

--- src/components/form/RadioField.stories.tsx
+++ src/components/form/RadioField.stories.tsx
@@ -1,5 +1,6 @@
 import { action } from '@storybook/addon-actions';
 import { boolean, select, text } from '@storybook/addon-knobs';
+import { startCase } from 'lodash';
 import React from 'react';
 import { Form } from 'react-final-form';
 import { RadioField, RadioOption } from './RadioField';
@@ -8,28 +9,36 @@ export default {
   title: 'Components/Forms/Fields',
 };
 
-export const Radio = () => (
-  <Form onSubmit={action('onSubmit')}>
-    {({ handleSubmit }) => (
-      <form onSubmit={handleSubmit}>
-        <RadioField
-          name="radio"
-          label={text('Label', 'Radio')}
-          disabled={boolean('Disabled', false)}
-          color={select('Color', ['primary', 'secondary'], 'secondary')}
-        >
-          <RadioOption
-            label={text('Option Label', 'Radio Option')}
-            value=""
-            disabled={boolean('Disabled', false)}
-            labelPlacement={select(
-              'Label Placement',
-              ['start', 'end', 'top', 'bottom'],
-              'end'
-            )}
-          />
-        </RadioField>
-      </form>
-    )}
-  </Form>
-);
+export const Radio = () => {
+  const labelPlacement = select(
+    'Label Placement',
+    ['start', 'end', 'top', 'bottom'],
+    'end'
+  );
+  const disabled = boolean('Disabled', false);
+
+  return (
+    <Form onSubmit={action('onSubmit')}>
+      {({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <RadioField
+            name="color"
+            label={text('Label', 'Color')}
+            disabled={disabled}
+            color={select('Color', ['primary', 'secondary'], 'secondary')}
+            helperText={text('Helper Text', 'Choose carefully')}
+          >
+            {['red', 'blue', 'green'].map((color) => (
+              <RadioOption
+                label={startCase(color)}
+                value={color}
+                disabled={disabled}
+                labelPlacement={labelPlacement}
+              />
+            ))}
+          </RadioField>
+        </form>
+      )}
+    </Form>
+  );
+};
