14665021cb2338c237270c80419ff44598e2c7d6
Carson Full <carson_full@tsco.org>
2020-05-11 18:49:04 -0500

    Allow HugeIcon to be customized with classes and/or children. Add storybook examples.

--- src/components/Icons/HugeIcon.tsx
+++ src/components/Icons/HugeIcon.tsx
@@ -1,32 +1,46 @@
-import { makeStyles, SvgIconProps } from '@material-ui/core';
-import { FC } from 'react';
+import {
+  createStyles,
+  SvgIconProps,
+  Theme,
+  WithStyles,
+  withStyles,
+} from '@material-ui/core';
+import clsx from 'clsx';
+import { cloneElement } from 'react';
 import * as React from 'react';
 
-const useStyles = makeStyles(({ palette }) => ({
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 64,
-    height: 64,
-    borderRadius: 64,
-    backgroundColor: palette.grey[200],
-  },
-  icon: {
-    color: palette.info.main,
-  },
-}));
+const styles = ({ palette }: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 64,
+      height: 64,
+      borderRadius: 64,
+      backgroundColor: palette.grey[200],
+    },
+    icon: {
+      color: palette.info.main,
+    },
+  });
 
-export interface HugeIconProps {
-  icon: React.ComponentType<SvgIconProps>;
+export interface HugeIconProps extends WithStyles<typeof styles> {
+  icon?: React.ComponentType<SvgIconProps>;
+  children?: React.ReactElement<SvgIconProps>;
 }
 
-export const HugeIcon: FC<HugeIconProps> = ({ icon: Icon }) => {
-  const classes = useStyles();
+export const HugeIcon = withStyles(styles, {
+  classNamePrefix: 'HugeIcon',
+})(({ classes, icon: Icon, children }: HugeIconProps) => {
+  const renderedIcon = Icon ? (
+    <Icon fontSize="large" className={classes.icon} />
+  ) : children ? (
+    cloneElement(children, {
+      fontSize: 'large',
+      className: clsx(classes.icon, children.props.className),
+    })
+  ) : null;
 
-  return (
-    <div className={classes.container}>
-      <Icon fontSize="large" className={classes.icon} />
-    </div>
-  );
-};
+  return <div className={classes.root}>{renderedIcon}</div>;
+});
