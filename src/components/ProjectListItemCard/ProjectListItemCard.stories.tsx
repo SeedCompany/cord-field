2acb0f8f6590fc53bd5eae39d4c8cd6d60475fae
Carson Full <carson_full@tsco.org>
2020-05-12 00:26:13 -0500

    Update ProjectListItemCard
    - Text is spaced evenly if card size needs to grow
    - Engagements are centered vertically on right side
    - Estimated submission is hidden when not given
    - Status is converted to human string
    - deptId is displayed first, falling back to ID
    - Card is linked to project detail route
    - Using width from theme & pic is 1/3 of card width
    - Random pic is used. Needs to be replaced but better than pixelated icon.

--- src/components/ProjectListItemCard/ProjectListItemCard.stories.tsx
+++ src/components/ProjectListItemCard/ProjectListItemCard.stories.tsx
@@ -1,51 +1,59 @@
-import { select, text } from '@storybook/addon-knobs';
+import { Box } from '@material-ui/core';
+import { date, select, text } from '@storybook/addon-knobs';
+import { DateTime } from 'luxon';
 import React from 'react';
 import { ProjectListItemCard as PIC } from './ProjectListItemCard';
 
-export default { title: 'Components/ProjectListItemCard' };
+export default { title: 'Components' };
 
 export const ProjectListItemCard = () => (
-  <PIC
-    id={text('Id', '123')}
-    createdAt="12/12/20"
-    type={select('Type', ['Translation', 'Internship'], 'Internship')}
-    status={select(
-      'status',
-      ['InDevelopment', 'Pending', 'Active', 'Stopped', 'Finished'],
-      'Active'
-    )}
-    sensitivity={select('sensitivity', ['High', 'Low', 'Medium'], 'High')}
-    modifiedAt="12/12/20"
-    deptId={{ value: '123' }}
-    estimatedSubmission={{ value: text('Id', '12/12/20') }}
-    step={{
-      value: select(
-        'ProjectStep',
-        ['Active', 'Rejected', 'Suspended'],
+  <Box display="flex">
+    <PIC
+      id="123"
+      createdAt="12/12/20"
+      type={select('Type', ['Translation', 'Internship'], 'Internship')}
+      status={select(
+        'status',
+        ['InDevelopment', 'Pending', 'Active', 'Stopped', 'Finished'],
         'Active'
-      ),
-    }}
-    name={{ value: text('Id', 'project') }}
-    location={{
-      value: {
-        __typename: 'Country',
-        name: { value: 'Texas' },
-        region: {
-          value: {
-            name: {
-              value: 'United States',
-            },
-            zone: {
-              value: {
-                name: {
-                  value: 'Americas',
+      )}
+      sensitivity={select('sensitivity', ['High', 'Low', 'Medium'], 'High')}
+      modifiedAt="12/12/20"
+      deptId={{ value: text('deptId', '1234567') }}
+      estimatedSubmission={{
+        value: DateTime.fromMillis(date('estimatedSubmission')).toLocaleString(
+          DateTime.DATE_SHORT
+        ),
+      }}
+      step={{
+        value: select(
+          'ProjectStep',
+          ['Active', 'Rejected', 'Suspended'],
+          'Active'
+        ),
+      }}
+      name={{ value: text('name', 'Project A') }}
+      location={{
+        value: {
+          __typename: 'Country',
+          name: { value: 'Texas' },
+          region: {
+            value: {
+              name: {
+                value: 'United States',
+              },
+              zone: {
+                value: {
+                  name: {
+                    value: 'Americas',
+                  },
                 },
               },
             },
           },
         },
-      },
-    }}
-    // engagements={{ total: number('Engagements', 123) }}
-  />
+      }}
+      // engagements={{ total: number('Engagements', 123) }}
+    />
+  </Box>
 );
--- src/components/ProjectListItemCard/ProjectListItemCard.tsx
+++ src/components/ProjectListItemCard/ProjectListItemCard.tsx
@@ -1,136 +1,159 @@
 import {
-  Box,
   Card,
   CardContent,
-  CardMedia,
+  Grid,
   makeStyles,
   Typography,
+  TypographyProps,
 } from '@material-ui/core';
-import { FC } from 'react';
+import clsx from 'clsx';
+import { random } from 'lodash';
+import { FC, useState } from 'react';
 import * as React from 'react';
 import { ProjectListItemFragment } from '../../api';
+import { displayStatus } from '../../api/displayStatus';
 import { displayLocation } from '../../api/location-helper';
+import { Picture } from '../Picture';
+import { CardActionAreaLink } from '../Routing';
 import { Sensitivity } from '../Sensitivity';
 
-const useStyles = makeStyles(({ spacing }) => ({
-  card: {
-    maxWidth: '573px',
-    marginTop: spacing(1),
-  },
-  media: {
-    minHeight: '169px',
-    width: '176px',
-  },
-  cardBody: {
-    display: 'flex',
-  },
-  cardContent: {
-    display: 'flex',
-    flex: '1 1 auto',
-    padding: spacing(2, 3),
-  },
-  sensitivityLabel: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  leftContent: {
-    display: 'flex',
-    flexDirection: 'column',
-    '& > *:not(:last-child)': {
+const useStyles = makeStyles(({ breakpoints, spacing }) => {
+  const cardWidth = breakpoints.values.sm;
+  return {
+    root: {
+      width: '100%',
+      maxWidth: cardWidth,
+    },
+    card: {
+      display: 'flex',
+      alignItems: 'initial',
+    },
+    media: {
+      width: cardWidth / 3,
+      borderTopRightRadius: 0,
+      borderBottomRightRadius: 0,
+    },
+    cardContent: {
+      flex: 1,
+      padding: spacing(2, 3),
+      display: 'flex',
+      justifyContent: 'space-between',
+    },
+    leftContent: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    },
+    rightContent: {
+      marginLeft: spacing(2),
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    },
+    sensitivity: {
       marginBottom: spacing(1),
     },
-  },
-  rightContent: {
-    display: 'flex',
-    flexDirection: 'column',
-    flex: '1 1 auto',
-    alignItems: 'flex-end',
-    justifyContent: 'flex-end',
-  },
-  statusLabel: {
-    marginTop: 'auto',
-  },
-  esadDateLabel: {
-    marginTop: spacing(2),
-    '& span': {
-      color: '#3c444e',
-    },
-  },
-}));
+  };
+});
 
-export type ProjectListItemCardProps = ProjectListItemFragment;
+export type ProjectListItemCardProps = ProjectListItemFragment & {
+  className?: string;
+};
 
-export const ProjectListItemCard: FC<ProjectListItemCardProps> = ({
-  ...props
-}) => {
-  const {
-    media,
-    card,
-    cardBody,
-    cardContent,
-    leftContent,
-    rightContent,
-    statusLabel,
-    esadDateLabel,
-  } = useStyles();
+export const ProjectListItemCard: FC<ProjectListItemCardProps> = (props) => {
+  const classes = useStyles();
+  const genSrc = () => `https://picsum.photos/id/${random(1, 2000)}/300/200`;
+  const [pic, setPic] = useState(genSrc);
+  const nextPic = () => setPic(genSrc());
 
   return (
-    <Card className={card}>
-      <div className={cardBody}>
-        <CardMedia
-          className={media}
-          image="/images/favicon-32x32.png" //Should be updated in next version.
-          title="Project Image"
-        />
-        <CardContent className={cardContent}>
-          <div className={leftContent}>
-            <Typography variant="h4">{props.name?.value}</Typography>
-            <Typography color="primary" variant="body2">
-              <Box
-                component="span"
-                m={1}
-                display="inline"
-                color="text.secondary"
-              >
-                {props.id}
-              </Box>
-              {displayLocation(props.location.value)}
-            </Typography>
-            <Sensitivity value={props.sensitivity} />
-            <Typography className={statusLabel} variant="body2">
-              Status:
-              <Box
-                component="span"
-                m={1}
-                display="inline"
-                color="text.secondary"
-              >
-                {props.status}
-              </Box>
-            </Typography>
-          </div>
-          <div className={rightContent}>
-            <Typography variant="h1">{0}</Typography>
-            <Typography variant="body2" color="primary" align="right">
-              {props.type === 'Internship' ? 'Internship' : 'Language'}
-              <br />
-              Engagements
-            </Typography>
-            {props.estimatedSubmission?.value ? (
-              <Typography
-                variant="body2"
-                color="primary"
-                className={esadDateLabel}
-              >
-                <span>ESAD:</span>
-                {props.estimatedSubmission?.value}
+    <Card className={clsx(classes.root, props.className)}>
+      <CardActionAreaLink to={`/projects/${props.id}`} className={classes.card}>
+        <div className={classes.media}>
+          <Picture
+            source={pic}
+            fit="cover"
+            width={300}
+            height={200}
+            onError={nextPic}
+          />
+        </div>
+        <CardContent className={classes.cardContent}>
+          <Grid
+            container
+            direction="column"
+            justify="space-between"
+            spacing={1}
+          >
+            <Grid item>
+              <Typography variant="h4">{props.name?.value}</Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" color="textSecondary">
+                {props.deptId.value ?? props.id}
               </Typography>
-            ) : (
-              ''
-            )}
+            </Grid>
+            <Grid item>
+              <Typography variant="body2" color="primary">
+                {displayLocation(props.location.value)}
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Sensitivity
+                value={props.sensitivity}
+                className={classes.sensitivity}
+              />
+            </Grid>
+            <Grid item>
+              <KeyValProp label="Status" value={displayStatus(props.status)} />
+            </Grid>
+          </Grid>
+          <div className={classes.rightContent}>
+            <KeyValProp aria-hidden="true" />
+
+            <div>
+              <Typography variant="h1" align="right">
+                {0}
+              </Typography>
+              <Typography variant="body2" color="primary" align="right">
+                {props.type === 'Internship' ? 'Internship' : 'Language'}
+                <br />
+                Engagements
+              </Typography>
+            </div>
+
+            <KeyValProp
+              label="ESAD"
+              value={props.estimatedSubmission?.value}
+              ValueProps={{ color: 'primary' }}
+            />
           </div>
         </CardContent>
-      </div>
+      </CardActionAreaLink>
     </Card>
   );
 };
+
+const KeyValProp = ({
+  label,
+  LabelProps,
+  value,
+  ValueProps,
+  ...props
+}: {
+  label?: string;
+  LabelProps?: TypographyProps;
+  value?: string | null;
+  ValueProps?: TypographyProps;
+} & TypographyProps) => (
+  <Typography variant="body2" {...props}>
+    {label && value ? (
+      <Typography variant="inherit" {...LabelProps}>
+        {label}:&nbsp;
+      </Typography>
+    ) : null}
+    <Typography variant="inherit" color="textSecondary" {...ValueProps}>
+      {value}
+    </Typography>
+  </Typography>
+);
