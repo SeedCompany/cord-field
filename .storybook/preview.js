408e0f4123821ff91e69337372889aa1bdc743c5
Carson Full <carson_full@tsco.org>
2020-05-12 14:45:49 -0500

    Allow SessionProvider to define initial user to prevent network call.
    Set session user to null in storybook.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -4,12 +4,14 @@ import { HashRouter } from 'react-router-dom';
 import React, { createElement } from 'react';
 import { appProviders } from '../src/App';
 import { Nest } from '../src/components/Nest';
+import { SessionProvider } from '../src/components/Session';
 
 addDecorator(withInfo);
 
 const storybookProviders = [
   createElement(HashRouter),
   ...appProviders,
+  createElement(SessionProvider, { user: null }),
 ];
 
 addDecorator(story => createElement(
