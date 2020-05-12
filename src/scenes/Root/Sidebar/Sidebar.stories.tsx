32b33ca0bb4421d7f9550a2531dd043bf4d4b7e6
aaronkchsu <aaronkchsu@gmail.com>
2020-05-12 14:01:15 -0500

    Fixed sidebar story issue

--- src/scenes/Root/Sidebar/Sidebar.stories.tsx
+++ src/scenes/Root/Sidebar/Sidebar.stories.tsx
@@ -1,6 +1,14 @@
+import { SnackbarProvider } from 'notistack';
 import React from 'react';
+import { ApolloProvider } from '../../../api';
 import { Sidebar as SidebarComponent } from './Sidebar';
 
 export default { title: 'Scenes/Root' };
 
-export const Sidebar = () => <SidebarComponent />;
+export const Sidebar = () => (
+  <SnackbarProvider>
+    <ApolloProvider>
+      <SidebarComponent />
+    </ApolloProvider>
+  </SnackbarProvider>
+);
