d56aa6c89978f3aa6b13862486f889202254b1f5
Carson Full <carson_full@tsco.org>
2020-05-13 15:02:04 -0500

    Add Date & Number formatting hooks

---
+++ src/components/Formatters/useDateFormatter.tsx
@@ -0,0 +1,8 @@
+import { isString } from 'lodash';
+import { DateTime } from 'luxon';
+import { CalendarDate } from '../../util';
+
+export const useDateFormatter = () => (date: CalendarDate | string) => {
+  const dt = isString(date) ? CalendarDate.fromISO(date) : date;
+  return dt.toLocaleString(DateTime.DATE_SHORT);
+};
