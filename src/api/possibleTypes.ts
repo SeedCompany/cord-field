4acf82291f482b4e0a0fa5b2dcdfbfe9abefeff2
Richard Girges <richardgirges@gmail.com>
2020-05-13 19:36:39 -0700

    configure possibleTypes generation via codegen

--- src/api/possibleTypes.ts
+++
@@ -1,7 +0,0 @@
-import { PossibleTypesMap } from '@apollo/client';
-
-export const possibleTypes: PossibleTypesMap = {
-  Project: ['TranslationProject', 'InternshipProject'],
-  Place: ['Country', 'Region', 'Zone'],
-  Location: ['Country', 'Region', 'Zone'],
-};
