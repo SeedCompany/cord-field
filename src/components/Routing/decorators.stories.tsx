bb40c529b9a9bc911ea2bed2f3f4856954b930d2
Carson Full <carson_full@tsco.org>
2020-05-22 19:44:27 -0500

    Update AddCurrentPath to use Code component

--- src/components/Routing/decorators.stories.tsx
+++ src/components/Routing/decorators.stories.tsx
@@ -1,6 +1,7 @@
 import { Box } from '@material-ui/core';
 import React, { ReactElement } from 'react';
 import { useLocation } from 'react-router';
+import { Code } from '../Debug';
 
 export const AddCurrentPath = (fn: () => ReactElement) => {
   // render fn with provider's context
@@ -9,8 +10,8 @@ export const AddCurrentPath = (fn: () => ReactElement) => {
     return (
       <>
         {fn()}
-        <Box component="pre" mt={4}>
-          Current path: {pathname}
+        <Box mt={4} display="flex">
+          <Code>Current path: {pathname}</Code>
         </Box>
       </>
     );
