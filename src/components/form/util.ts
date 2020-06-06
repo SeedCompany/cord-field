be92adeb2bbf87f170b2904a3b1395e2faef4b1d
Carson Full <carson_full@tsco.org>
2020-06-05 20:34:15 -0500

    Update mui/pickers to alpha 8. Update DateField.
    - Handle focus state between text input & popover
    - Allow defaultValue to be passed in, parsed, and handled correctly
    - Adjust inputFormat to use the format of the current locale. i.e. flip day & month for EU
    - Show this input format as helper text while text input is focused (disableable)
    - Allow mapping of error type to error messages to be passed in
    - Show errors immediately unless it's a parsing issue
    - Default opening popover to year if there's no date selected
    - Fix default min/max range with Luxon

--- src/components/form/util.ts
+++ src/components/form/util.ts
@@ -18,9 +18,11 @@ export const showError = (meta: FieldMetaState<any>) =>
 
 export const getHelperText = (
   meta: FieldMetaState<any>,
-  helperText?: ReactNode
+  helperText?: ReactNode,
+  hasError?: boolean
 ) => {
-  const text = showError(meta) ? meta.error || meta.submitError : helperText;
+  const text =
+    hasError ?? showError(meta) ? meta.error || meta.submitError : helperText;
   // always pass a truthy value, aka ' ', so layout doesn't adjust
   // when an error is shown. This is per Material Design.
   return text || ' ';
