5943ee78c1b4732702efa40803b767de44b3b2da
Ryan Elainska <ryan@elainska.com>
2020-06-08 20:36:55 -0400

    Update styling of `UserListItemCard` to match design and add loading state

--- src/components/UserListItemCard/UserListItemCard.tsx
+++ src/components/UserListItemCard/UserListItemCard.tsx
@@ -5,6 +5,7 @@ import {
   makeStyles,
   Typography,
 } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
 import * as React from 'react';
 import { FC } from 'react';
 import { Avatar } from '../Avatar';
@@ -14,46 +15,34 @@ import { UserListItemFragment } from './UserListItem.generated';
 const useStyles = makeStyles(({ palette, spacing }) => ({
   personCard: {
     maxWidth: '247px',
-    paddingTop: spacing(1),
   },
   cardContent: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    padding: spacing(3),
   },
   personName: {
-    paddingBottom: spacing(0.5),
+    marginBottom: spacing(3),
   },
   personImage: {
     width: '86px',
     height: '86px',
-    paddingBottom: spacing(2),
-  },
-  roleName: {
-    marginBottom: spacing(1),
+    marginBottom: spacing(2),
   },
   organizationName: {
     marginBottom: spacing(0.25),
-  },
-  statusCircle: {
-    height: '8px',
-    width: '8px',
-    marginRight: spacing(1),
-    borderRadius: '50%',
-    display: 'inline-block',
   },
   cardActions: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     padding: spacing(2),
-    border: `1px solid ${palette.grey[300]}`,
+    borderTop: `1px solid ${palette.grey[300]}`,
   },
-  activeColor: {
-    backgroundColor: '#619a55',
-  },
-  notActiveColor: {
-    backgroundColor: palette.action.disabled,
+  actionsButton: {
+    lineHeight: '1.143',
+    padding: 0,
   },
 }));
 
@@ -68,7 +57,11 @@ export const UserListItemCard: FC<UserListItemCardProps> = ({ user }) => {
   const { fullName, id } = user ?? { fullName: '', id: '' };
 
   // TODO destructure from user when this data becomes queryable
-  const avatarLetters = 'images/favicon-32x32.png';
+  const fullNameParts = fullName ? fullName.split(' ') : ['Unknown'];
+  const avatarLetters = fullNameParts.reduce(
+    (initials, part) => initials.concat(part.slice(0, 1).toUpperCase()),
+    ''
+  );
   const organization = {
     name: 'Seed Company',
     role: 'Software Developer',
@@ -79,35 +72,37 @@ export const UserListItemCard: FC<UserListItemCardProps> = ({ user }) => {
     <Card className={classes.personCard}>
       <CardContent className={classes.cardContent}>
         <Avatar
+          loading={!user}
           variant="circle"
-          alt={fullName ?? ''}
-          src={avatarLetters}
           className={classes.personImage}
-        />
+        >
+          {avatarLetters}
+        </Avatar>
         <Typography
           color="textPrimary"
           variant="body2"
           className={classes.personName}
         >
-          {fullName}
+          {!user ? <Skeleton variant="text" width="10ch" /> : fullName}
         </Typography>
         <Typography
           color="primary"
           variant="caption"
           className={classes.organizationName}
         >
-          {organizationName}
+          {!user ? <Skeleton variant="text" width="10ch" /> : organizationName}
         </Typography>
-        <Typography
-          color="textSecondary"
-          variant="caption"
-          className={classes.roleName}
-        >
-          {role}
+        <Typography color="textSecondary" variant="caption">
+          {!user ? <Skeleton variant="text" width="10ch" /> : role}
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <ButtonLink color="primary" to={`/users/${id}`}>
+        <ButtonLink
+          disabled={!user}
+          color="primary"
+          to={`/users/${id}`}
+          className={classes.actionsButton}
+        >
           View Details
         </ButtonLink>
       </CardActions>
