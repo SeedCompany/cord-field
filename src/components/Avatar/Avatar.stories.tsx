5d851c4823d84e69dd394f0cb12b1b55659be4c2
Blake Krammes <blake_krammes@tsco.org>
2020-06-17 18:14:57 -0500

    Add avatar story

---
+++ src/components/Avatar/Avatar.stories.tsx
@@ -0,0 +1,43 @@
+import { makeStyles } from '@material-ui/core';
+import { CreateNewFolder } from '@material-ui/icons';
+import { select } from '@storybook/addon-knobs';
+import * as React from 'react';
+import { Avatar as A, AvatarProps } from './Avatar';
+
+export default {
+  title: 'Components/Avatar',
+};
+
+const useStyles = makeStyles(() => ({
+  color: {
+    backgroundColor: '#467F3B',
+  },
+}));
+
+const GenericAvatar = (props: AvatarProps) => (
+  <>
+    <A
+      variant={select('Variant', ['square', 'rounded', 'circle'], 'circle')}
+      {...props}
+    />
+    <br></br>
+    <br></br>
+    <a href="https://material-ui.com/api/avatar/">MUI Docs</a>
+  </>
+);
+
+export const Image = () => (
+  <GenericAvatar
+    src="https://randomuser.me/api/portraits/men/34.jpg"
+    alt="A Glassed Man"
+  />
+);
+
+export const Initials = () => {
+  const classes = useStyles();
+  return <GenericAvatar children="JS" className={classes.color} />;
+};
+
+export const Loading = () => <GenericAvatar loading={true} />;
+
+export const Icon = () => <GenericAvatar children={<CreateNewFolder />} />;
