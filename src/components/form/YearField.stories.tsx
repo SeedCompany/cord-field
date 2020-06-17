7b09378a9c987d9bde45a1f0a6266aa5960372d7
Carson Full <carson_full@tsco.org>
2020-06-16 20:19:00 -0500

    Created YearField

---
+++ src/components/form/YearField.stories.tsx
@@ -0,0 +1,27 @@
+import { action } from '@storybook/addon-actions';
+import { boolean, text } from '@storybook/addon-knobs';
+import React from 'react';
+import { Form } from 'react-final-form';
+import { FieldSpy } from './FieldSpy';
+import { YearField } from './YearField';
+
+export default { title: 'Components/Forms/Fields' };
+
+export const Year = () => (
+  <Form onSubmit={action('submit')}>
+    {({ handleSubmit }) => (
+      <form onSubmit={handleSubmit}>
+        <YearField
+          name="year"
+          label={text('Label', 'Year')}
+          autoComplete="off"
+          placeholder={text('placeholder', 'Enter Year')}
+          required={boolean('Required', true)}
+          fullWidth={boolean('Full Width', true)}
+          disabled={boolean('Disabled', false)}
+        />
+        <FieldSpy name="year" />
+      </form>
+    )}
+  </Form>
+);
