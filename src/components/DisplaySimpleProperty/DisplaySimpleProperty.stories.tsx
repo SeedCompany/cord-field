1ec60128b010e723c5bf44d14da1c2addfe18a6f
Blake Krammes <blake_krammes@tsco.org>
2020-06-18 17:46:13 -0500

    Create story for DisplaySimpleProperty component

---
+++ src/components/DisplaySimpleProperty/DisplaySimpleProperty.stories.tsx
@@ -0,0 +1,55 @@
+import { boolean, select, text } from '@storybook/addon-knobs';
+import * as React from 'react';
+import { DisplaySimpleProperty as DSP } from './DisplaySimpleProperty';
+
+export default {
+  title: 'Components/Property',
+};
+
+export const Property = () => (
+  <>
+    <DSP
+      label={text('Label', 'Proper Property')}
+      LabelProps={{
+        color: select(
+          'Label Color',
+          [
+            'initial',
+            'inherit',
+            'primary',
+            'secondary',
+            'textPrimary',
+            'textSecondary',
+            'error',
+          ],
+          'initial'
+        ),
+      }}
+      value={text('Value', 'Valued Value')}
+      ValueProps={{
+        color: select(
+          'Value Color',
+          [
+            'initial',
+            'inherit',
+            'primary',
+            'secondary',
+            'textPrimary',
+            'textSecondary',
+            'error',
+          ],
+          'textSecondary'
+        ),
+      }}
+      loading={boolean('Loading', false)}
+      loadingWidth={select(
+        'Loading Width',
+        ['5%', '10%', '15%', '20%', '25%', '30%'],
+        '10%'
+      )}
+    />
+    <br></br>
+    <br></br>
+    <a href="https://material-ui.com/api/typography/">MUI Docs</a>
+  </>
+);
