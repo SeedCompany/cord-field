0fd7db6bc3089c0542eed265e87605ea8a18f76b
Ryan Elainska <ryan@elainska.com>
2020-06-23 16:08:47 -0400

    - Build out initially required actions for upload reducer - Update `UploadContext` with reducer and basic handling of user action

--- src/components/Upload/Context/UploadContext.tsx
+++ src/components/Upload/Context/UploadContext.tsx
@@ -1,12 +1,20 @@
-import React, { createContext, FC, useContext } from 'react';
+import React, {
+  createContext,
+  FC,
+  useCallback,
+  useContext,
+  useEffect,
+  useReducer,
+} from 'react';
 import { useRequestFileUploadMutation } from '../Upload.generated';
-
-type UploadStatus = 'idle' | 'adding' | 'added' | 'uploading' | 'completed';
+import * as actions from './uploadActions';
+import { initialState } from './uploadInitialState';
+import { uploadReducer } from './uploadReducer';
+import * as Types from './uploadTypings';
 
 interface UploadContextValue {
-  addedFiles: File[];
-  submittedFiles: File[];
-  status: UploadStatus;
+  files: Types.UploadFile[];
+  addFileToUploadQueue: (file: File) => void;
 }
 
 export const UploadContext = createContext<UploadContextValue | undefined>(
@@ -22,33 +30,65 @@ export const UploadProvider: FC = ({ children }) => (
 
 export const useUpload = () => {
   const uploadContext = useContext(UploadContext);
+  const [{ files }, dispatch] = useReducer(uploadReducer, initialState);
   const [requestFileUpload] = useRequestFileUploadMutation();
 
-  function handleFileProgress(event: ProgressEvent) {
+  const uploadFile = useCallback((file: Types.UploadFile, url: string) => {
+    const payload = new FormData();
+    payload.append('file', file.file);
+    const xhr = new XMLHttpRequest();
+    xhr.upload.onprogress = handleFileProgress.bind(null, file.uploadId);
+    xhr.open('PUT', url);
+    xhr.send(payload);
+  }, []);
+
+  const handleFileAdded = useCallback(
+    async (file: Types.UploadFile) => {
+      const { data } = await requestFileUpload();
+      const { /* id, */ url } = data?.requestFileUpload ?? { id: '', url: '' };
+      uploadFile(file, url);
+    },
+    [requestFileUpload, uploadFile]
+  );
+
+  // Very simple for now. We immediately submit all files to be uploaded
+  useEffect(() => {
+    const filesNotStarted = files.filter(
+      (file) => !file.uploading && !file.completedAt
+    );
+    for (const file of filesNotStarted) {
+      handleFileAdded(file);
+    }
+  }, [files, handleFileAdded]);
+
+  function handleFileProgress(
+    id: Types.UploadFile['uploadId'],
+    event: ProgressEvent
+  ) {
     const { loaded, total } = event;
     const percentCompleted = Math.floor((loaded / total) * 1000) / 10;
     if (percentCompleted >= 100) {
       console.log('Done');
+      dispatch({
+        type: actions.FILE_UPLOAD_COMPLETED,
+        id,
+        completedAt: new Date(),
+      });
     } else {
       console.log(`Percentage completed: ${percentCompleted}%`);
+      dispatch({
+        type: actions.PERCENT_COMPLETED_UPDATED,
+        id,
+        percentCompleted,
+      });
     }
   }
 
-  function uploadFile(file: File, url: string) {
-    const payload = new FormData();
-    payload.append('file', file);
-    const xhr = new XMLHttpRequest();
-    xhr.upload.onprogress = handleFileProgress;
-    xhr.open('PUT', url);
-    xhr.send(payload);
+  function addFileToUploadQueue(file: File /* TODO: pass in callback */) {
+    dispatch({ type: actions.FILE_SUBMITTED, file });
   }
 
-  /* eslint-disable */
-  async function handleFileUpload(file: File) {
-    const { data } = await requestFileUpload();
-    const { /* id, */ url } = data?.requestFileUpload ?? { id: '', url: '' };
-    uploadFile(file, url);
-  }
-
-  return uploadContext ? { ...uploadContext } : undefined;
+  return uploadContext
+    ? { ...uploadContext, files, addFileToUploadQueue }
+    : undefined;
 };
