2ea5f3a792f85c3b0ad06836e6198c465f6369c4
Frank Ma <yurenfrankma@gmail.com>
2020-06-23 12:59:06 -0700

    feat(Error.story): toggle error variant

--- src/components/Error/Error.stories.tsx
+++ src/components/Error/Error.stories.tsx
@@ -1,6 +1,13 @@
+import { action } from '@storybook/addon-actions';
+import { select } from '@storybook/addon-knobs';
 import React from 'react';
 import { Error as ErrorComponent } from './Error';
 
 export default { title: 'Components/Error' };
 
-export const Error = () => <ErrorComponent />;
+export const Error = () => (
+  <ErrorComponent
+    navigateBack={action('go back')}
+    variant={select('Error Type', ['Error', '404'], 'Error')}
+  />
+);
