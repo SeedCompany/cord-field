d6639ff6483b4e68a3c34c92bd07bc706f22e6fc
Ryan Elainska <ryan@elainska.com>
2020-06-23 19:40:44 -0400

    - Refactor `UploadContext` to only pass `addFileToUploadQueue()` function   in its value - Rename `Upload` component to `DropZone` - Rename `Context` folder to `Reducer` and move `UploadContext` to parent   folder - Create `UploadManager` component and return as child of   `UploadContext.Provider`

---
+++ src/components/Upload/UploadManager.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { UploadState } from './Reducer';
+
+interface UploadManagerProps {
+  state: UploadState;
+}
+
+export const UploadManager: FC<UploadManagerProps> = () => {
+  return <div></div>;
+};
