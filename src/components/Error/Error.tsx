5f18380f8a2c222c0620307f94301c32481a38f7
Frank Ma <yurenfrankma@gmail.com>
2020-06-23 12:16:24 -0700

    feat(Error): finish component UI

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,3 +1,36 @@
+import { Button, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 
-export const Error = () => <div>This is the Error component</div>;
+const useStyles = makeStyles(({ spacing }) => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    '& h3': {
+      fontWeight: 'bold',
+    },
+  },
+  button: {
+    height: spacing(4),
+    width: spacing(16),
+    marginTop: spacing(2),
+  },
+}));
+
+export const Error = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <Typography>Oops, Sorry.</Typography>
+      <Typography variant="h3">ERROR</Typography>
+      <Button
+        variant="contained"
+        color="secondary"
+        classes={{ root: classes.button }}
+      >
+        Back
+      </Button>
+    </div>
+  );
+};
