d856fdeb8a810f8362dcac59f7c81db538db452a
Frank Ma <yurenfrankma@gmail.com>
2020-06-23 11:48:15 -0700

    feat(Error): Error component skeleton

---
+++ src/components/Error/Error.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Error as ErrorComponent } from './Error';
+
+export default { title: 'Components/Error' };
+
+export const Error = () => <ErrorComponent />;
---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Error = () => <div>This is the Error component</div>;
