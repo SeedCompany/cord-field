e4d6b32a26766f10e91c1872332e5ac1c59b826c
Frank Ma <yurenfrankma@gmail.com>
2020-06-29 14:00:43 -0700

    fix(InternshipEngagement.graphql): update queries

--- src/scenes/Engagement/InternshipEngagement/InternshipEngagement.generated.ts
+++ src/scenes/Engagement/InternshipEngagement/InternshipEngagement.generated.ts
@@ -4,11 +4,72 @@ import * as Types from '../../../api/schema.generated';
 
 export type InternshipEngagementDetailFragment = {
   __typename?: 'InternshipEngagement';
-} & Pick<Types.InternshipEngagement, 'id' | 'createdAt'>;
+} & Pick<Types.InternshipEngagement, 'id' | 'createdAt'> & {
+    intern: { __typename?: 'SecuredUser' } & Pick<
+      Types.SecuredUser,
+      'canRead' | 'canEdit'
+    > & {
+        value?: Types.Maybe<
+          { __typename?: 'User' } & Pick<Types.User, 'fullName'>
+        >;
+      };
+    startDate: { __typename?: 'SecuredDate' } & Pick<
+      Types.SecuredDate,
+      'value' | 'canRead' | 'canEdit'
+    >;
+    endDate: { __typename?: 'SecuredDate' } & Pick<
+      Types.SecuredDate,
+      'value' | 'canRead' | 'canEdit'
+    >;
+    completeDate: { __typename?: 'SecuredDate' } & Pick<
+      Types.SecuredDate,
+      'value' | 'canRead' | 'canEdit'
+    >;
+    disbursementCompleteDate: { __typename?: 'SecuredDate' } & Pick<
+      Types.SecuredDate,
+      'value' | 'canRead' | 'canEdit'
+    >;
+    communicationsCompleteDate: { __typename?: 'SecuredDate' } & Pick<
+      Types.SecuredDate,
+      'value' | 'canRead' | 'canEdit'
+    >;
+  };
 
 export const InternshipEngagementDetailFragmentDoc = gql`
   fragment InternshipEngagementDetail on InternshipEngagement {
     id
     createdAt
+    intern {
+      value {
+        fullName
+      }
+      canRead
+      canEdit
+    }
+    startDate {
+      value
+      canRead
+      canEdit
+    }
+    endDate {
+      value
+      canRead
+      canEdit
+    }
+    completeDate {
+      value
+      canRead
+      canEdit
+    }
+    disbursementCompleteDate {
+      value
+      canRead
+      canEdit
+    }
+    communicationsCompleteDate {
+      value
+      canRead
+      canEdit
+    }
   }
 `;
