2a29da17c792b36c8ca0e8dc0d39fc62514dca01
Frank Ma <yurenfrankma@gmail.com>
2020-06-30 11:30:40 -0700

    feat(CeremonyCard): create ceremony card component

---
+++ src/components/CeremonyCard/CeremonyCard.tsx
@@ -0,0 +1,87 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import * as React from 'react';
+import { FC } from 'react';
+import { useDateFormatter } from '../Formatters';
+import { CeremonyCardFragment } from './CeremonyCard.generated';
+
+const useStyles = makeStyles(({ palette, spacing }) => ({
+  certificationCard: {
+    flex: 1,
+  },
+  cardContent: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  actualDate: {
+    height: 98,
+    width: 226,
+    backgroundColor: palette.grey[300],
+    borderRadius: 100,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: spacing(1),
+    marginBottom: spacing(2),
+  },
+
+  cardActions: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: spacing(2),
+    borderTop: `1px solid ${palette.grey[300]}`,
+  },
+}));
+
+type CeremonyCardProps = CeremonyCardFragment & {
+  canEdit?: boolean;
+  canRead?: boolean;
+  editCeremony: () => void;
+};
+
+export const CeremonyCard: FC<CeremonyCardProps> = ({
+  estimatedDate,
+  actualDate,
+  canEdit,
+  editCeremony,
+  //TODO: implement canRead functionality
+  // canRead
+}) => {
+  const classes = useStyles();
+  const formatDate = useDateFormatter();
+
+  return (
+    <Card className={classes.certificationCard}>
+      <CardContent className={classes.cardContent}>
+        <Typography color="textPrimary" variant="body2">
+          Certification Date
+        </Typography>
+        <div className={classes.actualDate}>
+          <Typography color="primary" variant="h2">
+            {formatDate(actualDate.value)}
+          </Typography>
+        </div>
+
+        <Typography color="textPrimary" variant="body1">
+          Estimated Date
+        </Typography>
+        <Typography color="textPrimary" variant="body1">
+          {formatDate(estimatedDate.value)}
+        </Typography>
+      </CardContent>
+      <CardActions className={classes.cardActions}>
+        <Button color="primary" onClick={editCeremony} disabled={!canEdit}>
+          Edit dates
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
