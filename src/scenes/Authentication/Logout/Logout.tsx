91f697d05a9cc66839ac47fe528212db191602b9
Richard Girges <richardgirges@gmail.com>
2020-07-07 12:25:34 -0700

    reset apollo store on logout, ensuring all cached queries are cleared out

--- src/scenes/Authentication/Logout/Logout.tsx
+++ src/scenes/Authentication/Logout/Logout.tsx
@@ -1,3 +1,4 @@
+import { useApolloClient } from '@apollo/client';
 import { CircularProgress } from '@material-ui/core';
 import * as React from 'react';
 import { useEffect } from 'react';
@@ -7,15 +8,18 @@ import { useLogoutMutation } from './logout.generated';
 
 export const Logout = () => {
   const navigate = useNavigate();
+  const client = useApolloClient();
   const [logout] = useLogoutMutation();
   const [, , setCurrentUser] = useSession();
 
   useEffect(() => {
-    logout().then(() => {
-      setCurrentUser(null);
-      navigate('/login', { replace: true });
-    });
-  }, [logout, setCurrentUser, navigate]);
+    logout()
+      .then(() => client.resetStore())
+      .then(() => {
+        setCurrentUser(null);
+        navigate('/login', { replace: true });
+      });
+  }, [logout, client, setCurrentUser, navigate]);
 
   return <CircularProgress />;
 };
