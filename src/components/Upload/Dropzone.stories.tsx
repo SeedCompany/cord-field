4a07beda0c20076968365e3e7354e06996c41eee
Ryan Elainska <ryan@elainska.com>
2020-07-07 13:25:24 -0400

    - Create stories file for `UploadItem` - Update title for `Dropzone` story to place it in an "Upload" folder   with `UploadItem`

--- src/components/Upload/Dropzone.stories.tsx
+++ src/components/Upload/Dropzone.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Dropzone as DropzoneComponent } from './Dropzone';
 
-export default { title: 'Components/Dropzone' };
+export default { title: 'Components/Upload/Dropzone' };
 
 export const Dropzone = () => <DropzoneComponent />;
