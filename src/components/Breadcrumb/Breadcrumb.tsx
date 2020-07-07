8a20e8ab8a73f5793f7ff2404d29bfe50b0cc18c
Carson Full <carson_full@tsco.org>
2020-07-06 19:10:45 -0500

    Added SecuredBreadcrumb to help handle breadcrumbs for API data

--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -4,20 +4,28 @@ import { isString } from 'lodash';
 import { FC } from 'react';
 import * as React from 'react';
 import { useMatch } from 'react-router-dom';
-import { Link } from '../Routing';
+import { Link, LinkProps } from '../Routing';
 
 export interface BreadcrumbProps {
   to: string | PathPieces;
+  LinkProps?: Partial<LinkProps>;
 }
 
-export const Breadcrumb: FC<BreadcrumbProps> = ({ to, children }) => {
-  const active = useMatch(isString(to) ? to : to.pathname!);
+export const Breadcrumb: FC<BreadcrumbProps> = ({
+  to,
+  children,
+  LinkProps,
+}) => {
+  const active =
+    useMatch(isString(to) ? to : to.pathname!) ||
+    // RR doesn't think current page is active. maybe a bug?
+    to === '.';
 
   if (active) {
     return <Typography variant="h4">{children}</Typography>;
   } else {
     return (
-      <Link variant="h4" to={to}>
+      <Link variant="h4" to={to} {...LinkProps}>
         {children}
       </Link>
     );
