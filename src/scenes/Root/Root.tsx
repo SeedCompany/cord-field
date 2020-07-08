535bfd05e4eca9097469ffa062989e7d66e5fbaf
Ryan Elainska <ryan@elainska.com>
2020-07-08 13:48:32 -0400

    Fix import order error in `Root`

--- src/scenes/Root/Root.tsx
+++ src/scenes/Root/Root.tsx
@@ -6,8 +6,8 @@ import { Home } from '../Home';
 import { Languages } from '../Languages';
 import { Organizations } from '../Organizations';
 import { Projects } from '../Projects';
-import { Users } from '../Users';
 import { Upload } from '../Upload/Upload';
+import { Users } from '../Users';
 import { Header } from './Header';
 import { Sidebar } from './Sidebar';
 
