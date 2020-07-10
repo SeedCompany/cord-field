0493cdfaf1f23457984e596d604965056ffe99f7
Ryan Elainska <ryan@elainska.com>
2020-07-10 19:27:35 -0400

    Post-upload error handling
    - Create `deleteFileNode` mutation and re-generate typings
    - Update `UploadContext` to handle an error in the file upload callback
      by deleting the uploaded file and setting an error for the user
    - Refactor `setUploadError` function in `UploadContext` to take an
      `errorMessage` string instead of an `error` Error and just create the
      `new Error()` from the `errorMessage` string

--- src/components/Upload/Upload.generated.ts
+++ src/components/Upload/Upload.generated.ts
@@ -25,6 +25,15 @@ export type CreateFileVersionMutation = { __typename?: 'Mutation' } & {
   >;
 };
 
+export interface DeleteFileNodeMutationVariables {
+  id: Types.Scalars['ID'];
+}
+
+export type DeleteFileNodeMutation = { __typename?: 'Mutation' } & Pick<
+  Types.Mutation,
+  'deleteFileNode'
+>;
+
 export const RequestFileUploadDocument = gql`
   mutation RequestFileUpload {
     requestFileUpload {
@@ -130,4 +139,52 @@ export type CreateFileVersionMutationResult = ApolloReactCommon.MutationResult<
 export type CreateFileVersionMutationOptions = ApolloReactCommon.BaseMutationOptions<
   CreateFileVersionMutation,
   CreateFileVersionMutationVariables
+>;
+export const DeleteFileNodeDocument = gql`
+  mutation DeleteFileNode($id: ID!) {
+    deleteFileNode(id: $id)
+  }
+`;
+export type DeleteFileNodeMutationFn = ApolloReactCommon.MutationFunction<
+  DeleteFileNodeMutation,
+  DeleteFileNodeMutationVariables
+>;
+
+/**
+ * __useDeleteFileNodeMutation__
+ *
+ * To run a mutation, you first call `useDeleteFileNodeMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useDeleteFileNodeMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [deleteFileNodeMutation, { data, loading, error }] = useDeleteFileNodeMutation({
+ *   variables: {
+ *      id: // value for 'id'
+ *   },
+ * });
+ */
+export function useDeleteFileNodeMutation(
+  baseOptions?: ApolloReactHooks.MutationHookOptions<
+    DeleteFileNodeMutation,
+    DeleteFileNodeMutationVariables
+  >
+) {
+  return ApolloReactHooks.useMutation<
+    DeleteFileNodeMutation,
+    DeleteFileNodeMutationVariables
+  >(DeleteFileNodeDocument, baseOptions);
+}
+export type DeleteFileNodeMutationHookResult = ReturnType<
+  typeof useDeleteFileNodeMutation
+>;
+export type DeleteFileNodeMutationResult = ApolloReactCommon.MutationResult<
+  DeleteFileNodeMutation
+>;
+export type DeleteFileNodeMutationOptions = ApolloReactCommon.BaseMutationOptions<
+  DeleteFileNodeMutation,
+  DeleteFileNodeMutationVariables
 >;
