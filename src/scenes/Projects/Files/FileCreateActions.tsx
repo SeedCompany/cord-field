555619f76b27d306b6bc690d9e6ab0ac43006d13
Ryan Elainska <ryan@elainska.com>
2020-07-10 18:45:17 -0400

    Dropzone Field component and uploading files to project
    - Move `Dropzone` component from `components/Upload` to `components/form`
    - Refactor `Dropzone` to consume `useField` so it can be properly used
      in a form
    - Create `UploadProjectFiles` dialog form and wire up to add uploaded
      files to correct project/directory
    - Wire up `UploadProjectFiles` to correct UI button in `FileCreateActions`

--- src/scenes/Projects/Files/FileCreateActions.tsx
+++ src/scenes/Projects/Files/FileCreateActions.tsx
@@ -3,18 +3,15 @@ import { CreateNewFolder, Publish } from '@material-ui/icons';
 import React from 'react';
 import { useDialog } from '../../../components/Dialog';
 import { CreateProjectDirectory } from './CreateProjectDirectory';
+import { UploadProjectFiles } from './UploadProjectFiles';
 
 export const FileCreateActions = () => {
   const [createDirectoryState, createDirectory] = useDialog();
-  // const [uploadFileState, uploadFile] = useDialog();
+  const [uploadFileState, uploadFile] = useDialog();
   return (
     <>
       <Box>
-        <IconButton
-          color="inherit"
-          onClick={() => console.log('Upload file')}
-          size="small"
-        >
+        <IconButton color="inherit" onClick={uploadFile} size="small">
           <Publish />
         </IconButton>
         <IconButton color="inherit" onClick={createDirectory} size="small">
@@ -22,6 +19,7 @@ export const FileCreateActions = () => {
         </IconButton>
       </Box>
       <CreateProjectDirectory {...createDirectoryState} />
+      <UploadProjectFiles {...uploadFileState} />
     </>
   );
 };
