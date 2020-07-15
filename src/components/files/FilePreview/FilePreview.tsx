289fcb74d70e4dd0320dad4eb9c77aa679db1110
Ryan Elainska <ryan@elainska.com>
2020-07-15 12:51:29 -0400

    File Download; File Component Organization
    - Add `file-saver` package and types to appliction
    - Wire up `Download` option in File Actions Menu to to `FileSaver.saveAs()`
      to download the file from its pre-signed URL
    - Consolidate all file-related components into new `components/files`
      directory

---
+++ src/components/files/FilePreview/FilePreview.tsx
@@ -0,0 +1,36 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from '@material-ui/core';
+import React, { FC } from 'react';
+import FileViewer from 'react-file-viewer';
+import { File } from '../../../api';
+
+interface FilePreviewProps {
+  file?: File;
+  open: boolean;
+  onClose: () => void;
+  onExited: () => void;
+}
+
+export const FilePreview: FC<FilePreviewProps> = (props) => {
+  const { file, onClose, ...rest } = props;
+  const { name, downloadUrl } = file ?? { name: '', downloadUrl: '' };
+  const fileType = name.split('.').pop();
+  return (
+    <Dialog onClose={onClose} {...rest} aria-labelledby="dialog-file-preview">
+      <DialogTitle id="dialog-file-preview">{name}</DialogTitle>
+      <DialogContent>
+        <FileViewer fileType={fileType} filePath={downloadUrl} />
+      </DialogContent>
+      <DialogActions>
+        <Button color="secondary" onClick={onClose}>
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
