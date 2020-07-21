606d552b045ce73ead005235d7e7629338eab4b4
Frank Ma <yurenfrankma@gmail.com>
2020-07-21 13:03:58 -0700

    feat(CheckBoxesField): allow pickOne functionality

--- src/components/form/CheckboxesField.tsx
+++ src/components/form/CheckboxesField.tsx
@@ -36,6 +36,7 @@ export type CheckboxesFieldProps = FieldConfig<string[]> &
     label?: ReactNode;
     helperText?: ReactNode;
     FormGroupProps?: FormGroupProps;
+    pickOne?: boolean;
   };
 
 export type CheckboxOptionProps = Pick<
@@ -70,6 +71,7 @@ export const CheckboxesField = ({
   labelPlacement = 'end',
   defaultValue: defaultValueProp,
   FormGroupProps,
+  pickOne,
   ...props
 }: CheckboxesFieldProps) => {
   // Memoize defaultValue so array can be passed inline while still preventing
@@ -137,6 +139,14 @@ export const CheckboxesField = ({
                 return;
               }
 
+              if (pickOne && checked) {
+                input.onChange([optName]);
+                return;
+              }
+              if (pickOne && !checked) {
+                input.onChange([]);
+                return;
+              }
               const newVal = new Set(value);
               if (checked) {
                 newVal.add(optName);
