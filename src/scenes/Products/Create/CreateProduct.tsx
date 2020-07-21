f99674ca55e1c4bdf9efb02f4d06e9044fdcd4cf
Frank Ma <yurenfrankma@gmail.com>
2020-07-20 17:25:10 -0700

    feat(Create): wire up basic product type section

--- src/scenes/Products/Create/CreateProduct.tsx
+++ src/scenes/Products/Create/CreateProduct.tsx
@@ -1,9 +1,17 @@
-import { Breadcrumbs, makeStyles, Typography } from '@material-ui/core';
-import React, { FC } from 'react';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Breadcrumbs,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import React, { FC, useState } from 'react';
 import { useParams } from 'react-router';
 import { Breadcrumb } from '../../../components/Breadcrumb';
 import { ProjectBreadcrumb } from '../../../components/ProjectBreadcrumb';
 import { useGetProjectBreadcrumbQuery } from './CreateProduct.generated';
+import { ProductTypeSection } from './Sections';
 
 const useStyles = makeStyles(({ spacing, breakpoints }) => ({
   root: {
@@ -26,6 +34,19 @@ export const CreateProduct: FC = () => {
 
   const project = data?.project;
 
+  const [expandedSection, setExpandedSection] = useState('product');
+  const [selectedItems, setSelectedItems] = useState({
+    product: '',
+  });
+
+  const selectProduct = (product: any) => {
+    setSelectedItems({
+      ...selectedItems,
+      product,
+    });
+    setExpandedSection('book');
+  };
+
   return (
     <main className={classes.root}>
       <Breadcrumbs>
@@ -38,6 +59,20 @@ export const CreateProduct: FC = () => {
         </Breadcrumb>
       </Breadcrumbs>
       <Typography variant="h2">Create Product</Typography>
+      <Accordion>
+        <AccordionSummary>
+          <Typography>Choose Product</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>details</Typography>
+        </AccordionDetails>
+      </Accordion>
+      <ProductTypeSection
+        open={expandedSection === 'product'}
+        selectedItem={selectedItems['product']}
+        submitSelection={selectProduct}
+        setExpandedSection={setExpandedSection}
+      />
     </main>
   );
 };
