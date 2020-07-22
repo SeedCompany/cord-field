4583f4a0928deb78a91324dc867b3aaedbf032be
Frank Ma <yurenfrankma@gmail.com>
2020-07-21 18:46:43 -0700

    feat(CreateProduct.tsx): consume AccordionState to match accordion view

--- src/scenes/Products/Create/CreateProduct.tsx
+++ src/scenes/Products/Create/CreateProduct.tsx
@@ -3,15 +3,27 @@ import {
   AccordionDetails,
   AccordionSummary,
   Breadcrumbs,
+  IconButton,
   makeStyles,
   Typography,
 } from '@material-ui/core';
-import React, { FC, useState } from 'react';
+import React, { FC, useReducer } from 'react';
+import { Form } from 'react-final-form';
 import { useParams } from 'react-router';
 import { Breadcrumb } from '../../../components/Breadcrumb';
+import {
+  ToggleButtonOption,
+  ToggleButtonsField,
+} from '../../../components/form';
+import { PencilCircledIcon } from '../../../components/Icons';
 import { ProjectBreadcrumb } from '../../../components/ProjectBreadcrumb';
+import {
+  AccordionState,
+  accordionStateReducer,
+  expandSection,
+  initialAccordionState,
+} from './AccordionState';
 import { useGetProjectBreadcrumbQuery } from './CreateProduct.generated';
-import { ProductTypeSection } from './Sections';
 
 const useStyles = makeStyles(({ spacing, breakpoints }) => ({
   root: {
@@ -33,19 +45,11 @@ export const CreateProduct: FC = () => {
   });
 
   const project = data?.project;
-
-  const [expandedSection, setExpandedSection] = useState('product');
-  const [selectedItems, setSelectedItems] = useState({
-    product: '',
-  });
-
-  const selectProduct = (product: any) => {
-    setSelectedItems({
-      ...selectedItems,
-      product,
-    });
-    setExpandedSection('book');
-  };
+  const [accordionState, dispatch] = useReducer(
+    accordionStateReducer,
+    initialAccordionState
+  );
+  console.log('accordionState: ', accordionState);
 
   return (
     <main className={classes.root}>
@@ -59,20 +63,81 @@ export const CreateProduct: FC = () => {
         </Breadcrumb>
       </Breadcrumbs>
       <Typography variant="h2">Create Product</Typography>
-      <Accordion>
-        <AccordionSummary>
-          <Typography>Choose Product</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>details</Typography>
-        </AccordionDetails>
-      </Accordion>
-      <ProductTypeSection
-        open={expandedSection === 'product'}
-        selectedItem={selectedItems['product']}
-        submitSelection={selectProduct}
-        setExpandedSection={setExpandedSection}
-      />
+      <Form
+        onSubmit={(...stuff) => console.log('submit clicked, stuff: ', stuff)}
+      >
+        {({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <AccordionState dispatch={dispatch} />
+            <Accordion expanded={accordionState.expand.product}>
+              <AccordionSummary>
+                <Typography>Choose Product</Typography>
+                {!accordionState.expand.product && (
+                  <IconButton
+                    color="primary"
+                    onClick={() => dispatch(expandSection('product'))}
+                  >
+                    <PencilCircledIcon />
+                  </IconButton>
+                )}
+              </AccordionSummary>
+              <AccordionDetails>
+                <ToggleButtonsField name="product" pickOne>
+                  {[
+                    'scripture',
+                    'story',
+                    'film',
+                    'song',
+                    'literacyMaterial',
+                  ].map((option) => (
+                    <ToggleButtonOption
+                      key={option}
+                      label={option}
+                      value={option}
+                    />
+                  ))}
+                </ToggleButtonsField>
+              </AccordionDetails>
+            </Accordion>
+            {accordionState.show.medium && (
+              <Accordion expanded={accordionState.expand.medium}>
+                <AccordionSummary>
+                  <Typography>Choose Medium</Typography>
+                  {!accordionState.expand.medium && (
+                    <IconButton
+                      color="primary"
+                      onClick={() => dispatch(expandSection('medium'))}
+                    >
+                      <PencilCircledIcon />
+                    </IconButton>
+                  )}
+                </AccordionSummary>
+                <AccordionDetails>
+                  <ToggleButtonsField name="medium">
+                    {[
+                      'print',
+                      'web',
+                      'video',
+                      'app',
+                      'ebook',
+                      'audioRecording',
+                      'oralTranslation',
+                      'other',
+                    ].map((option) => (
+                      <ToggleButtonOption
+                        key={option}
+                        label={option}
+                        value={option}
+                      />
+                    ))}
+                  </ToggleButtonsField>
+                </AccordionDetails>
+              </Accordion>
+            )}
+            <button type="submit">submit</button>
+          </form>
+        )}
+      </Form>
     </main>
   );
 };
