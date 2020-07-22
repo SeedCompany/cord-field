b0eca68f42fed56e3a4f0e9fac00c8cd1348b9a7
Ryan Elainska <ryan@elainska.com>
2020-07-22 15:38:32 -0400

    Remove `react-file-viewer` and sub in a simple iframe for now

--- src/components/files/FilePreview/FilePreview.tsx
+++ src/components/files/FilePreview/FilePreview.tsx
@@ -6,7 +6,6 @@ import {
   DialogTitle,
 } from '@material-ui/core';
 import React, { FC } from 'react';
-import FileViewer from 'react-file-viewer';
 import { File } from '../../../api';
 
 interface FilePreviewProps {
@@ -19,12 +18,11 @@ interface FilePreviewProps {
 export const FilePreview: FC<FilePreviewProps> = (props) => {
   const { file, onClose, ...rest } = props;
   const { name, downloadUrl } = file ?? { name: '', downloadUrl: '' };
-  const fileType = name.split('.').pop() ?? '.unknown';
   return (
     <Dialog onClose={onClose} {...rest} aria-labelledby="dialog-file-preview">
       <DialogTitle id="dialog-file-preview">{name}</DialogTitle>
       <DialogContent>
-        <FileViewer fileType={fileType} filePath={downloadUrl} />
+        <iframe title={`${name} file preview`} src={downloadUrl} />
       </DialogContent>
       <DialogActions>
         <Button color="secondary" onClick={onClose}>
