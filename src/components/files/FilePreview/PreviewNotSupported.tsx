e661d373bb43fa7e4d78991a904d5e981f79504d
Ryan Elainska <ryan@elainska.com>
2020-07-25 14:37:37 -0400

    Add minimal styling to `PreviewNotSupported` component

--- src/components/files/FilePreview/PreviewNotSupported.tsx
+++ src/components/files/FilePreview/PreviewNotSupported.tsx
@@ -1,11 +1,23 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 
+const useStyles = makeStyles(({ spacing }) => ({
+  container: {
+    margin: spacing(4),
+  },
+  text: {
+    lineHeight: 1.5,
+  },
+}));
+
 export const PreviewNotSupported = () => {
+  const classes = useStyles();
   return (
-    <Box textAlign="center">
-      <Typography variant="h3">
-        Previewing is not supported for this file type
+    <Box className={classes.container} textAlign="center">
+      <Typography variant="h3" className={classes.text}>
+        Previewing is not supported
+        <br />
+        for this file type
       </Typography>
     </Box>
   );
