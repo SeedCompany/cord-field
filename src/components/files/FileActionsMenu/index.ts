a583cc1a5d824543078a0867b43025ddcbdbfdd8
Ryan Elainska <ryan@elainska.com>
2020-07-26 00:16:31 -0400

    Get File `downloadUrl` on demand
    We want to make sure that if the user opens the files list for a project then
    leaves it open for a long time and after that tries to download a file without
    refreshing the page, they don't get a bad result because the presigned URL has
    expired.
    
    - Update `useDownloadFile` hook to use Apollo's `client.query` method to query
      for the `downloadUrl` imperatively in response to user click of "Download"
      button
    - Create new `FileDownloadUrl` query for the above
    - Remove `downloadUrl` from the fields queried in the main `ProjectDirectory`
      on all `File` and `FileVersion` items
    - Regenerate typings

--- src/components/files/FileActionsMenu/index.ts
+++ src/components/files/FileActionsMenu/index.ts
@@ -1,3 +1,4 @@
 export * from './DeleteFile';
 export * from './FileActionsMenu';
 export * from './RenameFile';
+export * from './FileActions.generated';
--- src/components/files/hooks/useDownloadFile.tsx
+++ src/components/files/hooks/useDownloadFile.tsx
@@ -1,20 +1,65 @@
+import { useApolloClient } from '@apollo/client';
 import FileSaver from 'file-saver';
+import { useSnackbar } from 'notistack';
+import { useCallback } from 'react';
 import { PartialDeep } from 'type-fest';
 import { Directory, File, FileVersion } from '../../../api';
+import {
+  FileDownloadUrlDocument,
+  FileDownloadUrlQuery,
+} from '../FileActionsMenu';
 
 type DownloadableFile = PartialDeep<Directory | File | FileVersion>;
 
 export const useDownloadFile = (): ((file: DownloadableFile) => void) => {
-  const downloadFile = (file: DownloadableFile) => {
-    // We'll construct this so that it can just swallow a
-    // `Directory`, and rely on other mechanisms to avoid
-    // passing it one for now. Later, this might be useful
-    // for letting the user download an entire folder.
-    const isDirectory = (
-      file: DownloadableFile
-    ): file is PartialDeep<Directory> => file.type === 'Directory';
-    if (!isDirectory(file) && file.downloadUrl)
-      FileSaver.saveAs(file.downloadUrl, file.name ?? undefined);
-  };
+  const { enqueueSnackbar } = useSnackbar();
+
+  const showSnackbarError = useCallback(() => {
+    enqueueSnackbar('Could not download file', {
+      variant: 'error',
+    });
+  }, [enqueueSnackbar]);
+
+  const client = useApolloClient();
+
+  const queryDownloadUrl = useCallback(
+    async (id: string): Promise<string> => {
+      try {
+        const { data } = await client.query<FileDownloadUrlQuery>({
+          query: FileDownloadUrlDocument,
+          variables: { id },
+        });
+        return data?.file.downloadUrl ?? '';
+      } catch (error) {
+        console.log(error);
+        showSnackbarError();
+        return '';
+      }
+    },
+    [client, showSnackbarError]
+  );
+
+  const downloadFile = useCallback(
+    async (file: DownloadableFile) => {
+      // We'll construct this so that it can just swallow a
+      // `Directory`, and rely on other mechanisms to avoid
+      // passing it one for now. Later, this might be useful
+      // for letting the user download an entire folder.
+      const isDirectory = (
+        file: DownloadableFile
+      ): file is PartialDeep<Directory> => file.type === 'Directory';
+      if (!isDirectory(file)) {
+        try {
+          const downloadUrl = await queryDownloadUrl(file.id!);
+          if (downloadUrl)
+            FileSaver.saveAs(downloadUrl, file.name ?? undefined);
+        } catch (error) {
+          console.log(error);
+          showSnackbarError();
+        }
+      }
+    },
+    [queryDownloadUrl, showSnackbarError]
+  );
   return downloadFile;
 };
