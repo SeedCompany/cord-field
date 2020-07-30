0b00b0833c51cb403feeb3f8c12dbb4e29c8bf20
Frank Ma <yurenfrankma@gmail.com>
2020-07-30 11:51:15 -0700

    fix(EditProduct.graphql): update mutation

--- src/scenes/Products/Edit/EditProduct.generated.ts
+++ src/scenes/Products/Edit/EditProduct.generated.ts
@@ -37,71 +37,136 @@ export interface ProductQuery {
   readonly product:
     | ({ readonly __typename?: 'DirectScriptureProduct' } & Pick<
         Types.DirectScriptureProduct,
-        'id' | 'approach' | 'legacyType'
+        'id' | 'legacyType' | 'approach'
       > & {
           readonly scriptureReferences: {
             readonly __typename?: 'SecuredScriptureRanges';
-          } & {
-            readonly value: ReadonlyArray<
-              { readonly __typename?: 'ScriptureRange' } & {
-                readonly start: {
-                  readonly __typename?: 'ScriptureReference';
-                } & Pick<
-                  Types.ScriptureReference,
-                  'book' | 'chapter' | 'verse'
-                >;
-                readonly end: {
-                  readonly __typename?: 'ScriptureReference';
-                } & Pick<
-                  Types.ScriptureReference,
-                  'book' | 'chapter' | 'verse'
-                >;
-              }
-            >;
-          };
+          } & Pick<Types.SecuredScriptureRanges, 'canRead' | 'canEdit'> & {
+              readonly value: ReadonlyArray<
+                { readonly __typename?: 'ScriptureRange' } & {
+                  readonly start: {
+                    readonly __typename?: 'ScriptureReference';
+                  } & Pick<
+                    Types.ScriptureReference,
+                    'book' | 'chapter' | 'verse'
+                  >;
+                  readonly end: {
+                    readonly __typename?: 'ScriptureReference';
+                  } & Pick<
+                    Types.ScriptureReference,
+                    'book' | 'chapter' | 'verse'
+                  >;
+                }
+              >;
+            };
           readonly mediums: {
             readonly __typename?: 'SecuredProductMediums';
-          } & Pick<Types.SecuredProductMediums, 'value'>;
+          } & Pick<
+            Types.SecuredProductMediums,
+            'canRead' | 'canEdit' | 'value'
+          >;
           readonly purposes: {
             readonly __typename?: 'SecuredProductPurposes';
-          } & Pick<Types.SecuredProductPurposes, 'value'>;
+          } & Pick<
+            Types.SecuredProductPurposes,
+            'canRead' | 'canEdit' | 'value'
+          >;
           readonly methodology: {
             readonly __typename?: 'SecuredMethodology';
-          } & Pick<Types.SecuredMethodology, 'value'>;
+          } & Pick<Types.SecuredMethodology, 'canRead' | 'canEdit' | 'value'>;
         })
     | ({ readonly __typename?: 'DerivativeScriptureProduct' } & Pick<
         Types.DerivativeScriptureProduct,
-        'id' | 'approach' | 'legacyType'
+        'id' | 'legacyType' | 'approach'
       > & {
-          readonly scriptureReferences: {
-            readonly __typename?: 'SecuredScriptureRanges';
-          } & {
-            readonly value: ReadonlyArray<
-              { readonly __typename?: 'ScriptureRange' } & {
-                readonly start: {
-                  readonly __typename?: 'ScriptureReference';
-                } & Pick<
-                  Types.ScriptureReference,
-                  'book' | 'chapter' | 'verse'
-                >;
-                readonly end: {
-                  readonly __typename?: 'ScriptureReference';
-                } & Pick<
-                  Types.ScriptureReference,
-                  'book' | 'chapter' | 'verse'
+          readonly produces: {
+            readonly __typename?: 'SecuredProducible';
+          } & Pick<Types.SecuredProducible, 'canRead' | 'canEdit'> & {
+              readonly value?: Types.Maybe<
+                | ({ readonly __typename: 'DirectScriptureProduct' } & Pick<
+                    Types.DirectScriptureProduct,
+                    'createdAt'
+                  >)
+                | ({ readonly __typename: 'DerivativeScriptureProduct' } & Pick<
+                    Types.DerivativeScriptureProduct,
+                    'createdAt'
+                  >)
+                | ({ readonly __typename: 'Film' } & Pick<
+                    Types.Film,
+                    'createdAt'
+                  >)
+                | ({ readonly __typename: 'LiteracyMaterial' } & Pick<
+                    Types.LiteracyMaterial,
+                    'createdAt'
+                  >)
+                | ({ readonly __typename: 'Story' } & Pick<
+                    Types.Story,
+                    'createdAt'
+                  >)
+                | ({ readonly __typename: 'Song' } & Pick<
+                    Types.Song,
+                    'createdAt'
+                  >)
+              >;
+            };
+          readonly scriptureReferencesOverride?: Types.Maybe<
+            { readonly __typename?: 'SecuredScriptureRanges' } & Pick<
+              Types.SecuredScriptureRanges,
+              'canRead' | 'canEdit'
+            > & {
+                readonly value: ReadonlyArray<
+                  { readonly __typename?: 'ScriptureRange' } & {
+                    readonly start: {
+                      readonly __typename?: 'ScriptureReference';
+                    } & Pick<
+                      Types.ScriptureReference,
+                      'book' | 'chapter' | 'verse'
+                    >;
+                    readonly end: {
+                      readonly __typename?: 'ScriptureReference';
+                    } & Pick<
+                      Types.ScriptureReference,
+                      'book' | 'chapter' | 'verse'
+                    >;
+                  }
                 >;
               }
-            >;
-          };
+          >;
+          readonly scriptureReferences: {
+            readonly __typename?: 'SecuredScriptureRanges';
+          } & Pick<Types.SecuredScriptureRanges, 'canRead' | 'canEdit'> & {
+              readonly value: ReadonlyArray<
+                { readonly __typename?: 'ScriptureRange' } & {
+                  readonly start: {
+                    readonly __typename?: 'ScriptureReference';
+                  } & Pick<
+                    Types.ScriptureReference,
+                    'book' | 'chapter' | 'verse'
+                  >;
+                  readonly end: {
+                    readonly __typename?: 'ScriptureReference';
+                  } & Pick<
+                    Types.ScriptureReference,
+                    'book' | 'chapter' | 'verse'
+                  >;
+                }
+              >;
+            };
           readonly mediums: {
             readonly __typename?: 'SecuredProductMediums';
-          } & Pick<Types.SecuredProductMediums, 'value'>;
+          } & Pick<
+            Types.SecuredProductMediums,
+            'canRead' | 'canEdit' | 'value'
+          >;
           readonly purposes: {
             readonly __typename?: 'SecuredProductPurposes';
-          } & Pick<Types.SecuredProductPurposes, 'value'>;
+          } & Pick<
+            Types.SecuredProductPurposes,
+            'canRead' | 'canEdit' | 'value'
+          >;
           readonly methodology: {
             readonly __typename?: 'SecuredMethodology';
-          } & Pick<Types.SecuredMethodology, 'value'>;
+          } & Pick<Types.SecuredMethodology, 'canRead' | 'canEdit' | 'value'>;
         });
   readonly project:
     | ({
@@ -181,8 +246,37 @@ export type UpdateProductMutationOptions = ApolloReactCommon.BaseMutationOptions
 export const ProductDocument = gql`
   query Product($productId: ID!, $projectId: ID!, $engagementId: ID!) {
     product(id: $productId) {
+      ... on DerivativeScriptureProduct {
+        produces {
+          canRead
+          canEdit
+          value {
+            __typename
+            createdAt
+          }
+        }
+        scriptureReferencesOverride {
+          canRead
+          canEdit
+          value {
+            start {
+              book
+              chapter
+              verse
+            }
+            end {
+              book
+              chapter
+              verse
+            }
+          }
+        }
+      }
       id
+      legacyType
       scriptureReferences {
+        canRead
+        canEdit
         value {
           start {
             book
@@ -197,12 +291,18 @@ export const ProductDocument = gql`
         }
       }
       mediums {
+        canRead
+        canEdit
         value
       }
       purposes {
+        canRead
+        canEdit
         value
       }
       methodology {
+        canRead
+        canEdit
         value
       }
       approach
