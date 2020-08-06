26015e140fb04550cd6f487b066e3af586f3a840
Ryan Elainska <ryan@elainska.com>
2020-08-06 15:50:58 -0400

    Update src/components/Upload/UploadManager.tsx
    Co-authored-by: Carson Full <carson_full@tsco.org>

--- src/components/Upload/UploadManager.tsx
+++ src/components/Upload/UploadManager.tsx
@@ -201,7 +201,6 @@ export const UploadManager: FC = memo((props) => {
 
   function handleCollapse(event: React.MouseEvent<HTMLButtonElement>) {
     event.stopPropagation();
-    console.log('Changing collapsed');
     setIsCollapsed((isCollapsed) => !isCollapsed);
   }
 
