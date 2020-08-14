2dd6ecfd8504d0deed97b14eb2979506add8655f
Ryan Elainska <ryan@elainska.com>
2020-08-14 16:27:15 -0400

    Skeleton of Engagement File Card Creates `components/EngagementFileCard` component and stories. Not yet wired up with GraphQL and still lacking context menu for performing actions on the passed-in file

---
+++ src/components/EngagementFileCard/EngagementFileCard.stories.tsx
@@ -0,0 +1,72 @@
+import { Box } from '@material-ui/core';
+import { text } from '@storybook/addon-knobs';
+import React from 'react';
+import { dateTime } from '../knobs.stories';
+import { EngagementFileCard as Card } from './EngagementFileCard';
+
+export default {
+  title: 'Components/Engagement File Card',
+};
+
+export const EngagementFileCard = () => {
+  const createdByUser = {
+    id: '9876',
+    displayFirstName: {
+      value: 'Jane',
+    },
+    displayLastName: {
+      value: 'Smith',
+    },
+  };
+  const modifiedByUser = {
+    id: '5432',
+    displayFirstName: {
+      value: text('displayFirstName', 'Jane'),
+    },
+    displayLastName: {
+      value: text('displayLastName', 'Smith'),
+    },
+  };
+  const childFile = {
+    mimeType: 'application/pdf',
+    size: 6000,
+    id: '12345',
+    createdAt: new Date(),
+    type: 'File',
+    category: 'Document',
+    name: 'Language PnP',
+    createdBy: createdByUser,
+    modifiedAt: new Date(),
+    modifiedBy: modifiedByUser,
+    downloadUrl: '',
+  };
+  const file = {
+    mimeType: 'application/pdf',
+    size: 6000,
+    id: '12345',
+    createdAt: new Date(),
+    type: 'File',
+    category: 'Document',
+    name: text('name', 'Language PnP'),
+    createdBy: createdByUser,
+    modifiedAt: dateTime('modifiedAt'),
+    modifiedBy: modifiedByUser,
+    children: {
+      total: 1,
+      hasMore: false,
+      items: [childFile],
+    },
+    downloadUrl: '',
+  };
+  return (
+    <Box display="flex" width={400}>
+      <Card file={file} />
+    </Box>
+  );
+};
+
+export const Loading = () => (
+  <Box display="flex" width={400}>
+    <Card file={undefined} />
+  </Box>
+);
---
+++ src/components/EngagementFileCard/EngagementFileCard.tsx
@@ -0,0 +1,87 @@
+import {
+  Card,
+  CardActionArea,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
+import React, { FC } from 'react';
+import { useDateFormatter } from '../Formatters';
+import { HugeIcon, ReportIcon } from '../Icons';
+
+const useStyles = makeStyles(({ palette, spacing }) => ({
+  root: {
+    flex: 1,
+    height: '100%',
+  },
+  actionArea: {
+    height: '100%',
+    display: 'flex',
+    justifyContent: 'space-evenly',
+    padding: spacing(3, 4),
+  },
+  fileInfo: {
+    flex: 1,
+    paddingLeft: spacing(4),
+  },
+  fileName: {
+    marginBottom: spacing(1),
+  },
+  fileMeta: {
+    color: palette.text.secondary,
+  },
+}));
+
+export interface EngagementFileCardProps {
+  file: any;
+}
+
+const FileCardMeta: FC<{ loading: boolean; text: string }> = ({
+  loading,
+  text,
+}) => {
+  const classes = useStyles();
+  return (
+    <Typography
+      className={classes.fileMeta}
+      color="initial"
+      variant="caption"
+      component="p"
+    >
+      {!loading ? text : <Skeleton />}
+    </Typography>
+  );
+};
+
+export const EngagementFileCard: FC<EngagementFileCardProps> = (props) => {
+  const classes = useStyles();
+  const { file } = props;
+  console.log('file', file);
+  const formatDate = useDateFormatter();
+  const { modifiedAt, name, modifiedBy } = file;
+  const {
+    displayFirstName: { value: firstName },
+    displayLastName: { value: lastName },
+  } = modifiedBy;
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea
+        className={classes.actionArea}
+        onClick={() => console.log('Preview File')}
+      >
+        <HugeIcon icon={ReportIcon} loading={!file} />
+        <div className={classes.fileInfo}>
+          <Typography className={classes.fileName} color="initial" variant="h4">
+            {file ? name : <Skeleton width="80%" />}
+          </Typography>
+          <FileCardMeta
+            text={`Updated by ${firstName} ${lastName}`}
+            loading={!file}
+          />
+          <FileCardMeta text={formatDate(modifiedAt)} loading={!file} />
+        </div>
+      </CardActionArea>
+    </Card>
+  );
+};
