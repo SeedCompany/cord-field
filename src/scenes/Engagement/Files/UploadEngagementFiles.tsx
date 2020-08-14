235d98777f4d2853044810bd017fe6c0a14a59a5
Ryan Elainska <ryan@elainska.com>
2020-08-14 16:24:07 -0400

    Add logic to `UploadEngagementFiles` to only render UI if `engagement` is defined

--- src/scenes/Engagement/Files/UploadEngagementFiles.tsx
+++ src/scenes/Engagement/Files/UploadEngagementFiles.tsx
@@ -64,7 +64,7 @@ export const UploadEngagementFiles: FC<UploadEngagementFilesProps> = ({
     });
   };
 
-  return (
+  return !engagement ? null : (
     <UploadFilesForm {...props} onFinalizeUpload={handleUploadCompleted} />
   );
 };
