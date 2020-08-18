95f471555a2d52803df8aef80163027c523baf11
Ryan Elainska <ryan@elainska.com>
2020-08-18 14:29:44 -0400

    Re-wrap hook functions in callbacks to avoid re-rendering that causes API calls to fire repeatedly

--- src/components/files/FilePreview/useRetrieveFile.ts
+++ src/components/files/FilePreview/useRetrieveFile.ts
@@ -1,36 +1,40 @@
 import { useSnackbar } from 'notistack';
+import { useCallback } from 'react';
 import { SupportedType } from '../FILE_MIME_TYPES';
 
 export const useRetrieveFile = () => {
   const { enqueueSnackbar } = useSnackbar();
 
-  const showSnackbarError = () => {
+  const showSnackbarError = useCallback(() => {
     enqueueSnackbar('Could not retrieve file', {
       variant: 'error',
     });
-  };
+  }, [enqueueSnackbar]);
 
-  const retrieveFile = async (
-    url: string,
-    type: SupportedType,
-    callback: (file: File) => void | Promise<void>,
-    errorHandler?: () => void
-  ): Promise<void> => {
-    try {
-      const response = await fetch(url);
-      if (response.status === 200) {
-        void callback(
-          new File([await response.blob()], 'Preview', {
-            type,
-          })
-        );
-      } else {
+  const retrieveFile = useCallback(
+    async (
+      url: string,
+      type: SupportedType,
+      callback: (file: File) => void | Promise<void>,
+      errorHandler?: () => void
+    ): Promise<void> => {
+      try {
+        const response = await fetch(url);
+        if (response.status === 200) {
+          void callback(
+            new File([await response.blob()], 'Preview', {
+              type,
+            })
+          );
+        } else {
+          errorHandler ? errorHandler() : showSnackbarError();
+        }
+      } catch {
         errorHandler ? errorHandler() : showSnackbarError();
       }
-    } catch {
-      errorHandler ? errorHandler() : showSnackbarError();
-    }
-  };
+    },
+    [showSnackbarError]
+  );
 
   return retrieveFile;
 };
--- src/components/files/hooks/useGetFileDownloadUrl.ts
+++ src/components/files/hooks/useGetFileDownloadUrl.ts
@@ -1,5 +1,6 @@
 import { useApolloClient } from '@apollo/client';
 import { useSnackbar } from 'notistack';
+import { useCallback } from 'react';
 import {
   FileDownloadUrl_Directory_Fragment,
   FileNodeDownloadUrlDocument,
@@ -9,33 +10,36 @@ import {
 export const useGetFileDownloadUrl = () => {
   const { enqueueSnackbar } = useSnackbar();
 
-  const showSnackbarError = () => {
+  const showSnackbarError = useCallback(() => {
     enqueueSnackbar('Could not download file', {
       variant: 'error',
     });
-  };
+  }, [enqueueSnackbar]);
 
   const client = useApolloClient();
 
-  const getFileDownloadUrl = async (id: string): Promise<string> => {
-    try {
-      const { data } = await client.query<FileNodeDownloadUrlQuery>({
-        query: FileNodeDownloadUrlDocument,
-        variables: { id },
-        fetchPolicy: 'network-only',
-      });
-      const isDirectory = (
-        node: FileNodeDownloadUrlQuery['fileNode'] | undefined
-      ): node is FileDownloadUrl_Directory_Fragment => {
-        return node?.type === 'Directory';
-      };
-      return !isDirectory(data?.fileNode)
-        ? data?.fileNode.downloadUrl ?? ''
-        : '';
-    } catch {
-      showSnackbarError();
-      return '';
-    }
-  };
+  const getFileDownloadUrl = useCallback(
+    async (id: string): Promise<string> => {
+      try {
+        const { data } = await client.query<FileNodeDownloadUrlQuery>({
+          query: FileNodeDownloadUrlDocument,
+          variables: { id },
+          fetchPolicy: 'network-only',
+        });
+        const isDirectory = (
+          node: FileNodeDownloadUrlQuery['fileNode'] | undefined
+        ): node is FileDownloadUrl_Directory_Fragment => {
+          return node?.type === 'Directory';
+        };
+        return !isDirectory(data?.fileNode)
+          ? data?.fileNode.downloadUrl ?? ''
+          : '';
+      } catch {
+        showSnackbarError();
+        return '';
+      }
+    },
+    [showSnackbarError, client]
+  );
   return getFileDownloadUrl;
 };
