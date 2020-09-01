2163d89ca56fbb48418bc37d0303f14a94b8b7d1
Ryan Elainska <ryan@elainska.com>
2020-09-01 13:22:18 -0400

    Remove mock data and replace with queried data

--- src/scenes/Projects/Budget/ProjectBudget.tsx
+++ src/scenes/Projects/Budget/ProjectBudget.tsx
@@ -1,134 +1,12 @@
 import { Breadcrumbs, makeStyles, Typography } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
-import { DateTime } from 'luxon';
-import React, { useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Breadcrumb } from '../../../components/Breadcrumb';
 import { ContentContainer as Content } from '../../../components/ContentContainer';
 import { useCurrencyFormatter } from '../../../components/Formatters/useCurrencyFormatter';
 import { Table } from '../../../components/Table';
-import {
-  ProjectBudgetQueryResult,
-  useProjectBudgetQuery,
-} from './ProjectBudget.generated';
-
-type MockQueryReturn = Pick<
-  ProjectBudgetQueryResult,
-  'data' | 'loading' | 'error'
->;
-
-/* eslint-disable @typescript-eslint/no-unnecessary-condition */
-export const MOCK_QUERY_RETURN: MockQueryReturn = {
-  loading: false,
-  error: undefined,
-  data: {
-    project: {
-      id: '234567',
-      name: {
-        canEdit: true,
-        canRead: true,
-        value: 'Pei language - phase 1',
-      },
-      deptId: {
-        canEdit: true,
-        canRead: true,
-        value: '123456',
-      },
-      budget: {
-        canEdit: true,
-        canRead: true,
-        value: {
-          id: '345678',
-          createdAt: DateTime.local(),
-          total: 34034,
-          records: [
-            {
-              id: '456789',
-              createdAt: DateTime.local(),
-              amount: {
-                canEdit: true,
-                canRead: true,
-                value: 29000,
-              },
-              fiscalYear: {
-                canEdit: true,
-                canRead: true,
-                value: 2021,
-              },
-              organization: {
-                canEdit: true,
-                canRead: true,
-                value: {
-                  id: '123456',
-                  createdAt: DateTime.local(),
-                  name: {
-                    canEdit: true,
-                    canRead: true,
-                    value: 'Ethnos360',
-                  },
-                },
-              },
-            },
-            {
-              id: '567890',
-              createdAt: DateTime.local(),
-              amount: {
-                canEdit: false,
-                canRead: true,
-                value: 3021,
-              },
-              fiscalYear: {
-                canEdit: true,
-                canRead: true,
-                value: 2020,
-              },
-              organization: {
-                canEdit: true,
-                canRead: true,
-                value: {
-                  id: '234567',
-                  createdAt: DateTime.local(),
-                  name: {
-                    canEdit: true,
-                    canRead: true,
-                    value: 'Seed Company',
-                  },
-                },
-              },
-            },
-            {
-              id: '012345',
-              createdAt: DateTime.local(),
-              amount: {
-                canEdit: true,
-                canRead: true,
-                value: 2013,
-              },
-              fiscalYear: {
-                canEdit: true,
-                canRead: true,
-                value: 2020,
-              },
-              organization: {
-                canEdit: true,
-                canRead: true,
-                value: {
-                  id: '345678',
-                  createdAt: DateTime.local(),
-                  name: {
-                    canEdit: true,
-                    canRead: true,
-                    value: 'Ethnos360',
-                  },
-                },
-              },
-            },
-          ],
-        },
-      },
-    },
-  },
-};
+import { useProjectBudgetQuery } from './ProjectBudget.generated';
 
 const useStyles = makeStyles(({ spacing }) => ({
   header: {
@@ -153,22 +31,14 @@ export const ProjectBudget = () => {
   const classes = useStyles();
   const formatCurrency = useCurrencyFormatter();
 
-  const {
-    data: budgetData,
-    // loading: budgetLoading,
-    // error: budgetError,
-  } = useProjectBudgetQuery({
+  const { data, loading, error } = useProjectBudgetQuery({
     variables: { id: projectId },
   });
-  console.log('records', budgetData?.project.budget.value?.records);
-  const [mock, setMock] = useState<MockQueryReturn>(MOCK_QUERY_RETURN);
 
-  const { data, loading, error } = mock;
-
-  const projectName = data?.project?.name.value;
-  const budget = data?.project?.budget.value;
+  const projectName = data?.project.name.value;
+  const budget = data?.project.budget.value;
   const total = budget?.total;
-  const canEditBudget = data?.project?.budget.canEdit;
+  const canEditBudget = data?.project.budget.canEdit;
   const budgetRecords = budget?.records ?? [];
 
   interface BudgetRowData {
@@ -183,13 +53,13 @@ export const ProjectBudget = () => {
     const { amount, fiscalYear, id, organization } = record;
     const { value: dollarAmount, canEdit } = amount;
     const { value: year } = fiscalYear;
-    const orgName = organization?.value?.name.value;
+    const orgName = organization.value?.name.value;
 
     const row = {
       id,
       organization: orgName ?? '',
-      fiscalYear: String(year) ?? '',
-      amount: String(dollarAmount) ?? '',
+      fiscalYear: String(year),
+      amount: String(dollarAmount),
       canEdit,
     };
     return rows.concat(row);
@@ -225,51 +95,8 @@ export const ProjectBudget = () => {
     },
   ];
 
-  function handleRowUpdate(data: BudgetRowData) {
-    // We'll totally redo this when we have a working query + mutation
-    const records = mock.data?.project.budget.value?.records ?? [];
-    const index = records.findIndex((record) => record.id === data.id);
-    const updatedRecord =
-      index > -1
-        ? {
-            ...records[index],
-            amount: {
-              ...records[index].amount,
-              value: Number(data.amount),
-            },
-          }
-        : undefined;
-    const updatedRecords = updatedRecord
-      ? records
-          .slice(0, index)
-          .concat(updatedRecord)
-          .concat(records.slice(index + 1))
-      : records;
-    const total = updatedRecords.reduce(
-      (total, record) => total + (record.amount.value ?? 0),
-      0
-    );
-    return new Promise((resolve) =>
-      resolve(
-        setMock((mock) => ({
-          ...mock,
-          data: {
-            ...mock.data!,
-            project: {
-              ...mock.data!.project,
-              budget: {
-                ...mock.data!.project.budget,
-                value: {
-                  ...mock.data!.project.budget.value!,
-                  records: updatedRecords,
-                  total,
-                },
-              },
-            },
-          },
-        }))
-      )
-    );
+  function handleRowUpdate(data: BudgetRowData): Promise<void> {
+    return new Promise((resolve) => resolve(console.log(data)));
   }
 
   return (
@@ -297,7 +124,7 @@ export const ProjectBudget = () => {
             ) : (
               <>
                 <Typography variant="h2">
-                  {data?.project.name?.value} Budget
+                  {data?.project.name.value} Budget
                 </Typography>
                 {total ? (
                   <Typography variant="body1" className={classes.total}>
