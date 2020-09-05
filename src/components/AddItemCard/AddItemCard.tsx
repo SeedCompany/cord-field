58995211df4de0e2ac244e29058e2f4963aaafa7
Ryan Elainska <ryan@elainska.com>
2020-09-05 16:28:57 -0400

    Refactor `AddItemCard` component to optionally act as a dropzone and pass in props appropriate to use of `react-dropzone`

--- src/components/AddItemCard/AddItemCard.tsx
+++ src/components/AddItemCard/AddItemCard.tsx
@@ -8,11 +8,14 @@ import {
 import { Add as AddIcon } from '@material-ui/icons';
 import clsx from 'clsx';
 import React, { FC } from 'react';
+import { DropzoneOptions, useDropzone } from 'react-dropzone';
+import { DropzoneOverlay, DropzoneOverlayClasses } from '../Upload';
 
 const useStyles = makeStyles(({ spacing }) => ({
   root: {
     flex: 1,
     height: '100%',
+    position: 'relative',
   },
   actionArea: {
     display: 'flex',
@@ -34,19 +37,67 @@ const useStyles = makeStyles(({ spacing }) => ({
   },
 }));
 
-interface AddItemCardProps {
-  className?: string;
-  itemType: string;
+interface AddItemDropzoneCard {
+  actionType: 'dropzone';
+  DropzoneProps?: {
+    options?: DropzoneOptions;
+    classes?: DropzoneOverlayClasses;
+  };
+  handleFileSelect: (files: File[]) => void;
+}
+
+interface AddItemModalCard {
+  actionType: 'modal';
   onClick: () => void;
 }
 
+interface AddItemCardSharedProps {
+  className?: string;
+  itemType: string;
+}
+
+type AddItemCardProps = (AddItemDropzoneCard | AddItemModalCard) &
+  AddItemCardSharedProps;
+
 export const AddItemCard: FC<AddItemCardProps> = (props) => {
   const classes = useStyles();
-  const { className, itemType, onClick } = props;
+  const { actionType, className, itemType } = props;
+
+  const isDropzone = (
+    props: AddItemCardProps
+  ): props is AddItemDropzoneCard & AddItemCardSharedProps =>
+    props.actionType === 'dropzone';
+
+  const dropzoneOptions = isDropzone(props)
+    ? {
+        ...props.DropzoneProps?.options,
+        onDrop: props.handleFileSelect,
+      }
+    : {};
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone(
+    dropzoneOptions
+  );
 
   return (
-    <Card className={clsx(classes.root, className)}>
-      <CardActionArea className={classes.actionArea} onClick={onClick}>
+    <Card
+      className={clsx(classes.root, className)}
+      {...(actionType === 'dropzone' ? { ...getRootProps() } : null)}
+    >
+      {isDropzone(props) && (
+        <>
+          <input {...getInputProps()} name="pnp_version_uploader" />
+          <DropzoneOverlay
+            classes={props.DropzoneProps?.classes}
+            isDragActive={isDragActive}
+            message={`Drop ${itemType} to upload`}
+          />
+        </>
+      )}
+      <CardActionArea
+        className={classes.actionArea}
+        onClick={!isDropzone(props) ? props.onClick : undefined}
+      >
         <Avatar classes={{ root: classes.avatar }}>
           <AddIcon className={classes.icon} fontSize="large" />
         </Avatar>
