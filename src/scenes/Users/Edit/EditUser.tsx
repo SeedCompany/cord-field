3866ece3937a5b194d6a45c2ea2a9f638487ab09
Frank Ma <yurenfrankma@gmail.com>
2020-09-08 12:11:29 -0700

    fix(EditUser.tsx): memoize initialValues

--- src/scenes/Users/Edit/EditUser.tsx
+++ src/scenes/Users/Edit/EditUser.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Except } from 'type-fest';
 import { Maybe, UpdateUserInput } from '../../../api';
 import { UserForm, UserFormProps } from '../UserForm';
@@ -13,28 +13,32 @@ export const EditUser = (props: EditUserProps) => {
   const [updateUser] = useUpdateUserMutation();
   const user = props.user;
 
+  const initialValues = useMemo(
+    () =>
+      user
+        ? {
+            user: {
+              id: user.id,
+              realFirstName: user.realFirstName.value,
+              realLastName: user.realLastName.value,
+              displayFirstName: user.displayFirstName.value,
+              displayLastName: user.displayLastName.value,
+              phone: user.phone.value,
+              timezone: user.timezone.value?.name,
+              bio: user.bio.value,
+              email: user.email.value,
+            },
+          }
+        : undefined,
+    [user]
+  );
+
   return (
     <UserForm<UpdateUserInput & { user: { email?: Maybe<string> } }>
       title="Edit Person"
       {...props}
       prefix="user"
-      initialValues={
-        user
-          ? {
-              user: {
-                id: user.id,
-                realFirstName: user.realFirstName.value,
-                realLastName: user.realLastName.value,
-                displayFirstName: user.displayFirstName.value,
-                displayLastName: user.displayLastName.value,
-                phone: user.phone.value,
-                timezone: user.timezone.value?.name,
-                bio: user.bio.value,
-                email: user.email.value,
-              },
-            }
-          : undefined
-      }
+      initialValues={initialValues}
       onSubmit={async ({ user: { email, ...user } }) => {
         await updateUser({
           variables: {
