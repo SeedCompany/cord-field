347c9b399e0809ab9ad529112fb6e822807bab71
Carson Full <carson_full@tsco.org>
2020-09-08 18:28:11 -0500

    Remove unneeded styles & props & generated lists

--- src/scenes/Projects/Members/Create/CreateProjectMember.tsx
+++ src/scenes/Projects/Members/Create/CreateProjectMember.tsx
@@ -1,8 +1,7 @@
-import { Container, makeStyles } from '@material-ui/core';
 import React from 'react';
 import { FormSpy } from 'react-final-form';
 import { Except } from 'type-fest';
-import { displayRole, Role } from '../../../../api';
+import { displayRole, Role, RoleList } from '../../../../api';
 import {
   DialogForm,
   DialogFormProps,
@@ -29,41 +28,10 @@ type CreateProjectMemberProps = Except<
   projectId: string;
 };
 
-const Roles: Role[] = [
-  'BibleTranslationLiaison',
-  'Consultant',
-  'ConsultantManager',
-  'Controller',
-  'Development',
-  'ExecutiveDevelopmentRepresentative',
-  'ExecutiveLeadership',
-  'FieldOperationsDirector',
-  'FieldPartner',
-  'FinancialAnalyst',
-  'Intern',
-  'Liaison',
-  'LeadFinancialAnalyst',
-  'Mentor',
-  'OfficeOfThePresident',
-  'ProjectManager',
-  'RegionalCommunicationsCoordinator',
-  'RegionalDirector',
-  'SupportingProjectManager',
-  'Translator',
-  'Writer',
-];
-
-const useStyles = makeStyles({
-  container: {
-    width: 400,
-  },
-});
-
 export const CreateProjectMember = ({
   projectId,
   ...props
 }: CreateProjectMemberProps) => {
-  const classes = useStyles();
   const [createProjectMember] = useCreateProjectMemberMutation({
     refetchQueries: [
       {
@@ -76,9 +44,6 @@ export const CreateProjectMember = ({
   return (
     <DialogForm<DialogValues>
       title="Create Team Member"
-      closeLabel="Close"
-      submitLabel="Save"
-      onlyDirtySubmit
       {...props}
       initialValues={{
         projectMember: {
@@ -98,41 +63,32 @@ export const CreateProjectMember = ({
         await createProjectMember({ variables: { input } });
       }}
     >
-      <Container className={classes.container}>
-        <SubmitError />
-        <FormSpy>
-          {({ values }) => {
-            const formValues = values as DialogValues;
-            const canRead = formValues.projectMember.userId?.roles.canRead;
-            const userRoles = formValues.projectMember.userId?.roles.value;
+      <SubmitError />
+      <FormSpy>
+        {({ values }) => {
+          const formValues = values as DialogValues;
+          const canRead = formValues.projectMember.userId?.roles.canRead;
+          const userRoles = formValues.projectMember.userId?.roles.value;
 
-            return (
-              <FieldGroup prefix="projectMember">
-                <UserField
-                  name="userId"
-                  label="Team member"
-                  required
-                  variant="outlined"
-                  fullWidth
-                />
-                <AutocompleteField
-                  fullWidth
-                  disabled={!canRead || !userRoles}
-                  multiple
-                  options={Roles}
-                  getOptionLabel={displayRole}
-                  name="roles"
-                  label="Roles"
-                  getOptionDisabled={(option) =>
-                    !!userRoles && !userRoles.includes(option)
-                  }
-                  variant="outlined"
-                />
-              </FieldGroup>
-            );
-          }}
-        </FormSpy>
-      </Container>
+          return (
+            <FieldGroup prefix="projectMember">
+              <UserField name="userId" required variant="outlined" />
+              <AutocompleteField
+                disabled={!canRead || !userRoles}
+                multiple
+                options={RoleList}
+                getOptionLabel={displayRole}
+                name="roles"
+                label="Roles"
+                getOptionDisabled={(option) =>
+                  !!userRoles && !userRoles.includes(option)
+                }
+                variant="outlined"
+              />
+            </FieldGroup>
+          );
+        }}
+      </FormSpy>
     </DialogForm>
   );
 };
