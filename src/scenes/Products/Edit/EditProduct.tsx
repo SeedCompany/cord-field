76832cee349442cdadd0468e530873d2f460e574
Frank Ma <yurenfrankma@gmail.com>
2020-09-15 15:35:16 -0700

    fix(EditProduct): memoize initialValues

--- src/scenes/Products/Edit/EditProduct.tsx
+++ src/scenes/Products/Edit/EditProduct.tsx
@@ -1,7 +1,7 @@
 import { Breadcrumbs, makeStyles, Typography } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
 import { useSnackbar } from 'notistack';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useNavigate, useParams } from 'react-router';
 import { Except } from 'type-fest';
 import { handleFormError, UpdateProduct } from '../../../api';
@@ -50,75 +50,43 @@ export const EditProduct = () => {
     },
   });
 
+  const [updateProduct] = useUpdateProductMutation();
+
   const project = data?.project;
   const engagement = data?.engagement;
+  const product = data?.product;
 
-  const renderProductForm = () => {
-    if (!data) return null;
-    const { product } = data;
-    const { id, mediums, purposes, methodology } = product;
+  const initialValues = useMemo(() => {
+    if (!product) return undefined;
+    const { id, mediums, purposes, methodology, scriptureReferences } = product;
 
-    const initialValues = {
-      id,
-      mediums: mediums.value,
-      purposes: purposes.value,
-      methodology: methodology.value,
-      scriptureReferences: removeScriptureTypename(
-        product.scriptureReferences.value
-      ),
-      //TODO: make sure these are shown when response is ready
-      ...(product.__typename === 'DirectScriptureProduct'
-        ? {
-            productType: product.__typename,
-          }
-        : product.__typename === 'DerivativeScriptureProduct' &&
-          (product.produces.value?.__typename === 'Film' ||
-            product.produces.value?.__typename === 'Song' ||
-            product.produces.value?.__typename === 'LiteracyMaterial' ||
-            product.produces.value?.__typename === 'Story')
-        ? {
-            produces: {
-              id: product.produces.value.id,
-              name: product.produces.value.name,
-            },
-            productType: product.produces.value.__typename,
-          }
-        : undefined),
-    };
-    return (
-      <ProductForm<
-        ProductFormCustomValues & {
-          product: Except<UpdateProduct, 'produces'>;
-        }
-      >
-        product={product}
-        onSubmit={async ({
-          product: { productType, book, produces, ...input },
-        }) => {
-          try {
-            await createProduct({
-              variables: {
-                input: {
-                  product: { ...input, produces: produces?.id },
-                },
+    return {
+      product: {
+        id,
+        mediums: mediums.value,
+        purposes: purposes.value,
+        methodology: methodology.value,
+        scriptureReferences: removeScriptureTypename(scriptureReferences.value),
+        ...(product.__typename === 'DirectScriptureProduct'
+          ? {
+              productType: product.__typename,
+            }
+          : product.__typename === 'DerivativeScriptureProduct' &&
+            (product.produces.value?.__typename === 'Film' ||
+              product.produces.value?.__typename === 'Song' ||
+              product.produces.value?.__typename === 'LiteracyMaterial' ||
+              product.produces.value?.__typename === 'Story')
+          ? {
+              produces: {
+                id: product.produces.value.id,
+                name: product.produces.value.name,
               },
-            });
-
-            enqueueSnackbar(`Updates Saved`, {
-              variant: 'success',
-            });
-
-            navigate('../../');
-          } catch (e) {
-            await handleFormError(e);
-          }
-        }}
-        initialValues={{ product: initialValues }}
-      />
-    );
-  };
-
-  const [createProduct] = useUpdateProductMutation();
+              productType: product.produces.value.__typename,
+            }
+          : undefined),
+      },
+    };
+  }, [product]);
 
   return (
     <main className={classes.root}>
@@ -130,7 +98,54 @@ export const EditProduct = () => {
       <Typography variant="h2">
         {loading ? <Skeleton width="50%" variant="text" /> : 'Edit Product'}
       </Typography>
-      {renderProductForm()}
+
+      {product && (
+        <ProductForm<
+          ProductFormCustomValues & {
+            product: Except<UpdateProduct, 'produces'>;
+          }
+        >
+          product={product}
+          onSubmit={async ({
+            product: {
+              productType,
+              book,
+              produces,
+              scriptureReferences,
+              ...input
+            },
+          }) => {
+            try {
+              await updateProduct({
+                variables: {
+                  input: {
+                    product: {
+                      ...input,
+                      produces: produces?.id,
+                      ...(productType !== 'DirectScriptureProduct'
+                        ? {
+                            scriptureReferencesOverride: scriptureReferences,
+                          }
+                        : {
+                            scriptureReferences,
+                          }),
+                    },
+                  },
+                },
+              });
+
+              enqueueSnackbar(`Updates Saved`, {
+                variant: 'success',
+              });
+
+              navigate('../../');
+            } catch (e) {
+              await handleFormError(e);
+            }
+          }}
+          initialValues={initialValues}
+        />
+      )}
     </main>
   );
 };
