17be8e1d474b554b6253f6c18a431c1a5552f1e7
Ryan Elainska <ryan@elainska.com>
2020-09-16 11:32:13 -0400

    File Version Refresh for Engagement Files
    - Refactor `useUploadEngagementFiles` completion handler to use new
      `updateCachedVersions` update function on completion of upload
    - Add necessary fields to mutation returns for engagement file uploads

--- src/components/files/updateCachedVersions.ts
+++ src/components/files/updateCachedVersions.ts
@@ -7,10 +7,12 @@ export const updateCachedVersions = <MutationData>(
   existingVersions: readonly FileNodeInfoFragment[],
   parentId: string
 ) => {
+  console.log('parentId', parentId);
   const response = cache.readQuery<FileVersionsQuery>({
     query: FileVersionsDocument,
     variables: { id: parentId },
   });
+  console.log('response', response);
   if (response) {
     const updatedData = {
       ...response,
