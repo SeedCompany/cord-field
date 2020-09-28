23d73ac044121e412fe25a1338ec97c2a7b25fe7
Ryan Elainska <ryan@elainska.com>
2020-09-28 16:19:50 -0400

    Fix bugs in data-loading logic for `VirtualList` story and improve styling

--- src/components/VirtualList/VirtualList.stories.tsx
+++ src/components/VirtualList/VirtualList.stories.tsx
@@ -1,12 +1,19 @@
 import { Box, makeStyles, Typography } from '@material-ui/core';
-import { text } from '@storybook/addon-knobs';
+import { number, text } from '@storybook/addon-knobs';
 import React, { FC, useEffect, useState } from 'react';
 import { sleep } from '../../util';
 import { useItemsPerPage } from './calculateItemsPerPage';
 import { VirtualList as VL } from './VirtualList';
 
-const useStyles = makeStyles(({ spacing }) => ({
+export default { title: 'Components/VirtualList' };
+
+const useStyles = makeStyles(({ palette, spacing }) => ({
   listItem: {
+    border: `1px solid ${palette.divider}`,
+    borderRadius: 4,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
     margin: spacing(2),
     padding: spacing(2, 4),
     width: 500,
@@ -14,7 +21,7 @@ const useStyles = makeStyles(({ spacing }) => ({
 }));
 
 interface ListItemProps {
-  height: 220 | 260 | 280;
+  height: 80 | 120 | 140;
   index: number;
 }
 
@@ -23,10 +30,13 @@ const ListItem: FC<ListItemProps> = (props) => {
   const classes = useStyles();
   return (
     <div className={classes.listItem} style={{ height }}>
-      <Typography variant="h3">Item #{index}</Typography>
+      <Typography variant="h3">Item #{index + 1}</Typography>
     </div>
   );
 };
+
+const itemType = text('itemType', '');
+const numberOfItems = number('numberOfItems', 30);
 
 export const VirtualList = () => {
   const [allItems, setAllItems] = useState<ListItemProps[]>([]);
@@ -36,46 +46,52 @@ export const VirtualList = () => {
   const next = async () => {
     await sleep(750);
     const nextIndex = items.length;
-    const stopIndex =
-      nextIndex + itemsPerPage <= allItems.length
-        ? nextIndex + itemsPerPage
-        : -1;
     const newItems = allItems[nextIndex]
-      ? [...items, ...allItems.slice(nextIndex, stopIndex)]
+      ? [...items, ...allItems.slice(nextIndex, nextIndex + itemsPerPage)]
       : items;
     setItems(newItems);
   };
 
   useEffect(() => {
-    void sleep(1200).then(() => {
-      const randomItems: ListItemProps[] = Array(100)
-        .fill(null)
-        .map((_, index) => {
-          const HEIGHTS = [220, 260, 280] as const;
-          const heightIndex = Math.floor(Math.random() * HEIGHTS.length);
-          const height = HEIGHTS[heightIndex];
-          return {
-            height: height,
-            index,
-          };
-        });
-      setAllItems(randomItems);
-    });
+    const randomItems: ListItemProps[] = Array(numberOfItems)
+      .fill(null)
+      .map((_, index) => {
+        const HEIGHTS = [80, 120, 140] as const;
+        const heightIndex = Math.floor(Math.random() * HEIGHTS.length);
+        const height = HEIGHTS[heightIndex];
+        return {
+          height: height,
+          index,
+        };
+      });
+    setAllItems(randomItems);
   }, [setAllItems]);
 
+  useEffect(() => {
+    if (allItems.length > 0) {
+      setItems(allItems.slice(0, itemsPerPage));
+    }
+  }, [allItems, setItems, itemsPerPage]);
+
   return (
-    <Box width="80%">
-      <VL
-        dataLength={items.length}
-        hasMore={items.length < allItems.length}
-        itemType={text('itemType', '')}
-        next={next}
-        setContainerHeight={setContainerHeight}
-      >
-        {items.map((item) => (
-          <ListItem key={item.index} index={item.index} height={item.height} />
-        ))}
-      </VL>
+    <Box width="60%" height={400}>
+      {items.length > 0 && (
+        <VL
+          dataLength={items.length}
+          hasMore={items.length < allItems.length}
+          itemType={itemType}
+          next={next}
+          setContainerHeight={setContainerHeight}
+        >
+          {items.map((item) => (
+            <ListItem
+              key={item.index}
+              index={item.index}
+              height={item.height}
+            />
+          ))}
+        </VL>
+      )}
     </Box>
   );
 };
