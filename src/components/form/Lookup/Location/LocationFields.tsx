2f3a592dbfbd7ffe9dfb89f2644b5d250a0b50ea
Ryan Elainska <ryan@elainska.com>
2020-10-07 17:13:58 -0400

    Re-add lookup fields for `FieldRegion` and `FieldZone` now that the API supports this

--- src/components/form/Lookup/Location/LocationFields.tsx
+++ src/components/form/Lookup/Location/LocationFields.tsx
@@ -1,10 +1,32 @@
-import { DisplayLocationFragment as LocationLookupItem } from '../../../../api/fragments/location.generated';
+import {
+  DisplayFieldRegionFragment as FieldRegionLookupItem,
+  DisplayFieldZoneFragment as FieldZoneLookupItem,
+  DisplayLocationFragment as LocationLookupItem,
+} from '../../../../api/fragments/location.generated';
 import { LookupField } from '../../index';
-import { useLocationLookupLazyQuery } from './LocationLookup.generated';
+import {
+  useFieldRegionLookupLazyQuery,
+  useFieldZoneLookupLazyQuery,
+  useLocationLookupLazyQuery,
+} from './LocationLookup.generated';
 
 export const LocationField = LookupField.createFor<LocationLookupItem>({
   resource: 'Location',
   useLookup: useLocationLookupLazyQuery,
   label: 'Location',
+  placeholder: 'Search for a location by name',
+});
+
+export const FieldRegionField = LookupField.createFor<FieldRegionLookupItem>({
+  resource: 'FieldRegion',
+  useLookup: useFieldRegionLookupLazyQuery,
+  label: 'FieldRegion',
+  placeholder: 'Search for a location by name',
+});
+
+export const FieldZoneField = LookupField.createFor<FieldZoneLookupItem>({
+  resource: 'FieldZone',
+  useLookup: useFieldZoneLookupLazyQuery,
+  label: 'FieldZone',
   placeholder: 'Search for a location by name',
 });
