da90315a089d1f7ec6c1623b13bc60e47cfcd5a1
Frank Ma <yurenfrankma@gmail.com>
2020-10-08 16:09:09 -0700

    fix(LanguageEngagementListItemCard): fix card

--- src/components/LanguageEngagementListItemCard/LanguageEngagementListItemCard.tsx
+++ src/components/LanguageEngagementListItemCard/LanguageEngagementListItemCard.tsx
@@ -1,5 +1,6 @@
 import {
   Card,
+  CardActions,
   CardContent,
   Grid,
   makeStyles,
@@ -10,14 +11,12 @@ import { FC } from 'react';
 import * as React from 'react';
 import { displayEngagementStatus } from '../../api';
 import { DisplaySimpleProperty } from '../DisplaySimpleProperty';
-import { useDateFormatter, useNumberFormatter } from '../Formatters';
-import { PencilCircledIcon, ScriptIcon } from '../Icons';
-import { Picture, useRandomPicture } from '../Picture';
-import { CardActionAreaLink } from '../Routing';
+import { useNumberFormatter } from '../Formatters';
+import { ButtonLink, CardActionAreaLink } from '../Routing';
 import { LanguageEngagementListItemFragment } from './LanguageEngagementListItem.generated';
 
 const useStyles = makeStyles(({ spacing }) => {
-  const cardWidth = 666;
+  const cardWidth = 600;
   return {
     root: {
       width: '100%',
@@ -28,27 +27,22 @@ const useStyles = makeStyles(({ spacing }) => {
       display: 'flex',
       alignItems: 'initial',
     },
-    media: {
-      width: cardWidth / 3,
-      borderTopRightRadius: 0,
-      borderBottomRightRadius: 0,
-    },
+
     cardContent: {
       flex: 1,
       padding: spacing(2, 3),
       display: 'flex',
-      justifyContent: 'space-between',
     },
     leftContent: {
       flex: 1,
     },
     rightContent: {
-      flex: 1,
+      flex: 0,
       textAlign: 'right',
       marginLeft: spacing(2),
       display: 'flex',
       flexDirection: 'column',
-      justifyContent: 'space-between',
+      justifyContent: 'flex-end',
     },
     productList: {
       padding: 0,
@@ -69,30 +63,29 @@ export type LanguageEngagementListItemCardProps = LanguageEngagementListItemFrag
   className?: string;
 };
 
-export const LanguageEngagementListItemCard: FC<LanguageEngagementListItemCardProps> = (
-  props
-) => {
+export const LanguageEngagementListItemCard: FC<LanguageEngagementListItemCardProps> = ({
+  id,
+  language: securedLanguage,
+  className,
+  projectId,
+  status,
+  products,
+}) => {
   const numberFormatter = useNumberFormatter();
-  const dateFormatter = useDateFormatter();
   const classes = useStyles();
-  const pic = useRandomPicture({ seed: props.id, width: 300, height: 200 });
 
-  const language = props.language.value;
+  const language = securedLanguage.value;
   const name = language?.name.value ?? language?.displayName.value;
   const population = language?.population.value;
   const registryOfDialectsCode = language?.registryOfDialectsCode.value;
   const ethnologueCode = language?.ethnologue.code.value;
-  const endDate = getEndDate(props);
 
   return (
-    <Card className={clsx(classes.root, props.className)}>
+    <Card className={clsx(classes.root, className)}>
       <CardActionAreaLink
-        to={`/projects/${props.projectId}/engagements/${props.id}`}
+        to={`/projects/${projectId}/engagements/${id}`}
         className={classes.card}
       >
-        <div className={classes.media}>
-          <Picture fit="cover" {...pic} />
-        </div>
         <CardContent className={classes.cardContent}>
           <Grid
             container
@@ -115,89 +108,37 @@ export const LanguageEngagementListItemCard: FC<LanguageEngagementListItemCardPr
               wrap={(node) => <Grid item>{node}</Grid>}
             />
             <DisplaySimpleProperty
-              label="Status"
-              value={displayEngagementStatus(props.status)}
+              label="Population"
+              value={population}
               wrap={(node) => <Grid item>{node}</Grid>}
             />
-            {props.products.total > 0 ? (
-              <Grid item>
-                <Typography variant="body2" gutterBottom>
-                  Products:
-                </Typography>
-                <ul className={classes.productList}>
-                  {props.products.items.map((product) => (
-                    <Typography
-                      component="li"
-                      variant="body2"
-                      color="textSecondary"
-                      className={classes.centerItems}
-                      key={product.id}
-                    >
-                      {/* TODO Product naming & mapping to icons */}
-                      <ScriptIcon className={classes.iconSpacing} />
-                      {product.id}
-                    </Typography>
-                  ))}
-                </ul>
-              </Grid>
-            ) : null}
-            <Grid item>
-              <Typography
-                variant="body2"
-                color="primary"
-                className={classes.centerItems}
-              >
-                <PencilCircledIcon className={classes.iconSpacing} />
-                Edit Info
-              </Typography>
-            </Grid>
+            <DisplaySimpleProperty
+              label="Status"
+              value={displayEngagementStatus(status)}
+              wrap={(node) => <Grid item>{node}</Grid>}
+            />
           </Grid>
-          {population || endDate ? (
-            <div className={classes.rightContent}>
-              <DisplaySimpleProperty aria-hidden="true" />
-              {population ? (
-                <div>
-                  <Typography variant="h1">
-                    {numberFormatter(population)}
-                  </Typography>
-                  <Typography variant="body2" color="primary">
-                    Population
-                  </Typography>
-                </div>
-              ) : null}
-              {endDate ? (
-                <DisplaySimpleProperty
-                  label={endDate.label}
-                  value={dateFormatter(endDate.value)}
-                  ValueProps={{ color: 'primary' }}
-                />
-              ) : null}
+          <div className={classes.rightContent}>
+            <DisplaySimpleProperty aria-hidden="true" />
+            <div>
+              <Typography variant="h1">
+                {numberFormatter(products.total)}
+              </Typography>
+              <Typography variant="body2" color="primary">
+                Products
+              </Typography>
             </div>
-          ) : null}
+          </div>
         </CardContent>
       </CardActionAreaLink>
+      <CardActions>
+        <ButtonLink
+          to={`/projects/${projectId}/engagements/${id}`}
+          color="primary"
+        >
+          View Details
+        </ButtonLink>
+      </CardActions>
     </Card>
   );
-};
-
-const getEndDate = (eng: LanguageEngagementListItemFragment) => {
-  const terminal = eng.status !== 'InDevelopment' && eng.status !== 'Active';
-  if (terminal && eng.completeDate.value) {
-    return {
-      label: displayEngagementStatus(eng.status) + ' Date',
-      value: eng.completeDate.value,
-    };
-  }
-  const endDate = eng.endDate.value;
-  if (!endDate) {
-    return null;
-  }
-  if (eng.status === 'InDevelopment') {
-    return { label: 'End Date', value: endDate };
-  }
-  const revised = endDate !== eng.initialEndDate.value;
-  return {
-    label: `${revised ? 'Revised' : 'Initial'} End Date`,
-    value: endDate,
-  };
 };
