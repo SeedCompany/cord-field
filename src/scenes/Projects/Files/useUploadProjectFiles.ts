7d18b730b4ca7d3f8740a42a526ec2dd9622ef6a
Ryan Elainska <ryan@elainska.com>
2020-10-21 17:12:25 -0400

    Add new `ProjectDirectoryContents` fragment and use in `ProjectDirectory` query instead of inline fields; Update `useUploadProjectFiles` hook to write newly uploaded file into cache for immediate UI refresh

--- src/scenes/Projects/Files/useUploadProjectFiles.ts
+++ src/scenes/Projects/Files/useUploadProjectFiles.ts
@@ -11,6 +11,10 @@ import {
 } from '../../../components/files/hooks';
 import { updateCachedVersions } from '../../../components/files/updateCachedVersions';
 import { useUpdateProjectBudgetUniversalTemplateMutation } from '../Budget/ProjectBudget.generated';
+import {
+  ProjectDirectoryContentsFragment,
+  ProjectDirectoryContentsFragmentDoc,
+} from './ProjectFiles.generated';
 
 export const useUploadProjectFiles = (): UploadFilesConsumerFunction => {
   const uploadFiles = useUploadFiles();
@@ -33,11 +37,50 @@ export const useUploadProjectFiles = (): UploadFilesConsumerFunction => {
         action === 'file' ? [GQLOperations.Query.ProjectDirectory] : undefined,
       update: (cache, { data }) => {
         if (data?.createFileVersion) {
-          updateCachedVersions<CreateFileVersionMutation>(
-            cache,
-            data.createFileVersion.children.items,
-            parentId
-          );
+          if (action === 'version') {
+            updateCachedVersions<CreateFileVersionMutation>(
+              cache,
+              data.createFileVersion.children.items,
+              parentId
+            );
+          } else {
+            try {
+              const id = `Directory:${parentId}`;
+              const response = cache.readFragment<
+                ProjectDirectoryContentsFragment
+              >({
+                id,
+                fragment: ProjectDirectoryContentsFragmentDoc,
+                fragmentName: 'ProjectDirectoryContents',
+              });
+              if (response) {
+                const newFile = data.createFileVersion;
+                const currentItems = response.children.items;
+                const updatedData = {
+                  ...response,
+                  children: {
+                    ...response.children,
+                    items: currentItems.concat(newFile),
+                    total: currentItems.length + 1,
+                  },
+                };
+                cache.writeFragment<ProjectDirectoryContentsFragment>({
+                  id,
+                  fragment: ProjectDirectoryContentsFragmentDoc,
+                  fragmentName: 'ProjectDirectoryContents',
+                  data: updatedData,
+                });
+              }
+            } catch {
+              /**
+               * We need this try/catch because if this data has never been fetched
+               * before, `cache.readQuery` will throw an error instead of returning
+               * anything, which is apparently a behavior the Apollo team finds
+               * acceptable.
+               */
+              return;
+            }
+          }
         }
       },
     });
