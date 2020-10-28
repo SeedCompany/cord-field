4d9e0820e2c3eaa4dd7630592ead0194132cfcea
Ryan Elainska <ryan@elainska.com>
2020-10-28 15:15:00 -0400

    Remove extra item added to `itemsPerPage`, since `react-infinite-scroll-component` automatically starts fetching new items when it has scrolled to 80% of the container's height

--- src/components/VirtualList/useItemsPerPage.ts
+++ src/components/VirtualList/useItemsPerPage.ts
@@ -4,8 +4,7 @@ export function calculateItemsPerPage(
   containerHeight: number,
   itemHeight: number
 ) {
-  // Throwing on an extra `1` just to play it safe
-  return Math.ceil(containerHeight / itemHeight) + 1;
+  return Math.ceil(containerHeight / itemHeight);
 }
 
 export const useItemsPerPage = (
