c1d646d56a457008614b5ae58780adf4c148aca8
Frank Ma <yurenfrankma@gmail.com>
2020-10-28 15:33:48 -0700

    feat(PartnershipForm.tsx): use decorator to wipe types fields on value change

--- src/scenes/Partnerships/PartnershipForm/PartnershipForm.tsx
+++ src/scenes/Partnerships/PartnershipForm/PartnershipForm.tsx
@@ -1,3 +1,5 @@
+import { Decorator } from 'final-form';
+import onFieldChange from 'final-form-calculate';
 import React from 'react';
 import {
   displayFinancialReportingType,
@@ -25,7 +27,7 @@ import { CreatePartnershipFormInput } from '../Create';
 import { EditPartnershipFormInput } from '../Edit';
 import { PartnershipFormFragment } from './PartnershipForm.generated';
 
-type PartnershipformValues = Partial<
+type PartnershipFormValues = Partial<
   CreatePartnershipFormInput | EditPartnershipFormInput
 > & {
   partnership?: {
@@ -34,7 +36,7 @@ type PartnershipformValues = Partial<
 };
 
 export type PartnershipFormProps<
-  T extends PartnershipformValues
+  T extends PartnershipFormValues
 > = DialogFormProps<T> & {
   partnership?: PartnershipFormFragment;
 };
@@ -42,68 +44,101 @@ export type PartnershipFormProps<
 export const hasManagingType = (types: Nullable<readonly PartnerType[]>) =>
   types?.includes('Managing') ?? false;
 
-export const PartnershipForm = <T extends PartnershipformValues>({
+const decorators: Array<Decorator<PartnershipFormValues>> = [
+  ...DialogForm.defaultDecorators,
+  onFieldChange(
+    // if user selects a different partner (on create partnership), wipe the types and fin type values
+    {
+      field: 'partnership.partnerLookupItem',
+      isEqual: PartnerField.isEqual,
+      updates: {
+        'partnership.types': () => undefined,
+        'partnership.financialReportingType': () => undefined,
+      },
+    },
+    // if user unselects managing type (on create or update), wipe the financial reporting type values
+    {
+      field: 'partnership.types',
+      updates: {
+        'partnership.financialReportingType': (partnerTypes, currentValues) =>
+          partnerTypes?.includes('Managing')
+            ? currentValues.partnership?.financialReportingType
+            : undefined,
+      },
+    }
+  ),
+];
+
+export const PartnershipForm = <T extends PartnershipFormValues>({
   partnership,
   ...rest
-}: PartnershipFormProps<T>) => (
-  <DialogForm<T> {...rest} fieldsPrefix="partnership">
-    {({ values }) => {
-      const lookupPartnerTypes =
-        values.partnership?.partnerLookupItem?.types.value;
-      const lookupPartnerFinType =
-        values.partnership?.partnerLookupItem?.financialReportingTypes.value;
-      const currentPartnerTypes = partnership?.partner.value?.types.value;
-      const currentPartnerFinTypes =
-        partnership?.partner.value?.financialReportingTypes.value;
+}: PartnershipFormProps<T>) => {
+  return (
+    <DialogForm<T>
+      {...rest}
+      fieldsPrefix="partnership"
+      decorators={(decorators as unknown) as Array<Decorator<T>>}
+    >
+      {({ values }) => {
+        const lookupPartnerTypes =
+          values.partnership?.partnerLookupItem?.types.value;
+        const lookupPartnerFinType =
+          values.partnership?.partnerLookupItem?.financialReportingTypes.value;
+        const currentPartnerTypes = partnership?.partner.value?.types.value;
+        const currentPartnerFinTypes =
+          partnership?.partner.value?.financialReportingTypes.value;
 
-      return (
-        <>
-          <SubmitError />
-          {!partnership && <PartnerField name="partnerLookupItem" required />}
-          {lookupPartnerTypes?.length || currentPartnerTypes?.length ? (
-            <SecuredField obj={partnership} name="types">
-              {(props) => (
-                <EnumField
-                  multiple
-                  label="Types"
-                  options={lookupPartnerTypes || currentPartnerTypes || []}
-                  layout="two-column"
-                  {...props}
-                />
-              )}
-            </SecuredField>
-          ) : null}
-          {hasManagingType(values.partnership?.types) ? (
-            <SecuredField obj={partnership} name="financialReportingType">
-              {(props) => (
-                <EnumField
-                  label="Financial Reporting Type"
-                  options={lookupPartnerFinType || currentPartnerFinTypes || []}
-                  getLabel={displayFinancialReportingType}
-                  {...props}
-                />
-              )}
-            </SecuredField>
-          ) : null}
-          {partnership && (
-            <>
-              <SecuredField obj={partnership} name="agreementStatus">
+        return (
+          <>
+            <SubmitError />
+            {!partnership && <PartnerField name="partnerLookupItem" required />}
+            {lookupPartnerTypes?.length || currentPartnerTypes?.length ? (
+              <SecuredField obj={partnership} name="types">
                 {(props) => (
-                  <AgreementStatusField label="Agreement Status" {...props} />
+                  <EnumField
+                    multiple
+                    label="Types"
+                    options={lookupPartnerTypes || currentPartnerTypes || []}
+                    layout="two-column"
+                    {...props}
+                  />
                 )}
               </SecuredField>
-              <SecuredField obj={partnership} name="mouStatus">
+            ) : null}
+            {hasManagingType(values.partnership?.types) ? (
+              <SecuredField obj={partnership} name="financialReportingType">
                 {(props) => (
-                  <AgreementStatusField label="Mou Status" {...props} />
+                  <EnumField
+                    label="Financial Reporting Type"
+                    options={
+                      lookupPartnerFinType || currentPartnerFinTypes || []
+                    }
+                    getLabel={displayFinancialReportingType}
+                    {...props}
+                  />
                 )}
               </SecuredField>
-            </>
-          )}
-        </>
-      );
-    }}
-  </DialogForm>
-);
+            ) : null}
+            {partnership && (
+              <>
+                <SecuredField obj={partnership} name="agreementStatus">
+                  {(props) => (
+                    <AgreementStatusField label="Agreement Status" {...props} />
+                  )}
+                </SecuredField>
+                <SecuredField obj={partnership} name="mouStatus">
+                  {(props) => (
+                    <AgreementStatusField label="Mou Status" {...props} />
+                  )}
+                </SecuredField>
+              </>
+            )}
+          </>
+        );
+      }}
+    </DialogForm>
+  );
+};
 
 const AgreementStatusField = (
   props: Omit<EnumFieldProps<PartnershipAgreementStatus, false>, 'children'>
