8266b212fc1fef3ab59dd9220cabea8c5a18f6de
Carson Full <carson_full@tsco.org>
2020-10-29 18:56:25 -0500

    Split main & aux types just for DX

--- src/api/typeMap.codegen.ts
+++ src/api/typeMap.codegen.ts
@@ -1,5 +1,5 @@
-import { isObjectType } from 'graphql';
-import { sortBy } from 'lodash';
+import { GraphQLNamedType, isObjectType } from 'graphql';
+import { difference, sortBy } from 'lodash';
 import { OptionalKind, PropertySignatureStructure } from 'ts-morph';
 import { getSchemaTypes } from './codeGenUtil/gql.util';
 import { tsMorphPlugin } from './codeGenUtil/ts.util';
@@ -20,14 +20,32 @@ export const plugin = tsMorphPlugin(({ schema, file }) => {
       type !== schema.getSubscriptionType()
   );
 
+  const mainTypes = userDefinedTypes.filter(
+    (t) =>
+      !t.name.startsWith('Secured') &&
+      !t.name.startsWith('Create') &&
+      !t.name.startsWith('Update') &&
+      !t.name.endsWith('Output')
+  );
+  const mainTsType = file.addInterface({
+    name: 'GqlTypeMapMain',
+    isExported: true,
+    properties: getProperties(mainTypes),
+  });
+
+  const auxTypes = difference(userDefinedTypes, mainTypes);
   file.addInterface({
     name: 'GqlTypeMap',
     isExported: true,
-    properties: sortBy(userDefinedTypes, (type) => type.name).map(
-      (type): OptionalKind<PropertySignatureStructure> => ({
-        name: type.name,
-        type: `Types.${type.name}`,
-      })
-    ),
+    extends: [mainTsType.getName()],
+    properties: getProperties(auxTypes),
   });
 });
+
+const getProperties = (types: readonly GraphQLNamedType[]) =>
+  sortBy(types, (type) => type.name).map(
+    (type): OptionalKind<PropertySignatureStructure> => ({
+      name: type.name,
+      type: `Types.${type.name}`,
+    })
+  );
