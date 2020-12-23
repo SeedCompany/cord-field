346628a7bbffb844143282b5b8f432cfe4bb19d6
Carson Full <carson_full@tsco.org>
2020-12-22 22:40:54 -0600

    Create useNavigate which works with SSR

--- src/components/Routing/Navigate.tsx
+++ src/components/Routing/Navigate.tsx
@@ -1,24 +1,43 @@
+import type { To } from 'history';
 import * as React from 'react';
 import { createContext, ReactElement, useContext } from 'react';
-import type { NavigateProps } from 'react-router';
-import { Navigate as ClientNavigate } from 'react-router-dom';
+import type { NavigateFunction, NavigateProps } from 'react-router';
+import {
+  Navigate as ClientNavigate,
+  useNavigate as useClientNavigate,
+} from 'react-router-dom';
 
-const ServerNavigate = (props: NavigateProps) => {
+const useServerNavigate = (): NavigateFunction => {
   const serverLocation = useContext(ServerLocationContext);
   if (!serverLocation) {
     throw new Error('Server location context should be provided for SSR');
   }
-  if (typeof props.to === 'string') {
-    serverLocation.url = props.to;
-  } else {
-    throw new Error('Navigate with partial paths are not yet implemented');
-  }
+  return (
+    to: To | number,
+    _options?: { replace?: boolean; state?: unknown }
+  ) => {
+    if (typeof to === 'number') {
+      throw new Error('Navigate with delta is not supported with SSR');
+    }
+    if (typeof to !== 'string') {
+      throw new Error('Navigate with partial paths are not yet implemented');
+    }
+    serverLocation.url = to;
+  };
+};
+
+const ServerNavigate = ({ to, ...options }: NavigateProps) => {
+  const navigate = useServerNavigate();
+  navigate(to, options);
 
   return null;
 };
 
 export const Navigate =
   typeof window === 'undefined' ? ServerNavigate : ClientNavigate;
+
+export const useNavigate =
+  typeof window === 'undefined' ? useServerNavigate : useClientNavigate;
 
 interface ServerLocationContextType {
   url?: string;
