3224aa08ea41876f03b0ed3afd09398f1c73500a
Carson Full <carson_full@tsco.org>
2020-12-23 20:09:02 -0600

    Update number formatter to use client locale for ssr

--- src/components/Formatters/useNumberFormatter.tsx
+++ src/components/Formatters/useNumberFormatter.tsx
@@ -1,7 +1,8 @@
+import { useLocale } from '../../hooks';
 import { Nullable } from '../../util';
 
 export const useNumberFormatter = (options?: Intl.NumberFormatOptions) => {
-  const formatter = new Intl.NumberFormat(undefined, options);
+  const formatter = new Intl.NumberFormat(useLocale(), options);
   return (value: Nullable<number>) =>
     value != null ? formatter.format(value) : '';
 };
