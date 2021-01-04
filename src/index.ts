f6df5f263b37218935be55f2eea0a9f7eb4ada48
Carson Full <carson_full@tsco.org>
2021-01-04 13:44:19 -0600

    Suppress experimental warning for AbortController

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import express from 'express';
 import { app } from './server/server';
 
+// Suppress experimental warning for AbortController
+if (typeof AbortController !== 'undefined') {
+  // eslint-disable-next-line @typescript-eslint/unbound-method
+  const emitWarning = process.emitWarning;
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  process.emitWarning = () => {};
+  new AbortController();
+  process.emitWarning = emitWarning;
+}
+
 let currentApp = app;
 
 if (module.hot) {
