cb63b3e1f3d41e753b0b7af8fe4ff0dd07895ab1
Carson Full <carson_full@tsco.org>
2021-01-11 20:13:08 -0600

    Initial pass at getting tests working for new ssr

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,40 @@
+import { ApolloProvider } from '@apollo/client';
 import { render } from '@testing-library/react';
-import React from 'react';
+import { Request as ExpressRequest } from 'express';
+import { Request } from 'jest-express/lib/request';
+import React, { FC, useState } from 'react';
+import { HelmetProvider } from 'react-helmet-async';
+import { StaticRouter } from 'react-router-dom/server';
 import { App } from './App';
+import { Nest } from './components/Nest';
+import { ServerDataProvider } from './components/ServerData';
+import { RequestContext } from './hooks';
+import { createServerApolloClient } from './server/renderServerSideApp';
+
+const TestContext: FC<{ url: string }> = ({ url, children }) => {
+  // @ts-expect-error yes the type doesn't match we are faking it.
+  const req: ExpressRequest = new Request(url);
+  const [client] = useState(() => createServerApolloClient(req, {}));
+  return (
+    <Nest
+      elements={[
+        <HelmetProvider context={{}} children={<></>} />,
+        <ServerDataProvider value={{}} />,
+        <RequestContext.Provider value={req} children={<></>} />,
+        <StaticRouter location={req.originalUrl} />,
+        <ApolloProvider client={client} children={<></>} />,
+      ]}
+      children={children}
+    />
+  );
+};
 
 test('renders HOME', () => {
-  const { getByRole } = render(<App />);
+  const { getByRole } = render(
+    <TestContext url="/">
+      <App />
+    </TestContext>
+  );
   const spinner = getByRole('progressbar');
   expect(spinner).toBeInTheDocument();
 });
