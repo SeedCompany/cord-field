f0f62be3e097ef74a1a89743d8cf9c280edcad43
Carson Full <carson_full@tsco.org>
2021-03-30 15:12:41 -0500

    Wrap IconButton to provide loading style & error color

---
+++ src/components/IconButton/IconButton.tsx
@@ -0,0 +1,54 @@
+import {
+  fade,
+  makeStyles,
+  IconButton as MUIIconButton,
+  IconButtonProps as MUIIconButtonProps,
+} from '@material-ui/core';
+// eslint-disable-next-line no-restricted-imports
+import { Palette, PaletteColor } from '@material-ui/core/styles/createPalette';
+import { Skeleton } from '@material-ui/lab';
+// eslint-disable-next-line no-restricted-imports
+import { CSSProperties } from '@material-ui/styles';
+import clsx from 'clsx';
+import * as React from 'react';
+import { forwardRef } from 'react';
+import { Except } from 'type-fest';
+
+const colorStyle = (color: PaletteColor, palette: Palette): CSSProperties => ({
+  color: color.main,
+  // backgroundColor: color.main,
+  '&:hover': {
+    backgroundColor: fade(color.main, palette.action.hoverOpacity),
+    // Reset on touch devices, it doesn't add specificity
+    '@media (hover: none)': {
+      backgroundColor: 'transparent',
+    },
+  },
+});
+
+const useStyles = makeStyles(({ palette }) => ({
+  error: colorStyle(palette.error, palette),
+}));
+
+export type IconButtonProps = Except<MUIIconButtonProps, 'color'> & {
+  color?: MUIIconButtonProps['color'] | 'error';
+  loading?: boolean;
+};
+
+export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  function IconButton({ color, loading, ...props }, ref) {
+    const classes = useStyles();
+    const fab = (
+      <MUIIconButton
+        ref={ref}
+        {...props}
+        color={color !== 'error' ? color : undefined}
+        className={clsx(
+          color === 'error' ? classes.error : undefined,
+          props.className
+        )}
+      />
+    );
+    return loading ? <Skeleton variant="circle">{fab}</Skeleton> : fab;
+  }
+);
