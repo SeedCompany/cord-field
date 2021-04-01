a95c9d09f0db7d46b770c5d594bf9cbb0ebe916f
Carson Full <carson_full@tsco.org>
2021-04-01 15:09:20 -0500

    Actually fixes #775. Logic was backwards

--- src/scenes/Projects/Create/CreateProject.tsx
+++ src/scenes/Projects/Create/CreateProject.tsx
@@ -16,8 +16,7 @@ export const CreateProject = (props: Except<Props, 'onSubmit'>) => {
     update: addItemToList({
       listId: 'projects',
       outputToItem: (data) => data.createProject.project,
-      filter: (args: ProjectListQueryVariables) =>
-        args.input.filter?.pinned ?? false,
+      filter: (args: ProjectListQueryVariables) => !args.input.filter?.pinned,
     }),
   });
   const { enqueueSnackbar } = useSnackbar();
