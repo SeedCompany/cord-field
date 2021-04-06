b379a0f6af8fe8e17d6ef6e0096070b4a7fe2e9d
Carson Full <carson_full@tsco.org>
2021-04-06 17:25:41 -0500

    Merge order & sort into one query parameter

--- src/components/Sort/useSort.ts
+++ src/components/Sort/useSort.ts
@@ -1,19 +1,38 @@
+import { Order } from '../../api';
 import { makeQueryHandler, StringParam, withTransform } from '../../hooks';
+import { Nullable } from '../../util';
 import { SortValue } from './SortControl';
 
 export const useSort = <T>() => {
   const [value, onChange] = useSortHandler();
   // Adapt output to have generic for sort value
   return {
-    value: value as Partial<SortValue<T>>,
-    onChange: onChange as (next: Partial<SortValue<T>>) => void,
+    value: value.sort,
+    onChange: (next: Partial<SortValue<T>>) => {
+      onChange({
+        sort: next as SortValue<Record<string, any>>,
+      });
+    },
   };
 };
 
 const useSortHandler = makeQueryHandler({
-  sort: StringParam,
-  order: withTransform(StringParam, {
-    encode: (val, encoder) => encoder(val?.toLowerCase()),
-    decode: (val, decoder) => decoder(val)?.toUpperCase(),
+  sort: withTransform<
+    Partial<SortValue<Record<string, any>>>,
+    Nullable<string>
+  >(StringParam, {
+    encode: (value, encoder) =>
+      encoder(
+        value.sort ? (value.order === 'DESC' ? '-' : '') + value.sort : null
+      ),
+    decode: (raw, decoder) => {
+      const value = decoder(raw);
+      if (!value) {
+        return {};
+      }
+      const order: Order = value.startsWith('-') ? 'DESC' : 'ASC';
+      const sort = value.startsWith('-') ? value.slice(1) : value;
+      return { sort, order };
+    },
   }),
 });
