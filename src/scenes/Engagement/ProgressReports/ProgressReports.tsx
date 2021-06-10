b391884da4d01589720f317291d4e9129482e92a
Carson Full <carson_full@tsco.org>
2021-06-09 20:41:17 -0500

    Show API errors and fix loading state of progress reports

--- src/scenes/Engagement/ProgressReports/ProgressReports.tsx
+++ src/scenes/Engagement/ProgressReports/ProgressReports.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from '@apollo/client';
+import { Skeleton } from '@material-ui/lab';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Breadcrumb } from '../../../components/Breadcrumb';
+import { Error } from '../../../components/Error';
 import { PeriodicReportsList } from '../../../components/PeriodicReports';
 import { ProjectBreadcrumb } from '../../../components/ProjectBreadcrumb';
 import { Redacted } from '../../../components/Redacted';
@@ -9,39 +11,57 @@ import { ProgressReportsDocument } from './ProgressReports.generated';
 
 export const ProgressReports = () => {
   const { projectId = '', engagementId = '' } = useParams();
-  const { data } = useQuery(ProgressReportsDocument, {
+  const { data, error } = useQuery(ProgressReportsDocument, {
     variables: {
       projectId,
       engagementId,
     },
   });
-  if (data?.engagement.__typename !== 'LanguageEngagement') {
-    return null;
+
+  if (error) {
+    return (
+      <Error page error={error}>
+        {{
+          NotFound: 'Could not find project or engagement',
+          Default: 'Error loading progress reports',
+        }}
+      </Error>
+    );
   }
 
-  const language = data.engagement.language.value;
+  const engagement =
+    data?.engagement.__typename === 'LanguageEngagement'
+      ? data.engagement
+      : undefined;
+  const language = engagement?.language.value;
   const languageName = language?.name.value ?? language?.displayName.value;
 
   return (
     <PeriodicReportsList
       type="Progress"
-      pageTitleSuffix={data.project.name.value ?? 'A Project'}
+      pageTitleSuffix={data?.project.name.value ?? 'A Project'}
       breadcrumbs={[
-        <ProjectBreadcrumb data={data.project} />,
-        languageName ? (
-          <Breadcrumb
-            to={`/projects/${data.project.id}/engagements/${data.engagement.id}`}
-          >
-            {languageName}
-          </Breadcrumb>
-        ) : (
-          <Redacted
-            info="You do not have permission to view this engagement's name"
-            width={200}
-          />
-        ),
+        <ProjectBreadcrumb data={data?.project} />,
+        <Breadcrumb
+          to={
+            data
+              ? `/projects/${data.project.id}/engagements/${data.engagement.id}`
+              : '.'
+          }
+        >
+          {!data ? (
+            <Skeleton width={200} />
+          ) : languageName ? (
+            languageName
+          ) : (
+            <Redacted
+              info="You do not have permission to view this engagement's name"
+              width={200}
+            />
+          )}
+        </Breadcrumb>,
       ]}
-      reports={data.engagement.progressReports.items}
+      reports={engagement?.progressReports.items}
     />
   );
 };
--- src/scenes/Projects/Reports/ProjectReports.tsx
+++ src/scenes/Projects/Reports/ProjectReports.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@apollo/client';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { ReportType } from '../../../api';
+import { Error } from '../../../components/Error';
 import { PeriodicReportsList } from '../../../components/PeriodicReports/PeriodicReportsList';
 import { ProjectBreadcrumb } from '../../../components/ProjectBreadcrumb';
 import {
@@ -11,12 +12,23 @@ import {
 
 export const ProjectReports = ({ type }: { type: ReportType }) => {
   const { projectId = '' } = useParams();
-  const { data } = useQuery(
+  const { data, error } = useQuery(
     type === 'Financial' ? FinancialReportsDocument : NarrativeReportsDocument,
     {
       variables: { projectId },
     }
   );
+
+  if (error) {
+    return (
+      <Error page error={error}>
+        {{
+          NotFound: 'Could not find project or engagement',
+          Default: 'Error loading progress reports',
+        }}
+      </Error>
+    );
+  }
 
   return (
     <PeriodicReportsList
