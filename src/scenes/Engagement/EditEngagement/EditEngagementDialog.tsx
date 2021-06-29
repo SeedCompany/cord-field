SeedCompany/cord-field

commit a189ef9e3a5466a7a28e727a9f0dc225e7d8d577

Replace start/end date pairs with new date range fields

src/scenes/Engagement/EditEngagement/EditEngagementDialog.tsx
@@ -44,8 +44,7 @@ export type EditableEngagementField = ExtractStrict<
   | SecuredEditableKeys<LanguageEngagement>
   | SecuredEditableKeys<InternshipEngagement>,
   // Add more fields here as needed
-  | 'startDateOverride'
-  | 'endDateOverride'
+  | 'dateRangeOverride'
   | 'completeDate'
   | 'disbursementCompleteDate'
   | 'methodologies'
@@ -68,19 +67,21 @@ const fieldMapping: Record<
   EditableEngagementField,
   ComponentType<EngagementFieldProps>
 > = {
-  startDateOverride: ({ props }) => (
-    <DateField
-      {...props}
-      label="Start Date"
-      helperText="Leave blank to use project's start date"
-    />
-  ),
-  endDateOverride: ({ props }) => (
-    <DateField
-      {...props}
-      label="End Date"
-      helperText="Leave blank to use project's end date"
-    />
+  dateRangeOverride: ({ props }) => (
+    <>
+      <DateField
+        {...props}
+        name="startDateOverride"
+        label="Start Date"
+        helperText="Leave blank to use project's start date"
+      />
+      <DateField
+        {...props}
+        name="endDateOverride"
+        label="End Date"
+        helperText="Leave blank to use project's end date"
+      />
+    </>
   ),
   completeDate: ({ props, engagement }) => (
     <DateField
@@ -199,8 +200,8 @@ export const EditEngagementDialog: FC<EditEngagementDialogProps> = ({
       EngagementFormValues['engagement'],
       'id'
     > = {
-      startDateOverride: engagement.startDateOverride.value,
-      endDateOverride: engagement.endDateOverride.value,
+      startDateOverride: engagement.dateRangeOverride.value.start,
+      endDateOverride: engagement.dateRangeOverride.value.end,
       completeDate: engagement.completeDate.value,
       disbursementCompleteDate: engagement.disbursementCompleteDate.value,
       ...(engagement.__typename === 'LanguageEngagement'

src/scenes/Projects/Update/UpdateProjectDialog.tsx
@@ -31,11 +31,10 @@ import { ProjectOverviewFragment } from '../Overview/ProjectOverview.generated';
 import { UpdateProjectDocument } from './UpdateProject.generated';
 
 export type EditableProjectField = ExtractStrict<
-  keyof UpdateProject,
+  keyof UpdateProject | 'mouRange',
   // Add more fields here as needed
   | 'name'
-  | 'mouStart'
-  | 'mouEnd'
+  | 'mouRange'
   | 'estimatedSubmission'
   | 'fieldRegionId'
   | 'primaryLocationId'
@@ -60,8 +59,12 @@ const fieldMapping: Record<
   fieldRegionId: ({ props }) => (
     <FieldRegionField {...props} label="Field Region" />
   ),
-  mouStart: ({ props }) => <DateField {...props} label="Start Date" />,
-  mouEnd: ({ props }) => <DateField {...props} label="End Date" />,
+  mouRange: ({ props }) => (
+    <>
+      <DateField {...props} name="mouStart" label="Start Date" />
+      <DateField {...props} name="mouEnd" label="End Date" />
+    </>
+  ),
   estimatedSubmission: ({ props }) => (
     <DateField {...props} label="Estimated Submission Date" />
   ),
@@ -109,8 +112,8 @@ export const UpdateProjectDialog = ({
       name: project.name.value,
       primaryLocationId: project.primaryLocation.value,
       fieldRegionId: project.fieldRegion.value,
-      mouStart: project.mouStart.value,
-      mouEnd: project.mouEnd.value,
+      mouStart: project.mouRange.value.start,
+      mouEnd: project.mouRange.value.end,
       estimatedSubmission: project.estimatedSubmission.value,
       sensitivity: project.sensitivity,
     };
@@ -131,8 +134,8 @@ export const UpdateProjectDialog = ({
     project.name.value,
     project.primaryLocation.value,
     project.fieldRegion.value,
-    project.mouStart.value,
-    project.mouEnd.value,
+    project.mouRange.value.start,
+    project.mouRange.value.end,
     project.estimatedSubmission.value,
     project.sensitivity,
     project.id,

src/api/dateRange.ts
@@ -1,30 +1,0 @@
-import { DateTime, Interval } from 'luxon';
-import { Merge } from 'type-fest';
-import { CalendarDate, Nullable } from '../util';
-import { SecuredProp } from './secured';
-
-interface DateRange<T> {
-  start: Nullable<T>;
-  end: Nullable<T>;
-}
-
-type DateInterval<T extends DateTime> = Merge<
-  Interval,
-  Record<'start' | 'end', T>
->;
-
-const securedRange =
-  <T extends DateTime>() =>
-  (start: SecuredProp<T>, end: SecuredProp<T>): SecuredProp<DateRange<T>> => ({
-    canRead: start.canRead && end.canRead,
-    canEdit: start.canEdit && end.canEdit,
-    value:
-      start.value && end.value
-        ? (Interval.fromDateTimes(start.value, end.value) as DateInterval<T>)
-        : !start.value && !end.value
-        ? null
-        : { start: start.value, end: end.value },
-  });
-
-export const securedDateRange = securedRange<CalendarDate>();
-export const securedDateTimeRange = securedRange<DateTime>();

src/api/fragments/secured.graphql
@@ -21,3 +21,12 @@ fragment securedBool on SecuredBoolean {
   canEdit
   value
 }
+
+fragment securedDateRange on SecuredDateRange {
+  canRead
+  canEdit
+  value {
+    start
+    end
+  }
+}

src/api/index.ts
@@ -5,7 +5,6 @@ export * from './operations.generated';
 export * from './networkStatus';
 export * from './displayEnums';
 export * from './enumLists';
-export * from './dateRange';
 export * from './secured';
 export * from './approach';
 export * from './list-caching';

src/api/typePolicies/typePolicies.base.ts
@@ -31,6 +31,10 @@ type TypePolicies = {
 const scriptureKeyFields = ['book', 'chapter', 'verse'] as const;
 
 export const typePolicies: TypePolicies = {
+  DateRange: {
+    keyFields: false,
+    merge: true,
+  },
   Language: {
     fields: {
       ethnologue: { merge: true },

src/components/FieldOverviewCard/FieldOverviewCard.tsx
@@ -13,7 +13,7 @@ import { Skeleton } from '@material-ui/lab';
 import clsx from 'clsx';
 import { To } from 'history';
 import { DateTime } from 'luxon';
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import * as React from 'react';
 import { useDateTimeFormatter } from '../Formatters';
 import { HugeIcon, HugeIconProps } from '../Icons';
@@ -51,7 +51,7 @@ const useStyles = makeStyles(({ spacing, palette }) => ({
 }));
 
 interface FieldOverviewCardData {
-  value?: string;
+  value?: ReactNode;
   updatedAt?: DateTime;
   to?: To;
 }

src/components/Formatters/FormattedDate.tsx
@@ -1,6 +1,8 @@
 import { Tooltip } from '@material-ui/core';
 import { DateTime, DateTimeFormatOptions } from 'luxon';
 import * as React from 'react';
+import { MergeExclusive } from 'type-fest';
+import { DateRange } from '../../api';
 import { CalendarDate, Nullable } from '../../util';
 import { useDateFormatter, useDateTimeFormatter } from './useDateFormatter';
 
@@ -22,10 +24,18 @@ export const FormattedDate = ({
 export const FormattedDateRange = ({
   start,
   end,
-}: {
-  start: Nullable<CalendarDate>;
-  end: Nullable<CalendarDate>;
-}) => {
+  range,
+}: MergeExclusive<
+  {
+    start: Nullable<CalendarDate>;
+    end: Nullable<CalendarDate>;
+  },
+  { range?: DateRange }
+>) => {
+  if (range) {
+    start = range.start;
+    end = range.end;
+  }
   if (!start && !end) {
     return null;
   }

commit 63b7081f613722440b1b0d123baffbced462a2fc

Fix date range initial values
Regressed with a189ef9e3a5466a7a28e727a9f0dc225e7d8d577

src/scenes/Engagement/EditEngagement/EditEngagementDialog.tsx
@@ -221,7 +221,11 @@ export const EditEngagementDialog: FC<EditEngagementDialogProps> = ({
     // Filter out irrelevant initial values so they don't get added to the mutation
     const filteredInitialValuesFields = pick(
       fullInitialValuesFields,
-      editFields
+      editFields.flatMap((field) =>
+        field === 'dateRangeOverride'
+          ? ['startDateOverride', 'endDateOverride']
+          : field
+      )
     );
 
     return {

src/scenes/Projects/Update/UpdateProjectDialog.tsx
@@ -121,7 +121,9 @@ export const UpdateProjectDialog = ({
     // Filter out irrelevant initial values so they don't get added to the mutation
     const filteredInitialValuesFields = pick(
       fullInitialValuesFields,
-      editFields
+      editFields.flatMap((field) =>
+        field === 'mouRange' ? ['mouStart', 'mouEnd'] : field
+      )
     );
 
     return {