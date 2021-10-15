67185dc05154217de2e11b88eb469e346e361a44
Carson Full <carson_full@tsco.org>
2021-10-15 10:54:26 -0500

    Ask for methodology on planning pnp upload

--- src/scenes/Engagement/LanguageEngagement/ProgressAndPlanning/ProgressAndPlanning.tsx
+++ src/scenes/Engagement/LanguageEngagement/ProgressAndPlanning/ProgressAndPlanning.tsx
@@ -1,9 +1,18 @@
-import { Tooltip } from '@material-ui/core';
+import { useMutation } from '@apollo/client';
+import { Tooltip, Typography } from '@material-ui/core';
 import React from 'react';
+import {
+  displayMethodologyWithLabel,
+  ProductMethodology as Methodology,
+} from '../../../../api';
 import { DefinedFileCard } from '../../../../components/DefinedFileCard';
+import { useDialog } from '../../../../components/Dialog';
+import { DialogForm } from '../../../../components/Dialog/DialogForm';
 import { FileActionsContextProvider } from '../../../../components/files/FileActions';
+import { HandleUploadCompletedFunction } from '../../../../components/files/hooks';
+import { EnumField } from '../../../../components/form';
 import { PeriodicReportCard } from '../../../../components/PeriodicReports';
-import { UploadLanguageEngagementPnpDocument } from '../../Files';
+import { UploadLanguageEngagementPnpDocument as UploadPnp } from '../../Files';
 import { ProgressAndPlanningFragment } from './ProgressAndPlanning.generated';
 
 interface Props {
@@ -21,17 +30,61 @@ export const ProgressReports = ({ engagement }: Props) => (
   </FileActionsContextProvider>
 );
 
-export const PlanningSpreadsheet = ({ engagement }: Props) => (
-  <FileActionsContextProvider>
-    <Tooltip title="This holds the planning info of PnP files" placement="top">
-      <DefinedFileCard
-        label="Planning Spreadsheet"
-        uploadMutationDocument={UploadLanguageEngagementPnpDocument}
-        parentId={engagement.id}
-        resourceType="engagement"
-        securedFile={engagement.pnp}
-        disableIcon
-      />
-    </Tooltip>
-  </FileActionsContextProvider>
-);
+export const PlanningSpreadsheet = ({ engagement }: Props) => {
+  const [dialogState, setUploading, upload] =
+    // Functions cannot be passed directly here so wrap in object
+    useDialog<{ submit: (next: HandleUploadCompletedFunction) => void }>();
+  const [updateEngagement] = useMutation(UploadPnp);
+
+  return (
+    <FileActionsContextProvider>
+      <Tooltip
+        title="This holds the planning info of PnP files"
+        placement="top"
+      >
+        <DefinedFileCard
+          label="Planning Spreadsheet"
+          uploadMutationDocument={UploadPnp}
+          parentId={engagement.id}
+          resourceType="engagement"
+          securedFile={engagement.pnp}
+          disableIcon
+          onUpload={setUploading}
+        />
+      </Tooltip>
+      {upload && (
+        <DialogForm<{ methodology?: Methodology }>
+          {...dialogState}
+          onSubmit={async ({ methodology }) => {
+            upload.submit(async ({ uploadId, name }) => {
+              await updateEngagement({
+                variables: {
+                  id: engagement.id,
+                  upload: { uploadId, name },
+                  methodology,
+                },
+              });
+            });
+          }}
+          title="Extract Goals?"
+          submitLabel="Upload"
+        >
+          <Typography>
+            CORD can create goals from this spreadsheet for you. We just need to
+            know which methodology the new goals should have.
+          </Typography>
+          <EnumField
+            name="methodology"
+            options={MethodologyOptions}
+            getLabel={displayMethodologyWithLabel}
+            layout="column"
+            defaultOption="Skip extracting goals"
+          />
+        </DialogForm>
+      )}
+    </FileActionsContextProvider>
+  );
+};
+
+// Only methodologies that have available steps matching pnp columns
+const MethodologyOptions: Methodology[] = ['Paratext', 'OtherWritten'];
