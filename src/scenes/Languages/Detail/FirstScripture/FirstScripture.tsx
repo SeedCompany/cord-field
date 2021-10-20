866d2ad2d0e48b6adc4fe7b5c5b286ab477f56f2
Daniel Leo <danielld707@gmail.com>
2021-10-20 11:47:19 -0400

    Refactor firstScripture field

--- src/scenes/Languages/Detail/FirstScripture/FirstScripture.tsx
+++ src/scenes/Languages/Detail/FirstScripture/FirstScripture.tsx
@@ -6,15 +6,11 @@ import {
 } from '@material-ui/icons';
 import { Skeleton } from '@material-ui/lab';
 import * as React from 'react';
-import { assert } from 'ts-essentials';
+import { IdFragment } from '../../../../api';
 import { Redacted } from '../../../../components/Redacted';
 import { Link } from '../../../../components/Routing';
-import { isTypename } from '../../../../util';
 import { getProjectUrl } from '../../../Projects/useProjectId';
-import {
-  LanguageProjectEngagement_LanguageEngagement_Fragment as Engagement,
-  FirstScriptureFragment,
-} from './FirstScripture.generated';
+import { FirstScriptureFragment } from './FirstScripture.generated';
 
 const useStyles = makeStyles(({ spacing }) => ({
   root: {
@@ -32,10 +28,6 @@ const useStyles = makeStyles(({ spacing }) => ({
 export const FirstScripture = ({ data }: { data?: FirstScriptureFragment }) => {
   const classes = useStyles();
 
-  const engagements = data?.projects.items
-    .flatMap((project) => project.engagements.items)
-    .filter(isTypename<Engagement>('LanguageEngagement'));
-
   if (!data) {
     return (
       <div className={classes.root}>
@@ -49,13 +41,8 @@ export const FirstScripture = ({ data }: { data?: FirstScriptureFragment }) => {
       </div>
     );
   }
-  assert(engagements);
 
-  if (
-    !data.projects.canRead ||
-    !data.hasExternalFirstScripture.canRead ||
-    engagements.some((e) => !e.firstScripture.canRead || !e.language.canRead)
-  ) {
+  if (!data.firstScripture.canRead) {
     return (
       <div className={classes.root}>
         <IndeterminateCheckBox className={classes.icon} />
@@ -78,24 +65,22 @@ export const FirstScripture = ({ data }: { data?: FirstScriptureFragment }) => {
     );
   }
 
-  // If the API is working properly, there should be a maximum of 1
-  const engagement = engagements.find(
-    (engagement) =>
-      engagement.firstScripture.value &&
-      engagement.language.value!.id === data.id
-  );
-  if (engagement) {
-    const project = data.projects.items.find((p) =>
-      p.engagements.items.some((e) => e.id === engagement.id)
-    )!;
+  const engagement =
+    data.firstScripture.value?.__typename === 'InternalFirstScripture'
+      ? data.firstScripture.value.engagement
+      : null;
 
+  if (engagement) {
+    const project = engagement.project;
     return (
       <div className={classes.root}>
         <Check color="primary" className={classes.icon} />
         <Typography className={classes.text}>
           First Scripture:&nbsp;&nbsp;
           <Link
-            to={`${getProjectUrl(project)}/engagements/${engagement.id}`}
+            to={`${getProjectUrl(project as IdFragment)}/engagements/${
+              engagement.id
+            }`}
             underline={project.name.canRead ? undefined : 'none'}
           >
             {project.name.canRead ? (
