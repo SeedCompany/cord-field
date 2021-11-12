8d8037f4564999903806987ab1a76f2824b2a2bc
Carson Full <carson_full@tsco.org>
2021-11-12 11:58:11 -0600

    Format location isoAlpha3 as uppercase in form since that's how the API formats it

--- src/components/form/AlphaLowercaseField.tsx
+++ src/components/form/AlphaLowercaseField.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import { Except } from 'type-fest';
 import {
   FormattedTextField,
   FormattedTextFieldProps,
 } from './FormattedTextField';
 import { isLength } from './validators';
 
-interface AlphaLowercaseFieldProps
-  extends Except<
-    FormattedTextFieldProps,
-    'accept' | 'formatInput' | 'replace' | 'validate'
-  > {
+interface AlphaFieldProps extends FormattedTextFieldProps {
   chars: number;
 }
 
-// A lower-cased alpha string field
-export const AlphaLowercaseField = ({
-  chars,
-  ...props
-}: AlphaLowercaseFieldProps) => (
+export const AlphaField = ({ chars, ...props }: AlphaFieldProps) => (
   <FormattedTextField
     accept={/[a-zA-Z]/g}
     formatInput={(value) =>
       (value.match(/[a-zA-Z]+/g) || []).join('').substr(0, chars)
     }
-    replace={(value) => value.toLowerCase()}
     validate={isLength(chars)}
     {...props}
   />
+);
+
+export const AlphaLowercaseField = (props: AlphaFieldProps) => (
+  <AlphaField replace={(value) => value.toLowerCase()} {...props} />
+);
+
+export const AlphaUppercaseField = (props: AlphaFieldProps) => (
+  <AlphaField replace={(value) => value.toUpperCase()} {...props} />
 );
