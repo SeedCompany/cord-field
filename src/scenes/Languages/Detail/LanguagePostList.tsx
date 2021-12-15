e7ad4bed2791fd970829164a031aa15d6487ec88
Carson Full <carson_full@tsco.org>
2021-12-14 18:18:21 -0600

    Use post abstraction & move query to separate file

--- src/scenes/Languages/Detail/LanguagePostList.tsx
+++ src/scenes/Languages/Detail/LanguagePostList.tsx
@@ -1,69 +1,20 @@
-import { Grid, makeStyles, Tooltip, Typography } from '@material-ui/core';
-import { Add } from '@material-ui/icons';
-import { FC } from 'react';
 import * as React from 'react';
-import { useDialog } from '../../../components/Dialog';
-import { Fab } from '../../../components/Fab';
-import { List, useListQuery } from '../../../components/List';
-import { CreatePost } from '../../../components/posts/CreatePost';
-import { PostListItemCard } from '../../../components/posts/PostListItemCard';
-import {
-  LanguageQuery,
-  LanguagePostListOverviewDocument as PostList,
-} from './LanguageDetail.generated';
+import { useListQuery } from '../../../components/List';
+import { PostableIdFragment } from '../../../components/posts/PostableId.generated';
+import { PostList } from '../../../components/posts/PostList';
+import { LanguagePostListDocument as LanguagePosts } from './LanguagePostList.generated';
 
 interface LanguagePostListProps {
-  language: LanguageQuery['language'];
+  language: PostableIdFragment;
 }
 
-const useStyles = makeStyles(({ spacing }) => ({
-  postList: {
-    marginTop: spacing(1),
-    marginLeft: spacing(1.5),
-    maxWidth: 500,
-  },
-  postListItems: {
-    maxWidth: 600,
-  },
-}));
-
-export const LanguagePostList: FC<LanguagePostListProps> = ({ language }) => {
-  const classes = useStyles();
-
-  const [createPostState, createPost] = useDialog();
-
-  const posts = useListQuery(PostList, {
+export const LanguagePostList = ({ language }: LanguagePostListProps) => {
+  const posts = useListQuery(LanguagePosts, {
     listAt: (data) => data.language.posts,
     variables: {
       language: language.id,
     },
   });
 
-  return (
-    <div className={classes.postList}>
-      <Grid container spacing={2} alignItems="center">
-        <Grid item>
-          <Typography variant="h3">Posts</Typography>
-        </Grid>
-        <Grid item>
-          <Tooltip title="Add Post">
-            <Fab color="error" onClick={createPost}>
-              <Add />
-            </Fab>
-          </Tooltip>
-        </Grid>
-      </Grid>
-      <List
-        {...posts}
-        classes={{ container: classes.postListItems }}
-        spacing={3}
-        renderItem={(post) => (
-          <PostListItemCard parent={language} post={post} />
-        )}
-        renderSkeleton={null}
-        skeletonCount={0}
-      />
-      <CreatePost {...createPostState} parent={language} />
-    </div>
-  );
+  return <PostList parent={language} {...posts} />;
 };
