4de2a7af7ea863cdf99f9ee442c2cb550c63cd7e
Carson Full <carson_full@tsco.org>
2021-12-16 18:39:25 -0600

    Add skipped prefix to tooltip for clarity

--- src/components/PeriodicReports/PeriodicReportRow.tsx
+++ src/components/PeriodicReports/PeriodicReportRow.tsx
@@ -4,9 +4,10 @@ import { PaperTooltip } from '../PaperTooltip';
 import { ReportRow } from './PeriodicReportsTable';
 
 export const PeriodicReportRow = (props: { data: ReportRow }) => {
+  const skipped = props.data.report.skippedReason.value;
   return (
     <PaperTooltip
-      title={props.data.report.skippedReason.value ?? ''}
+      title={skipped ? <>Skipped&mdash;{skipped}</> : ''}
       placement="bottom-start"
       arrow={false}
     >
