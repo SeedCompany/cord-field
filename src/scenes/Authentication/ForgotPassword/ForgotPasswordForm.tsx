24f02f4deb27cccbfb2f24936bcab6008c18e35c
Carson Full <carson_full@tsco.org>
2022-02-08 13:30:10 -0600

    Limit back link widths on auth scenes to help prevent misclicks

--- src/scenes/Authentication/ForgotPassword/ForgotPasswordForm.tsx
+++ src/scenes/Authentication/ForgotPassword/ForgotPasswordForm.tsx
@@ -19,7 +19,7 @@ const useStyles = makeStyles(({ spacing }) => ({
     marginTop: spacing(1),
   },
   loginLink: {
-    display: 'block',
+    display: 'inline-block',
     marginTop: spacing(1),
   },
 }));
