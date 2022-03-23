ba5fcfed7e241b43465b323bb3f663d979cb7070
Carson Full <carson_full@tsco.org>
2022-03-23 18:41:42 -0500

    Restrict ~ alias usage to only other parents
    This balances import flexibility by allowing
    - unrelated app code from absolute path
    - while still showing the relation of related sibling/cousin files
      And sorting those below absolute paths

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,48 @@
 // @ts-check
+// eslint-disable-next-line @typescript-eslint/no-require-imports,@typescript-eslint/no-var-requires
+const fs = require('fs');
+
+const roots = fs
+  .readdirSync('./src')
+  .flatMap((item) => (fs.statSync(`./src/${item}`).isDirectory() ? item : []));
+
+const restrictedImports = {
+  paths: [
+    // As noted in https://github.com/mui-org/material-ui/releases/tag/v4.5.1
+    {
+      name: '@material-ui/styles',
+      message: "Use '@material-ui/core' instead",
+    },
+    // Enforce SVG Icons as recommended
+    {
+      name: '@material-ui/core',
+      importNames: ['Icon'],
+      message: "Use specific SVG Icons from '@material-ui/icons' instead",
+    },
+    // Enforce our Link component
+    {
+      name: '@material-ui/core',
+      importNames: ['Link'],
+      message:
+        'Use Link in components/Routing instead which has routing integrated',
+    },
+    {
+      name: 'camel-case',
+      message: 'Use camelCase from lodash instead.',
+    },
+    {
+      name: 'is-plain-object',
+      message: 'Use isPlainObject from lodash instead.',
+    },
+  ],
+  patterns: [
+    // Enforce single import. Leave code splitting to webpack.
+    ...['@material-ui/core', '@material-ui/icons', 'lodash'].map((item) => ({
+      group: [`${item}/*`],
+      message: `\nImport from '${item}' instead. Leave code splitting to webpack.\n`,
+    })),
+  ],
+};
 
 /** @type {import('eslint').Linter.Config} */
 const config = {
@@ -6,49 +50,36 @@ const config = {
   plugins: ['@seedcompany'],
   extends: ['plugin:@seedcompany/react'],
   rules: {
-    'no-restricted-imports': [
-      'error',
-      {
-        paths: [
-          // As noted in https://github.com/mui-org/material-ui/releases/tag/v4.5.1
-          {
-            name: '@material-ui/styles',
-            message: "Use '@material-ui/core' instead",
-          },
-          // Enforce SVG Icons as recommended
-          {
-            name: '@material-ui/core',
-            importNames: ['Icon'],
-            message: "Use specific SVG Icons from '@material-ui/icons' instead",
-          },
-          // Enforce our Link component
-          {
-            name: '@material-ui/core',
-            importNames: ['Link'],
-            message:
-              'Use Link in components/Routing instead which has routing integrated',
-          },
-          {
-            name: 'camel-case',
-            message: 'Use camelCase from lodash instead.',
-          },
-          {
-            name: 'is-plain-object',
-            message: 'Use isPlainObject from lodash instead.',
-          },
-        ],
-        patterns: [
-          // Enforce single import. Leave code splitting to webpack.
-          '@material-ui/core/*',
-          '@material-ui/icons/*',
-          'lodash/*',
-        ],
-      },
-    ],
+    'no-restricted-imports': ['error', restrictedImports],
 
     // TODO This needs to be turned on and errors fixed
     '@typescript-eslint/restrict-template-expressions': 'off',
   },
+  overrides: [
+    ...roots.map((root) => {
+      /** @type {import('eslint').Linter.ConfigOverride} */
+      const override = {
+        files: [`./src/${root}/**/*`],
+        rules: {
+          'no-restricted-imports': [
+            'error',
+            {
+              ...restrictedImports,
+              patterns: [
+                ...restrictedImports.patterns,
+                {
+                  group: [`~/${root}`],
+                  message:
+                    '\nUse a relative import instead for imports within your sub-folder\n',
+                },
+              ],
+            },
+          ],
+        },
+      };
+      return override;
+    }),
+  ],
 };
 
 module.exports = config;
