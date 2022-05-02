b2f67212b8b91883dad02f171a9bc14dce6c69b5
Carson Full <carson_full@tsco.org>
2022-05-02 18:33:22 -0500

    Add more info for number formatter failure

--- src/components/Formatters/useNumberFormatter.tsx
+++ src/components/Formatters/useNumberFormatter.tsx
@@ -1,14 +1,19 @@
-import { useMemo } from 'react';
-import { useLocale } from '../../hooks';
+import { useContext, useMemo } from 'react';
+import { RequestContext, useLocale } from '../../hooks';
 import { Nullable } from '../../util';
 
 export const useNumberFormatter = (options?: Intl.NumberFormatOptions) => {
+  const req = useContext(RequestContext);
   const locale = useLocale();
   const formatter = useMemo(() => {
     try {
       return new Intl.NumberFormat(locale, options);
     } catch (e) {
-      console.warn('Failed to create number formatter with locale', locale);
+      console.warn('Failed to create number formatter with locale', {
+        locale,
+        ua: req?.headers['user-agent'] ?? navigator.userAgent,
+        ssr: typeof window === 'undefined',
+      });
     }
     try {
       return new Intl.NumberFormat(undefined, options);
@@ -18,7 +23,7 @@ export const useNumberFormatter = (options?: Intl.NumberFormatOptions) => {
     return {
       format: (value: number): string => `${value}`,
     };
-  }, [locale, options]);
+  }, [req, locale, options]);
   return (value: Nullable<number>) =>
     value != null ? formatter.format(value) : '';
 };
