0c94a8de026a2fc6626fa0a85e50cb4fcaec73e3
Carson Full <carson_full@tsco.org>
2022-05-03 10:12:53 -0500

    Validate locale from request header with Intl API before using in app
    Rollback logging & error handling for Intl.NumberFormat as it should be fine now.

--- src/components/Formatters/useNumberFormatter.tsx
+++ src/components/Formatters/useNumberFormatter.tsx
@@ -1,30 +1,13 @@
-import { useContext, useMemo } from 'react';
-import { RequestContext, useLocale } from '../../hooks';
+import { useMemo } from 'react';
+import { useLocale } from '../../hooks';
 import { Nullable } from '../../util';
 
 export const useNumberFormatter = (options?: Intl.NumberFormatOptions) => {
-  const req = useContext(RequestContext);
   const locale = useLocale();
-  const formatter = useMemo(() => {
-    const normalized = locale === '*' ? undefined : locale;
-    try {
-      return new Intl.NumberFormat(normalized, options);
-    } catch (e) {
-      console.warn('Failed to create number formatter with locale', {
-        locale: normalized,
-        ua: req?.headers['user-agent'] ?? navigator.userAgent,
-        ssr: typeof window === 'undefined',
-      });
-    }
-    try {
-      return new Intl.NumberFormat(undefined, options);
-    } catch (e) {
-      console.warn('Failed to create number formatter with default locale');
-    }
-    return {
-      format: (value: number): string => `${value}`,
-    };
-  }, [req, locale, options]);
+  const formatter = useMemo(
+    () => new Intl.NumberFormat(locale, options),
+    [locale, options]
+  );
   return (value: Nullable<number>) =>
     value != null ? formatter.format(value) : '';
 };
--- src/hooks/useRequest.tsx
+++ src/hooks/useRequest.tsx
@@ -1,10 +1,20 @@
 import { Request } from 'express';
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useMemo } from 'react';
 
 export const RequestContext = createContext<Request | undefined>(undefined);
 
-export const useLocale = (): string => {
+export const useLocale = (): string | undefined => {
   const req = useContext(RequestContext);
-  const locale = req?.acceptsLanguages()[0] ?? navigator.language;
-  return locale.replace(/_/g, '-');
+  return useMemo(() => {
+    if (!req) {
+      return navigator.language;
+    }
+    try {
+      const accept = req.acceptsLanguages()[0]?.replace(/_/g, '-');
+      new Intl.Locale(accept); // check if valid
+      return accept;
+    } catch {
+      return undefined;
+    }
+  }, [req]);
 };
