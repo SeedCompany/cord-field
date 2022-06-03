f3622851db432f063866db7d8f72ad0a43f1b85f
Elisabeth Shakespeare <shakespearee04@gmail.com>
2022-06-03 12:11:42 -0500

    add helperText when the user chooses a language to provide the rod and ethcode of the language selected.

--- src/scenes/Engagement/LanguageEngagement/Create/CreateLanguageEngagement.tsx
+++ src/scenes/Engagement/LanguageEngagement/Create/CreateLanguageEngagement.tsx
@@ -4,7 +4,9 @@ import {
   ListItem,
   ListItemText,
   ListSubheader,
+  makeStyles,
   Paper,
+  Typography,
 } from '@material-ui/core';
 import React from 'react';
 import { Except } from 'type-fest';
@@ -25,6 +27,17 @@ import {
 import { callAll } from '../../../../util';
 import { CreateLanguageEngagementDocument } from './CreateLanguageEngagement.graphql';
 import { recalculateSensitivity } from './recalculateSensitivity';
+
+const useStyles = makeStyles(({ palette, spacing }) => ({
+  helperTextKey: {
+    marginRight: spacing(1),
+    color: palette.text.secondary,
+  },
+  helperTextValue: {
+    marginRight: spacing(3),
+    color: palette.text.secondary,
+  },
+}));
 
 interface CreateLanguageEngagementFormValues {
   engagement: {
@@ -49,6 +62,7 @@ export const CreateLanguageEngagement = ({
   engagedIds,
   ...props
 }: CreateLanguageEngagementProps) => {
+  const classes = useStyles();
   const [createEngagement] = useMutation(CreateLanguageEngagementDocument);
   const submit = async ({ engagement }: CreateLanguageEngagementFormValues) => {
     const languageRef = {
@@ -92,42 +106,87 @@ export const CreateLanguageEngagement = ({
       title="Create Language Engagement"
       changesetAware
     >
-      <SubmitError />
-      <LanguageField
-        name="engagement.languageId"
-        label="Language"
-        required
-        // wrap the options in a List with a subheader
-        PaperComponent={({ children }) => {
-          return (
-            <Paper>
-              <List>
-                <ListSubheader>
-                  <ListItem divider dense>
-                    <ListItemText secondary="NAME" />
-                    <ListItemText secondary="ETH" />
-                    <ListItemText secondary="ROD" />
-                  </ListItem>
-                </ListSubheader>
-                {children}
-              </List>
-            </Paper>
-          );
-        }}
-        sortOptionComparator={sortByWhetherAnEngagedId}
-        getOptionDisabled={(lang) => engagedIds?.includes(lang.id) ?? true}
-        renderOption={(option) => (
-          <ListItem dense>
-            <ListItemText
-              primary={option.name.value ?? option.displayName.value}
+      {({ values }) => {
+        // need to check if the engagement is null or not because not returned from autocomplete yet.
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        const currentLanguage = values.engagement
+          ? values.engagement.languageId
+          : null;
+        return (
+          <>
+            <SubmitError />
+            <LanguageField
+              name="engagement.languageId"
+              label="Language"
+              required
+              // wrap the options in a List with a subheader
+              PaperComponent={({ children }) => {
+                return (
+                  <Paper>
+                    <List>
+                      <ListSubheader>
+                        <ListItem divider dense>
+                          <ListItemText secondary="NAME" />
+                          <ListItemText secondary="ETH" />
+                          <ListItemText secondary="ROD" />
+                        </ListItem>
+                      </ListSubheader>
+                      {children}
+                    </List>
+                  </Paper>
+                );
+              }}
+              sortOptionComparator={sortByWhetherAnEngagedId}
+              getOptionDisabled={(lang) =>
+                engagedIds?.includes(lang.id) ?? true
+              }
+              helperText={
+                currentLanguage ? (
+                  <>
+                    <Typography
+                      variant="caption"
+                      className={classes.helperTextKey}
+                    >
+                      ETH
+                    </Typography>
+                    <Typography
+                      variant="caption"
+                      className={classes.helperTextValue}
+                    >
+                      {currentLanguage.ethnologue.code.value ?? '-'}
+                    </Typography>
+                    <Typography
+                      variant="caption"
+                      className={classes.helperTextKey}
+                    >
+                      ROD
+                    </Typography>
+                    <Typography
+                      variant="caption"
+                      className={classes.helperTextValue}
+                    >
+                      {currentLanguage.registryOfDialectsCode.value ?? '-'}
+                    </Typography>
+                  </>
+                ) : (
+                  ''
+                )
+              }
+              renderOption={(option) => (
+                <ListItem dense>
+                  <ListItemText
+                    primary={option.name.value ?? option.displayName.value}
+                  />
+                  <ListItemText primary={option.ethnologue.code.value ?? '-'} />
+                  <ListItemText
+                    primary={option.registryOfDialectsCode.value ?? '-'}
+                  />
+                </ListItem>
+              )}
             />
-            <ListItemText primary={option.ethnologue.code.value ?? '-'} />
-            <ListItemText
-              primary={option.registryOfDialectsCode.value ?? '-'}
-            />
-          </ListItem>
-        )}
-      />
+          </>
+        );
+      }}
     </DialogForm>
   );
 };
