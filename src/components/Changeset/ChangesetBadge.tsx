4279c6e5c7400bed63d7f83c28299e4df08ebef1
Carson Full <carson_full@tsco.org>
2022-07-15 11:53:45 -0500

    Migrate ChangesetBadge to TSS

--- src/components/Changeset/ChangesetBadge.tsx
+++ src/components/Changeset/ChangesetBadge.tsx
@@ -3,36 +3,33 @@ import {
   ChangeHistory as ChangeIcon,
   Remove as RemoveIcon,
 } from '@mui/icons-material';
-import {
-  Badge,
-  Grid,
-  makeStyles,
-  TooltipProps,
-  Typography,
-} from '@mui/material';
-import clsx from 'clsx';
+import { Badge, Grid, TooltipProps, Typography } from '@mui/material';
 import { startCase } from 'lodash';
 import { cloneElement, isValidElement, ReactElement, ReactNode } from 'react';
+import { makeStyles } from 'tss-react/mui';
 import { simpleSwitch, UseStyles } from '~/common';
 import { BadgeWithTooltip } from '../BadgeWithTooltip';
 import { PaperTooltip } from '../PaperTooltip';
 
-const useStyles = makeStyles(({ palette }) => ({
+const useStyles = makeStyles<
+  ChangesetBadgeOwnProps,
+  'added' | 'changed' | 'removed' | 'outline'
+>()(({ palette }, _params, classes) => ({
   root: {
     display: 'flex',
   },
   badge: {
     padding: 0,
     cursor: 'help',
-    '&$added': {
+    [`&.${classes.added}`]: {
       color: 'white',
       background: palette.success.main,
     },
-    '&$changed': {
+    [`&.${classes.changed}`]: {
       color: palette.info.contrastText,
       background: palette.info.main,
     },
-    '&$removed': {
+    [`&.${classes.removed}`]: {
       color: palette.error.contrastText,
       background: palette.error.main,
     },
@@ -41,14 +38,14 @@ const useStyles = makeStyles(({ palette }) => ({
     fontSize: 12,
   },
   children: {
-    '&$added$outline': {
+    [`&.${classes.added}.${classes.outline}`]: {
       border: `2px solid ${palette.success.main}`,
     },
-    '&$changed$outline': {
+    [`&.${classes.changed}.${classes.outline}`]: {
       border: `2px solid ${palette.info.main}`,
     },
-    '&$removed': {
-      '&$outline': {
+    [`&.${classes.removed}`]: {
+      [`&.${classes.outline}`]: {
         border: `2px solid ${palette.error.main}`,
       },
       boxShadow: 'none',
@@ -64,7 +61,7 @@ const useStyles = makeStyles(({ palette }) => ({
   outline: {},
 }));
 
-export interface ChangesetBadgeProps extends UseStyles<typeof useStyles> {
+interface ChangesetBadgeOwnProps {
   children: ReactNode;
   mode?: 'added' | 'removed' | 'changed';
   disableOutline?: boolean;
@@ -72,10 +69,16 @@ export interface ChangesetBadgeProps extends UseStyles<typeof useStyles> {
   TooltipProps?: Omit<TooltipProps, 'title' | 'children'>;
 }
 
+export interface ChangesetBadgeProps
+  extends ChangesetBadgeOwnProps,
+    UseStyles<typeof useStyles> {}
+
 export const ChangesetBadge = (props: ChangesetBadgeProps) => {
   const { mode, disableOutline, children, moreInfo, TooltipProps } = props;
   const outline = !disableOutline;
-  const classes = useStyles(props);
+  const { classes, cx } = useStyles(props, {
+    props: { classes: props.classes },
+  });
 
   if (!mode) {
     return <>{children}</>;
@@ -109,7 +112,7 @@ export const ChangesetBadge = (props: ChangesetBadgeProps) => {
       badgeContent={<Icon color="inherit" className={classes.icon} />}
       classes={{
         root: classes.root,
-        badge: clsx({
+        badge: cx({
           [classes.badge]: true,
           [classes.added]: mode === 'added',
           [classes.changed]: mode === 'changed',
@@ -121,7 +124,7 @@ export const ChangesetBadge = (props: ChangesetBadgeProps) => {
       {isValidElement(children)
         ? cloneElement(children, {
             ...children.props,
-            className: clsx(
+            className: cx(
               {
                 [classes.children]: true,
                 [classes.added]: mode === 'added',
