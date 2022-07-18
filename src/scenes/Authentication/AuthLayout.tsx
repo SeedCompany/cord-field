4d981331d6a8753131fc4841604ade3fe2ab5b02
Carson Full <carson_full@tsco.org>
2022-07-18 11:53:31 -0500

    Migrate our AuthLayout to MUI v5

--- src/scenes/Authentication/AuthLayout.tsx
+++ src/scenes/Authentication/AuthLayout.tsx
@@ -1,49 +1,28 @@
-import { makeStyles } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { styled, ThemeProvider } from '@mui/material/styles';
 import { Outlet } from 'react-router-dom';
 import { ChildrenProp } from '~/common';
 import { Picture } from '../../components/Picture';
 import { createTheme } from '../../theme';
 import backgroundImg from './background.png';
 
-const useStyles = makeStyles(
-  ({ palette }) => ({
-    '@global': {
-      body: {
-        // Here instead of `root` so overscroll doesn't have an abrupt white background.
-        backgroundColor: palette.background.default,
-      },
-    },
-    root: {
-      flex: 1,
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-evenly',
-      alignItems: 'center',
-      position: 'relative', // for background
-    },
-  }),
-  {
-    index: 1, // Higher specificity than baseline
-  }
-);
+const Root = styled('div')({
+  flex: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-evenly',
+  alignItems: 'center',
+  position: 'relative', // for background
+});
 
 const authTheme = createTheme({ dark: true });
 
-const ThemedLayout = ({ children }: ChildrenProp) => {
-  const classes = useStyles(); // has auth theme applied
-  return (
-    <ThemeProvider theme={authTheme}>
-      <div className={classes.root}>
-        <Picture lazy background source={backgroundImg} />
-        {children ?? <Outlet />}
-      </div>
-    </ThemeProvider>
-  );
-};
-
-export const AuthLayout = (props: ChildrenProp) => (
+export const AuthLayout = ({ children }: ChildrenProp) => (
   <ThemeProvider theme={authTheme}>
-    <ThemedLayout {...props} />
+    <CssBaseline />
+    <Root>
+      <Picture lazy background source={backgroundImg} />
+      {children ?? <Outlet />}
+    </Root>
   </ThemeProvider>
 );
