3b28aca9654ac7358d022d640032262e43d6b431
Carson Full <carson_full@tsco.org>
2022-07-18 12:53:10 -0500

    Migrate Menu usage for MUI v5

--- src/scenes/Root/Header/UserActionsMenu/UserActionsMenu.tsx
+++ src/scenes/Root/Header/UserActionsMenu/UserActionsMenu.tsx
@@ -46,9 +46,8 @@ export const UserActionsMenu = (props: Partial<MenuProps>) => {
       id="profile-menu"
       keepMounted
       open={Boolean(props.anchorEl)}
-      getContentAnchorEl={null}
       anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-      transformOrigin={{ vertical: spacing(-2), horizontal: 'right' }}
+      transformOrigin={{ vertical: parseInt(spacing(-2)), horizontal: 'right' }}
       classes={{ paper: classes.menu }}
       {...props}
     >
