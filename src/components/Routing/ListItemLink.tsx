77bfee0ff113f16c87c415c8fbe3e0cf168d44f3
Carson Full <carson_full@tsco.org>
2022-07-27 17:18:32 -0500

    Migrate ListItemLink for MUI v5

--- src/components/Routing/ListItemLink.tsx
+++ src/components/Routing/ListItemLink.tsx
@@ -1,13 +1,12 @@
-import { ListItem, ListItemProps } from '@mui/material';
+import { ListItemButton, ListItemButtonProps } from '@mui/material';
 import { forwardRef } from 'react';
 // eslint-disable-next-line @seedcompany/no-restricted-imports
 import { Link, LinkProps, useMatch } from 'react-router-dom';
-import { assert } from 'ts-essentials';
 import { Merge } from 'type-fest';
 
 export type ListItemLinkProps = InternalProps | ExternalProps;
 
-type BaseProps = Omit<ListItemProps<'a'>, 'button' | 'component' | 'href'> & {
+type BaseProps = Omit<ListItemButtonProps<'a'>, 'component'> & {
   /**
    * Whether the active styles should ONLY be applied for paths
    * that exactly match the given one
@@ -40,28 +39,17 @@ export const ListItemLink = forwardRef<HTMLAnchorElement, ListItemLinkProps>(
   ) {
     const path = typeof to === 'string' ? to : to.pathname!;
     const active = useMatch({ path, end: exact });
-
-    if (external) {
-      assert(typeof to === 'string');
-      return (
-        <ListItem {...props} href={to} button ref={ref} component="a">
-          {children}
-        </ListItem>
-      );
-    }
-
     return (
-      <ListItem
+      <ListItemButton
         selected={Boolean(active)}
         {...props}
-        to={to}
-        button
         ref={ref}
-        component={Link}
+        {...(external ? { href: to } : { to })}
+        component={external ? 'a' : Link}
         aria-current={active ? props['aria-current'] ?? 'page' : undefined}
       >
         {children}
-      </ListItem>
+      </ListItemButton>
     );
   }
 );
