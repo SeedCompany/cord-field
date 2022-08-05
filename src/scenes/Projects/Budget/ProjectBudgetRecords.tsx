f05bcf65a4905b5da470bbe28700e1bbb6f16dff
Carson Full <carson_full@tsco.org>
2022-08-05 09:11:13 -0500

    Migrate budget records to MUI DataGrid (no changeset or editing for now)

--- src/scenes/Projects/Budget/ProjectBudgetRecords.tsx
+++ src/scenes/Projects/Budget/ProjectBudgetRecords.tsx
@@ -1,185 +1,83 @@
-import { useApolloClient, useMutation } from '@apollo/client';
-import { sortBy, sumBy } from 'lodash';
+import { Card } from '@mui/material';
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { sortBy } from 'lodash';
 import { useMemo } from 'react';
-import { onUpdateChangeFragment, readFragment } from '~/api';
-import { RecalculateChangesetDiffFragmentDoc as RecalculateChangesetDiff } from '~/common/fragments';
-import {
-  PropertyDiff,
-  useDeletedItemsOfChangeset,
-  useDetermineChangesetDiffItem,
-} from '../../../components/Changeset';
+import { SecuredProp } from '~/common';
 import { useCurrencyFormatter } from '../../../components/Formatters/useCurrencyFormatter';
-import { ChangesetAwareTable } from '../../../components/Table';
-import type { ChangesetRowData } from '../../../components/Table/ChangesetAwareTable';
 import {
   BudgetRecordFragment as BudgetRecord,
-  CalculateNewTotalFragmentDoc as CalculateNewTotal,
   ProjectBudgetQuery,
-  UpdateProjectBudgetRecordDocument,
 } from './ProjectBudget.graphql';
-
-const tableComponents = {
-  // No toolbar since it's just empty space, we don't use it for anything.
-  Toolbar: () => null,
-};
-
-interface BudgetRowData extends ChangesetRowData {
-  record: BudgetRecord;
-  fundingPartner: string;
-  fiscalYear: string;
-  amount: string | null;
-}
 
 interface ProjectBudgetRecordsProps {
   budget: ProjectBudgetQuery['project']['budget'] | undefined;
   loading: boolean;
 }
 
+const getSecuredValue = ({ value }: GridValueGetterParams<SecuredProp<any>>) =>
+  value?.value;
+
 export const ProjectBudgetRecords = (props: ProjectBudgetRecordsProps) => {
   const { loading, budget } = props;
   const formatCurrency = useCurrencyFormatter();
-  const apollo = useApolloClient();
-  const [updateBudgetRecord] = useMutation(UpdateProjectBudgetRecordDocument, {
-    update: onUpdateChangeFragment({
-      object: budget?.value ?? undefined,
-      fragment: CalculateNewTotal,
-      updater: (cached) => ({
-        ...cached,
-        total: sumBy(cached.records, (record) => record.amount.value ?? 0),
-      }),
-    }),
-  });
-  const determineChangesetDiff = useDetermineChangesetDiffItem();
-  const deletedRecords = useDeletedItemsOfChangeset(
-    (obj): obj is BudgetRecord => obj.__typename === 'BudgetRecord'
-  );
 
-  const records: readonly BudgetRecord[] = budget?.value?.records ?? [];
+  const rows = useMemo(() => {
+    const raw: readonly BudgetRecord[] = budget?.value?.records ?? [];
+    return sortBy(raw, [
+      (record) => record.fiscalYear.value,
+      (record) => record.organization.value?.name.value ?? '',
+    ]);
+  }, [budget]);
 
-  const rowData = sortBy(
-    [...records, ...deletedRecords].map<BudgetRowData>((record) => {
-      const diff = determineChangesetDiff(record);
-      return {
-        record,
-        fundingPartner: record.organization.value?.name.value ?? '',
-        fiscalYear: String(record.fiscalYear.value),
-        amount: String(record.amount.value ?? ''),
-        diffMode: diff.mode,
-        diffInfo:
-          diff.mode === 'changed' ? (
-            <PropertyDiff
-              previous={diff.previous.amount.value ?? 0}
-              current={diff.current.amount.value ?? 0}
-              labelBy={formatCurrency}
-            />
-          ) : undefined,
-      };
-    }),
-    [(record) => record.fiscalYear, (record) => record.fundingPartner]
-  );
-
-  const blankAmount = 'click to edit';
-  const columns = useMemo(
-    () => [
-      {
-        field: 'record',
-        hidden: true,
-      },
-      {
-        field: 'fundingPartner',
-        editable: 'never',
-      },
-      {
-        field: 'fiscalYear',
-        editable: 'never',
-        defaultSort: 'asc',
-      },
-      {
-        field: 'amount',
-        type: 'currency',
-        editable: (_: any, rowData: BudgetRowData) =>
-          rowData.record.amount.canEdit,
-        render: (rowData: BudgetRowData) =>
-          rowData.amount
-            ? formatCurrency(Number(rowData.amount))
-            : rowData.record.amount.canEdit
-            ? blankAmount
-            : undefined,
-      },
-    ],
-    [formatCurrency]
-  );
+  const columns: Array<GridColDef<BudgetRecord>> = [
+    {
+      headerName: 'Funding Partner',
+      field: 'fundingPartner',
+      flex: 1,
+      valueGetter: ({ row }) => row.organization.value?.name.value ?? '',
+    },
+    {
+      headerName: 'Fiscal Year',
+      field: 'fiscalYear',
+      flex: 1,
+      valueGetter: getSecuredValue,
+    },
+    {
+      headerName: 'Amount',
+      field: 'amount',
+      flex: 1,
+      align: 'right',
+      headerAlign: 'right',
+      valueGetter: getSecuredValue,
+      valueFormatter: ({ value }) => formatCurrency(value ?? 0),
+    },
+  ];
 
   return (
-    <ChangesetAwareTable
-      data={rowData}
-      columns={columns}
-      isLoading={loading}
-      components={tableComponents}
-      cellEditable={
-        budget?.canEdit
-          ? {
-              onCellEditApproved: async (
-                newAmount: any,
-                _: any,
-                { record }: BudgetRowData
-              ) => {
-                if (newAmount === blankAmount || newAmount === '') return;
-
-                // If we have a changeset, fetch (from cache) the additional
-                // data required to provide an optimistic response.
-                // We need this because in our update operation we ask for the
-                // API to send back the updated diff. Because of this Apollo
-                // wants the updated diff, so we'll tell it that optimistically
-                // it is unchanged. The actual API result still overrides this
-                // when we get it.
-                const cachedChangeset = record.changeset
-                  ? readFragment(apollo.cache, {
-                      fragment: RecalculateChangesetDiff,
-                      object: record,
-                    })?.changeset
-                  : null;
-
-                await updateBudgetRecord({
-                  variables: {
-                    input: {
-                      budgetRecord: {
-                        id: record.id,
-                        amount: Number(newAmount),
-                      },
-                      changeset: budget.value?.changeset?.id,
-                    },
-                  },
-                  optimisticResponse:
-                    record.changeset && !cachedChangeset
-                      ? // If we are in a changeset, but we cannot get the required
-                        // data from cache, then skip the optimistic response.
-                        undefined
-                      : {
-                          updateBudgetRecord: {
-                            __typename: 'UpdateBudgetRecordOutput',
-                            budgetRecord: {
-                              __typename: 'BudgetRecord',
-                              id: record.id,
-                              changeset: cachedChangeset,
-                              amount: {
-                                __typename: 'SecuredFloatNullable',
-                                value: Number(newAmount),
-                              },
-                            },
-                          },
-                        },
-                });
-              },
-            }
-          : undefined
-      }
-      options={{
-        thirdSortClick: false,
-        headerStyle: {
-          background: 'unset',
-        },
-      }}
-    />
+    <Card>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        loading={loading}
+        components={{
+          Footer: () => null,
+        }}
+        initialState={{
+          sorting: { sortModel: [{ field: 'fiscalYear', sort: 'asc' }] },
+        }}
+        localeText={{
+          noRowsLabel:
+            'Project does not have a date range or funding partnerships',
+        }}
+        autoHeight
+        disableColumnMenu
+        sx={{
+          '& .MuiDataGrid-columnHeader:last-child .MuiDataGrid-columnSeparator--sideRight':
+            {
+              display: 'none',
+            },
+        }}
+      />
+    </Card>
   );
 };
