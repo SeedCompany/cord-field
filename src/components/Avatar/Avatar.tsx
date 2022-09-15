44784158d965e3ac0432a039b1c4160956d2205f
Andrew Murray <andrew_murray@tsco.org>
2022-09-14 20:43:30 -0500

    updated Avatar component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -3,35 +3,33 @@ import {
   AvatarProps as MuiAvatarProps,
   Skeleton,
 } from '@mui/material';
-import { makeStyles } from 'tss-react/mui';
 
 export interface AvatarProps extends MuiAvatarProps {
   loading?: boolean;
 }
 
-const useStyles = makeStyles()(() => ({
-  loading: {
-    backgroundColor: 'transparent',
-  },
-  skeleton: {
-    width: '100%',
-    height: '100%',
-  },
-}));
-
 export const Avatar = ({ loading, ...props }: AvatarProps) => {
   const { alt, src, srcSet, sizes, children, ...rest } = props;
-  const { classes } = useStyles();
   return (
     <MuiAvatar
       {...(loading ? rest : props)}
-      classes={{
-        ...props.classes,
-        colorDefault: loading ? classes.loading : props.classes?.colorDefault,
-      }}
+      sx={
+        loading
+          ? {
+              backgroundColor: 'transparent',
+            }
+          : undefined // setting to undefined will make it fallback to the default
+      }
+      classes={props.classes}
     >
       {loading ? (
-        <Skeleton variant="rectangular" className={classes.skeleton} />
+        <Skeleton
+          variant="rectangular"
+          sx={{
+            width: '100%',
+            height: '100%',
+          }}
+        />
       ) : (
         children
       )}
