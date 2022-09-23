7d33eddfd0d536c36033748a271ae96032d4e9bd
Andrew Murray <andrew_murray@tsco.org>
2022-09-23 14:56:22 -0500

    removed tss-react from 2 more components

--- src/components/Debug/Code.tsx
+++ src/components/Debug/Code.tsx
@@ -1,24 +1,23 @@
-import { makeStyles } from 'tss-react/mui';
+import { Box } from '@mui/material';
 import { ChildrenProp } from '~/common';
-
-const useStyles = makeStyles()((theme) => ({
-  root: {
-    padding: theme.spacing(1),
-    borderRadius: theme.shape.borderRadius,
-    color: theme.palette.background.paper,
-    backgroundColor: theme.palette.grey[800],
-  },
-}));
 
 export const Code = ({
   className,
   json,
   children,
 }: { json?: any; className?: string } & ChildrenProp) => {
-  const { classes, cx } = useStyles();
   return (
-    <pre className={cx(classes.root, className)}>
+    <Box
+      component="pre"
+      className={className}
+      sx={(theme) => ({
+        padding: theme.spacing(1),
+        borderRadius: theme.shape.borderRadius,
+        color: theme.palette.background.paper,
+        backgroundColor: theme.palette.grey[800],
+      })}
+    >
       {json ? JSON.stringify(json, undefined, 2) : children}
-    </pre>
+    </Box>
   );
 };
