549139978180f0b27950689378cb17be24738b36
William Harris <will_harris@tsco.org>
2022-09-28 13:38:26 -0700

    removed tss-react from ErrorButton.tsx

--- src/components/ErrorButton/ErrorButton.tsx
+++ src/components/ErrorButton/ErrorButton.tsx
@@ -1,43 +1,35 @@
 import { Button, ButtonProps } from '@mui/material';
 import { alpha as fade } from '@mui/material/styles';
-import { makeStyles } from 'tss-react/mui';
 import { Except } from 'type-fest';
-
-const useStyles = makeStyles()(({ palette }) => ({
-  text: {
-    color: palette.error.main,
-    '&:hover': {
-      backgroundColor: fade(palette.error.main, palette.action.hoverOpacity),
-    },
-  },
-  contained: {
-    color: palette.error.contrastText,
-    backgroundColor: palette.error.main,
-    '&:hover': {
-      backgroundColor: palette.error.dark,
-      // Reset on touch devices, it doesn't add specificity
-      '@media (hover: none)': {
-        backgroundColor: palette.error.main,
-      },
-    },
-  },
-}));
 
 export type ErrorButtonProps = Except<ButtonProps, 'color'>;
 
-export const ErrorButton = ({
-  classes: classesProp = {},
-  ...rest
-}: ErrorButtonProps) => {
-  const { classes, cx } = useStyles();
+export const ErrorButton = ({ ...rest }: ErrorButtonProps) => {
   return (
     <Button
       {...rest}
-      classes={{
-        ...classes,
-        text: cx(classes.text, classesProp.text),
-        contained: cx(classes.contained, classesProp.contained),
-      }}
+      sx={(theme) => ({
+        '&.MuiButton-text': {
+          color: theme.palette.error.main,
+          '&:hover': {
+            backgroundColor: fade(
+              theme.palette.error.main,
+              theme.palette.action.hoverOpacity
+            ),
+          },
+        },
+        '&.MuiButton-contained': {
+          color: theme.palette.error.contrastText,
+          backgroundColor: theme.palette.error.main,
+          '&:hover': {
+            backgroundColor: theme.palette.error.dark,
+            // Reset on touch devices, it doesn't add specificity
+            '@media (hover: none)': {
+              backgroundColor: theme.palette.error.main,
+            },
+          },
+        },
+      })}
     />
   );
 };
