90b24f03749b0c2288ec0e9459d9855649b1635b
William Harris <will_harris@tsco.org>
2022-09-30 14:52:37 -0700

    removed tss-react from FundingAccountCard.tsx

--- src/components/FundingAccountCard/FundingAccountCard.tsx
+++ src/components/FundingAccountCard/FundingAccountCard.tsx
@@ -1,23 +1,7 @@
 import { Card, CardActions, CardContent, Typography } from '@mui/material';
-import { makeStyles } from 'tss-react/mui';
 import { DisplaySimpleProperty } from '../DisplaySimpleProperty';
 import { FormattedDateTime } from '../Formatters';
 import { FundingAccountCardFragment } from './FundingAccountCard.graphql';
-
-const useStyles = makeStyles()(({ spacing }) => {
-  return {
-    root: {
-      width: '100%',
-      maxWidth: 400,
-    },
-    name: {
-      marginBottom: spacing(2),
-    },
-    cardActions: {
-      justifyContent: 'flex-end',
-    },
-  };
-});
 
 interface FundingAccountCardProps {
   className?: string;
@@ -28,12 +12,21 @@ export const FundingAccountCard = ({
   className,
   fundingAccount,
 }: FundingAccountCardProps) => {
-  const { classes, cx } = useStyles();
-
   return (
-    <Card className={cx(classes.root, className)}>
+    <Card
+      className={className}
+      sx={{
+        width: '100%',
+        maxWidth: 400,
+      }}
+    >
       <CardContent>
-        <Typography variant="h4" className={classes.name}>
+        <Typography
+          variant="h4"
+          sx={(theme) => ({
+            marginBottom: theme.spacing(2),
+          })}
+        >
           {fundingAccount.name.value}
         </Typography>
         <DisplaySimpleProperty
@@ -41,7 +34,11 @@ export const FundingAccountCard = ({
           value={fundingAccount.accountNumber.value}
         />
       </CardContent>
-      <CardActions className={classes.cardActions}>
+      <CardActions
+        sx={{
+          justifyContent: 'flex-end',
+        }}
+      >
         <Typography variant="caption" color="textSecondary">
           Created <FormattedDateTime date={fundingAccount.createdAt} />
         </Typography>
