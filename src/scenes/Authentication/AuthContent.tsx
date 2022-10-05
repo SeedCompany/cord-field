d7400a496be79c3757fec187745216f7d9d5b59f
William Harris <will_harris@tsco.org>
2022-10-05 13:30:46 -0700

    removed tss-react from AuthContent.tsx

--- src/scenes/Authentication/AuthContent.tsx
+++ src/scenes/Authentication/AuthContent.tsx
@@ -1,14 +1,5 @@
+import { Box } from '@mui/material';
 import { ReactNode } from 'react';
-import { makeStyles } from 'tss-react/mui';
-
-const useStyles = makeStyles()(({ spacing }) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    maxWidth: 400,
-    margin: spacing(4, 1),
-  },
-}));
 
 interface AuthContentProps {
   className?: string;
@@ -16,6 +7,17 @@ interface AuthContentProps {
 }
 
 export const AuthContent = ({ className, children }: AuthContentProps) => {
-  const { classes, cx } = useStyles();
-  return <div className={cx(classes.root, className)}>{children}</div>;
+  return (
+    <Box
+      className={className}
+      sx={(theme) => ({
+        display: 'flex',
+        flexDirection: 'column',
+        maxWidth: 400,
+        margin: theme.spacing(4, 1),
+      })}
+    >
+      {children}
+    </Box>
+  );
 };
