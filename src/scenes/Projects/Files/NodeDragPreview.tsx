0cb0b4561217818f8668f4c820212f6d406d9c78
William Harris <will_harris@tsco.org>
2022-10-12 11:53:29 -0700

    removed tss-react from NodeDragPreview.tsx

--- src/scenes/Projects/Files/NodeDragPreview.tsx
+++ src/scenes/Projects/Files/NodeDragPreview.tsx
@@ -1,29 +1,26 @@
 import { Paper } from '@mui/material';
-import { makeStyles } from 'tss-react/mui';
 import { FileNodeInfoFragment } from '../../../components/files/files.graphql';
 import { fileIcon } from '../../../components/files/fileTypes';
 import { parseFileNameAndExtension } from '../../../components/Formatters';
 import { isDirectory } from './util';
 
-const useStyles = makeStyles()(({ palette, spacing }) => ({
-  root: {
-    display: 'flex',
-    alignItems: 'center',
-    padding: spacing(1),
-  },
-  fileIcon: {
-    color: palette.action.active,
-    marginRight: spacing(0.5),
-  },
-}));
-
 export const NodeDragPreview = ({ node }: { node: FileNodeInfoFragment }) => {
-  const { classes } = useStyles();
-
   const Icon = fileIcon(isDirectory(node) ? 'directory' : node.mimeType);
   return (
-    <Paper elevation={12} className={classes.root}>
-      <Icon className={classes.fileIcon} />
+    <Paper
+      elevation={12}
+      sx={(theme) => ({
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(1),
+      })}
+    >
+      <Icon
+        sx={(theme) => ({
+          color: theme.palette.action.active,
+          marginRight: theme.spacing(0.5),
+        })}
+      />
       {parseFileNameAndExtension(node.name).displayName}
     </Paper>
   );
