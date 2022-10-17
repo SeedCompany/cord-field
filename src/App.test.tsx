a51d12e3f71d91f43b34f966ac878d17f5a902f5
Andrew Murray <andrew_murray@tsco.org>
2022-10-17 16:47:51 -0400

    reverted initial tests idea

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,37 @@
+import { ApolloProvider } from '@apollo/client';
 import { render } from '@testing-library/react';
+import {
+  Request as ExpressRequest,
+  Response as ExpressResponse,
+} from 'express';
+import { Request } from 'jest-express/lib/request';
+import { Response } from 'jest-express/lib/response';
+import { useMemo, useState } from 'react';
+import { HelmetProvider } from 'react-helmet-async';
+import { StaticRouter } from 'react-router-dom/server';
+import { ChildrenProp } from '~/common';
+import { createClient } from './api/client/createClient';
 import { App } from './App';
-import { TestContext } from './TestContext';
+import { Nest } from './components/Nest';
+import { RequestContext } from './hooks';
+
+const TestContext = ({ url, children }: { url: string } & ChildrenProp) => {
+  // @ts-expect-error yes the type doesn't match we are faking it.
+  const req: ExpressRequest = useMemo(() => new Request(url), [url]);
+  const res = new Response() as unknown as ExpressResponse;
+  const [client] = useState(() => createClient({ ssr: { req, res } }));
+  return (
+    <Nest
+      elements={[
+        <HelmetProvider key="helmet" context={{}} children={[]} />,
+        <RequestContext.Provider key="req" value={req} />,
+        <StaticRouter key="router" location={req.originalUrl} />,
+        <ApolloProvider key="apollo" client={client} children={[]} />,
+      ]}
+      children={children}
+    />
+  );
+};
 
 test('renders HOME', () => {
   const { getByRole } = render(
