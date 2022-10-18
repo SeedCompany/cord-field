f2c36c2b407dda514634321079814febc2611e94
William Harris <will_harris@tsco.org>
2022-10-17 18:07:02 -0700

    updated Grid conponent with container property to solve an error thrown in the console

--- src/components/MethodologiesCard/MethodologiesCard.tsx
+++ src/components/MethodologiesCard/MethodologiesCard.tsx
@@ -42,6 +42,7 @@ export const MethodologiesCard = ({
         {methodology && (
           <Grid
             item
+            container
             wrap="nowrap"
             sx={(theme) => ({
               display: 'flex',
