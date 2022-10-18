527989fb28ff2a799254cd43f8337cb1a7196a0f
Andrew Murray <andrew_murray@tsco.org>
2022-10-18 13:54:25 -0400

    updated DropzoneOverlay component

--- src/components/Upload/DropzoneOverlay.tsx
+++ src/components/Upload/DropzoneOverlay.tsx
@@ -1,33 +1,9 @@
-import { Typography } from '@mui/material';
-import { makeStyles } from 'tss-react/mui';
+import { Box, Typography } from '@mui/material';
 
 /**
  * This component requires a parent with a `position` value,
  * since it uses `position: absolute`.
  */
-
-const useStyles = makeStyles()(({ palette, spacing }) => ({
-  dropContainer: {
-    backgroundColor: palette.grey['600'],
-    border: `4px dashed ${palette.grey['300']}`,
-    cursor: 'pointer',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    opacity: 0.8,
-    padding: spacing(3),
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
-    zIndex: 100,
-  },
-  instructions: {
-    color: palette.grey['300'],
-    textAlign: 'center',
-  },
-}));
 
 export interface DropzoneOverlayClasses {
   root?: string;
@@ -41,25 +17,41 @@ interface DropzoneOverlayProps {
 }
 
 export const DropzoneOverlay = (props: DropzoneOverlayProps) => {
-  const { classes, cx } = useStyles();
   const {
     classes: classNames,
     isDragActive,
     message = 'Drop files to start uploading',
   } = props;
   return !isDragActive ? null : (
-    <div
-      className={cx(classes.dropContainer, classNames?.root && classNames.root)}
+    <Box
+      sx={(theme) => ({
+        backgroundColor: theme.palette.grey['600'],
+        border: `4px dashed ${theme.palette.grey['300']}`,
+        cursor: 'pointer',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        opacity: 0.8,
+        padding: 3,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        zIndex: 100,
+      })}
+      className={classNames?.root && classNames.root}
     >
       <Typography
         variant="h1"
-        className={cx(
-          classes.instructions,
-          classNames?.text && classNames.text
-        )}
+        className={classNames?.text && classNames.text}
+        sx={(theme) => ({
+          color: theme.palette.grey['300'],
+          textAlign: 'center',
+        })}
       >
         {message}
       </Typography>
-    </div>
+    </Box>
   );
 };
