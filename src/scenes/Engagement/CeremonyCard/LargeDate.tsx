b2b63de864299259345f0dd1842f6c0497fe811e
William Harris <will_harris@tsco.org>
2022-10-17 18:05:30 -0700

    removed tss-react from LargeDate.tsx

--- src/scenes/Engagement/CeremonyCard/LargeDate.tsx
+++ src/scenes/Engagement/CeremonyCard/LargeDate.tsx
@@ -1,35 +1,18 @@
-import { Skeleton, Typography } from '@mui/material';
+import { Box, Skeleton, Typography } from '@mui/material';
 import { useState } from 'react';
-import { makeStyles } from 'tss-react/mui';
 import { CalendarDate, Nullable, SecuredProp } from '~/common';
 import { useDateFormatter } from '../../../components/Formatters';
 import { Redacted } from '../../../components/Redacted';
 
-const useStyles = makeStyles()(({ palette, spacing }) => ({
-  root: {
-    padding: spacing(2),
-    borderRadius: 100,
-    position: 'relative',
-  },
-  loaded: {
-    backgroundColor: palette.grey[300],
-  },
-  hidden: {
-    visibility: 'hidden',
-  },
-  skeleton: {
-    borderRadius: 'inherit',
-    height: 'initial',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-  },
-  redacted: {
-    backgroundColor: palette.grey[700],
-  },
-}));
+const skeleton = {
+  borderRadius: 'inherit',
+  height: 'initial',
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0,
+};
 
 interface LargeDateProps {
   date?: Nullable<SecuredProp<CalendarDate>>;
@@ -37,7 +20,6 @@ interface LargeDateProps {
 }
 
 export const LargeDate = ({ date, className }: LargeDateProps) => {
-  const { classes, cx } = useStyles();
   const formatDate = useDateFormatter();
   const [placeholderNow] = useState(() => CalendarDate.local());
 
@@ -45,25 +27,45 @@ export const LargeDate = ({ date, className }: LargeDateProps) => {
     <Typography
       color="primary"
       variant="h2"
-      className={cx(
-        classes.root,
-        !date || !date.canRead ? null : classes.loaded,
-        className
-      )}
+      className={className}
+      sx={[
+        (theme) => ({
+          padding: theme.spacing(2),
+          borderRadius: 100,
+          position: 'relative',
+        }),
+        !date || !date.canRead
+          ? null
+          : (theme) => ({
+              backgroundColor: theme.palette.grey[300],
+            }),
+      ]}
     >
       {date?.value ? (
         formatDate(date.value)
       ) : (
         <>
-          <span className={classes.hidden}>{formatDate(placeholderNow)}</span>
+          <Box
+            component="span"
+            sx={{
+              visibility: 'hidden',
+            }}
+          >
+            {formatDate(placeholderNow)}
+          </Box>
           {!date ? (
-            <Skeleton variant="rectangular" className={classes.skeleton} />
+            <Skeleton variant="rectangular" sx={skeleton} />
           ) : !date.canRead ? (
             <Redacted
               info="You don't have permission to view this date"
               SkeletonProps={{
                 variant: 'rectangular',
-                className: cx(classes.skeleton, classes.redacted),
+                sx: [
+                  skeleton,
+                  (theme) => ({
+                    backgroundColor: theme.palette.grey[700],
+                  }),
+                ],
               }}
             />
           ) : null}
