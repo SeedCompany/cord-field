4513292cddd1553f026dbf2ad66619238a91d3f2
Andrew Murray <andrew_murray@tsco.org>
2022-10-18 14:09:36 -0400

    updated SnackbarProvider component

--- src/components/Snackbar/SnackbarProvider.tsx
+++ src/components/Snackbar/SnackbarProvider.tsx
@@ -1,36 +1,33 @@
 import { Report as ErrorIcon } from '@mui/icons-material';
+import { Box } from '@mui/material';
 import { SnackbarProvider as BaseSnackbarProvider } from 'notistack';
-import { makeStyles } from 'tss-react/mui';
 import { ChildrenProp } from '~/common';
-
-const useStyles = makeStyles()(({ palette }) => ({
-  /* eslint-disable tss-unused-classes/unused-classes */
-  variantSuccess: {
-    backgroundColor: palette.success.main,
-  },
-  variantError: {
-    backgroundColor: palette.error.main,
-  },
-  variantInfo: {
-    backgroundColor: palette.info.main,
-  },
-  variantWarning: {
-    backgroundColor: palette.warning.main,
-  },
-  /* eslint-enable tss-unused-classes/unused-classes */
-}));
 
 const icons = {
   error: <ErrorIcon style={{ fontSize: 20, marginInlineEnd: 8 }} />,
 };
 
 export const SnackbarProvider = ({ children }: ChildrenProp) => {
-  const { classes } = useStyles();
   return (
-    <BaseSnackbarProvider
-      classes={classes}
+    <Box
+      component={BaseSnackbarProvider}
       children={children}
       iconVariant={icons}
+      maxSnack={10}
+      sx={(theme) => ({
+        '&.SnackbarItem-variantInfo': {
+          backgroundColor: theme.palette.info.main,
+        },
+        '&.SnackbarItem-variantSuccess': {
+          backgroundColor: theme.palette.success.main,
+        },
+        '&.SnackbarItem-variantWarning': {
+          backgroundColor: theme.palette.warning.main,
+        },
+        '&.SnackbarItem-variantError': {
+          backgroundColor: theme.palette.error.main,
+        },
+      })}
     />
   );
 };
