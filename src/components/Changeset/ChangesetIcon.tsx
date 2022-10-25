d8e7dc1175fe83da1c53d16c13a0e0bbca20c9b2
William Harris <will_harris@tsco.org>
2022-10-25 08:07:06 -0700

    Revert "reverted to old version per instruction from carson"
    This reverts commit 30521358e1fafab23a1dcd4ff52eed1537f18612.

--- src/components/Changeset/ChangesetIcon.tsx
+++ src/components/Changeset/ChangesetIcon.tsx
@@ -4,18 +4,18 @@ import {
   Remove as RemoveIcon,
 } from '@mui/icons-material';
 import { SvgIconProps } from '@mui/material';
-import { simpleSwitch, StyleProps } from '~/common';
+import { simpleSwitch } from '~/common';
 import { DiffMode } from './ChangesetDiffContext';
 
-export interface ChangesetIconProps extends SvgIconProps, StyleProps {
+export interface ChangesetIconProps extends SvgIconProps {
   mode: DiffMode;
 }
 
-export const ChangesetIcon = ({ mode, sx, ...props }: ChangesetIconProps) => {
+export const ChangesetIcon = ({ mode, ...props }: ChangesetIconProps) => {
   const Icon = simpleSwitch(mode, {
     added: AddIcon,
     changed: ChangeIcon,
     removed: RemoveIcon,
   })!;
-  return <Icon {...props} sx={sx} />;
+  return <Icon {...props} />;
 };
