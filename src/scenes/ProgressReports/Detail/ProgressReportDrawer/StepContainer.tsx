342e00e19a2a45b017c5d032373338f8130887b9
Andrew Murray <andrew_murray@tsco.org>
2022-11-01 13:08:11 -0400

    removed variant buttons from header

--- src/scenes/ProgressReports/Detail/ProgressReportDrawer/StepContainer.tsx
+++ src/scenes/ProgressReports/Detail/ProgressReportDrawer/StepContainer.tsx
@@ -1,5 +1,5 @@
 import { ArrowForward } from '@mui/icons-material';
-import { Box, Button, Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { Sx } from '~/common';
 import { useProgressReportContext } from '../../ProgressReportContext';
 import { Step1, Step2, Step3 } from '../TemporarySteps';
@@ -14,10 +14,11 @@ const typographyLinkSx: Sx = (theme) => ({
   '&:hover': {
     textDecoration: 'underline',
   },
+  userSelect: 'none',
 });
 
 export const StepContainer = () => {
-  const { step } = useProgressReportContext();
+  const { step, nextProgressReportStep } = useProgressReportContext();
 
   return (
     <div
@@ -27,111 +28,13 @@ export const StepContainer = () => {
         height: '100%',
       }}
     >
-      <StepContainerHeader />
-      <Box sx={{ flex: 1, padding: 2, pt: 1 }}>
-        {step === 0 && <TeamHighlightStep />}
-        {step === 1 && <Step1 />}
-        {step === 2 && <Step2 />}
-        {step === 3 && <Step3 />}
-      </Box>
-    </div>
-  );
-};
-
-const marginRightSx = {
-  marginRight: 2,
-};
-
-const isActiveSx: Sx = {
-  backgroundColor: 'error.main',
-  color: 'white',
-  '&:hover': {
-    backgroundColor: 'error.dark',
-    borderColor: 'error.dark',
-  },
-};
-
-const StepContainerHeader = () => {
-  const { nextProgressReportStep, setPromptVariant, promptVariant } =
-    useProgressReportContext();
-
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        backgroundColor: 'background.paper',
-        padding: 2,
-        borderTop: `1px solid ${colorPalette.header.border}`,
-        borderBottom: `1px solid ${colorPalette.header.border}`,
-      }}
-    >
-      <div
-        css={{
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <div>
-          <Typography variant="body2" component="span" marginRight={1}>
-            Refinement Roles:
-          </Typography>
-          <Button
-            variant="outlined"
-            color="error"
-            size="small"
-            onClick={() => setPromptVariant('Partner')}
-            sx={[marginRightSx, promptVariant === 'Partner' && isActiveSx]}
-          >
-            Partner
-          </Button>
-          <Button
-            variant="outlined"
-            color="error"
-            size="small"
-            onClick={() => setPromptVariant('Translation')}
-            sx={[marginRightSx, promptVariant === 'Translation' && isActiveSx]}
-          >
-            Translation
-          </Button>
-          <Button
-            variant="outlined"
-            color="error"
-            size="small"
-            onClick={() => setPromptVariant('FPM Notes')}
-            sx={[marginRightSx, promptVariant === 'FPM Notes' && isActiveSx]}
-          >
-            FPM
-          </Button>
-          <Button
-            variant="outlined"
-            color="error"
-            size="small"
-            onClick={() => setPromptVariant('Communications Edit')}
-            sx={[
-              marginRightSx,
-              promptVariant === 'Communications Edit' && isActiveSx,
-            ]}
-          >
-            Communication
-          </Button>
-        </div>
-        <Typography
-          variant="caption"
-          color="error"
-          sx={{
-            fontSize: 10,
-            mt: 1,
-          }}
-        >
-          * These buttons are ONLY for testing purposes
-        </Typography>
-      </div>
       <Box
         sx={{
           display: 'flex',
-          alignItems: 'center',
+          justifyContent: 'end',
+          padding: 2,
+          borderTop: `1px solid ${colorPalette.header.border}`,
+          borderBottom: `1px solid ${colorPalette.header.border}`,
         }}
       >
         <Typography
@@ -149,6 +52,13 @@ const StepContainerHeader = () => {
           />
         </Typography>
       </Box>
-    </Box>
+
+      <Box sx={{ flex: 1, padding: 2, pt: 1 }}>
+        {step === 0 && <TeamHighlightStep />}
+        {step === 1 && <Step1 />}
+        {step === 2 && <Step2 />}
+        {step === 3 && <Step3 />}
+      </Box>
+    </div>
   );
 };
