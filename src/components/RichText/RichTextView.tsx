7207902c062b4227d8746a31d7e2769227d21479
Carson Full <carson_full@tsco.org>
2022-11-02 09:42:21 -0500

    Add RichTextView for view only that renders natively in react

---
+++ src/components/RichText/RichTextView.tsx
@@ -0,0 +1,29 @@
+import type { OutputData as RichTextData } from '@editorjs/editorjs';
+import { Typography } from '@mui/material';
+import Blocks from 'editorjs-blocks-react-renderer';
+import HTMLReactParser from 'html-react-parser';
+import { memo } from 'react';
+import { ToolKey } from './editorJsTools';
+
+export const RichTextView = memo(function RichTextView({
+  data,
+}: {
+  data?: RichTextData;
+}) {
+  if (!data) {
+    return null;
+  }
+  const data1 = { version: '0', time: 0, ...data };
+  return <Blocks data={data1} renderers={renderers} />;
+});
+
+type RenderFn<T = undefined> = (_: { data?: T }) => JSX.Element;
+
+const ParagraphBlock: RenderFn<{ text: string }> = ({ data }) => {
+  const { text } = data ?? {};
+  return <Typography paragraph>{text && HTMLReactParser(text)}</Typography>;
+};
+
+const renderers: { [K in ToolKey]?: RenderFn<any> } = {
+  paragraph: ParagraphBlock,
+};
--- src/components/RichText/index.ts
+++ src/components/RichText/index.ts
@@ -1,2 +1,3 @@
+export * from './RichTextView';
 export * from './RichTextField';
 export type { ToolKey } from './editorJsTools';
