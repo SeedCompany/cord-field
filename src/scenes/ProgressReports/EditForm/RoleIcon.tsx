cbb9c0e64ff0cb35aad741b015121560ef7d0510
Andrew Murray <andrew_murray@tsco.org>
2022-11-10 07:30:37 -0500

    updated RoleIcon to accept sx props

--- src/scenes/ProgressReports/EditForm/RoleIcon.tsx
+++ src/scenes/ProgressReports/EditForm/RoleIcon.tsx
@@ -2,6 +2,7 @@ import { Grading, Translate } from '@mui/icons-material';
 import { SvgIconProps } from '@mui/material';
 import { ComponentType } from 'react';
 import { Role } from '~/api/schema.graphql';
+import { extendSx, StyleProps } from '~/common';
 import { PeopleJoinedIcon } from '~/components/Icons';
 import { ProjectManagerIcon } from '~/components/Icons/ProjectManagerIcon';
 import { colorPalette } from './colorPalette';
@@ -14,11 +15,11 @@ const variantToIconMapper: {
   ProjectManager: ProjectManagerIcon,
   Marketing: Grading,
 };
-interface StepIconProps {
+interface StepIconProps extends StyleProps {
   role?: Role | null;
 }
 
-export const RoleIcon = ({ role }: StepIconProps) => {
+export const RoleIcon = ({ role, sx }: StepIconProps) => {
   if (!role) {
     return null;
   }
@@ -27,13 +28,16 @@ export const RoleIcon = ({ role }: StepIconProps) => {
 
   return Icon ? (
     <Icon
-      sx={{
-        backgroundColor: colorPalette.stepperCard.iconBackground[role],
-        marginRight: 1,
-        padding: 1,
-        height: 48,
-        width: 48,
-      }}
+      sx={[
+        {
+          backgroundColor: colorPalette.stepperCard.iconBackground[role],
+          marginRight: 1,
+          padding: 1,
+          height: 48,
+          width: 48,
+        },
+        ...extendSx(sx),
+      ]}
     />
   ) : null;
 };
