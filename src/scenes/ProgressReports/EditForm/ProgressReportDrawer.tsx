afa573f1459c8593b8debc88191071d5b2c1e510
Andrew Murray <andrew_murray@tsco.org>
2022-11-18 12:13:59 -0500

    display error if issues with Progress Report in Edit form

--- src/scenes/ProgressReports/EditForm/ProgressReportDrawer.tsx
+++ src/scenes/ProgressReports/EditForm/ProgressReportDrawer.tsx
@@ -1,6 +1,8 @@
 import { useQuery } from '@apollo/client';
 import { Box, Drawer, Typography } from '@mui/material';
+import { ReactNode } from 'react';
 import { useMatch } from 'react-router-dom';
+import { Error } from '~/components/Error';
 import { useNavigate } from '~/components/Routing';
 import { ProgressReportContextProvider } from './ProgressReportContext';
 import { ProgressReportDrawerHeader } from './ProgressReportDrawerHeader';
@@ -19,11 +21,25 @@ export const ProgressReportDrawer = ({
 
   const navigate = useNavigate();
 
-  const { data } = useQuery(ProgressReportEditDocument, {
+  const { data, error } = useQuery(ProgressReportEditDocument, {
     variables: {
       progressReportId: reportId,
     },
   });
+
+  if (error) {
+    return (
+      <CustomDrawer open={open} onClose={() => navigate('../')}>
+        <ProgressReportDrawerHeader />
+        <Error page error={error}>
+          {{
+            NotFound: 'Could not find progress report',
+            Default: 'Error loading progress report',
+          }}
+        </Error>
+      </CustomDrawer>
+    );
+  }
 
   if (data?.periodicReport.__typename !== 'ProgressReport') {
     return (
@@ -35,50 +51,67 @@ export const ProgressReportDrawer = ({
 
   return (
     <ProgressReportContextProvider report={data.periodicReport}>
-      <Drawer
-        anchor="right"
-        open={open}
-        onClose={() => navigate('..')}
-        sx={{
-          '& .MuiDrawer-paper': {
-            width: '100%',
-          },
-        }}
-      >
-        <Box sx={{ display: 'flex', height: 1, width: 'calc(100% - 300px)' }}>
-          <Box
-            sx={{
-              width: 1,
-              height: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              padding: 2,
-              pt: 4,
-            }}
-          >
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'space-between',
-              }}
-            >
-              <ProgressReportDrawerHeader />
-            </Box>
-            <Box
-              sx={{
-                flex: 1,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-              }}
-            >
-              <StepContainer />
-            </Box>
-          </Box>
-          <ProgressReportSidebar />
+      <CustomDrawer open={open} onClose={() => navigate('..')}>
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <ProgressReportDrawerHeader />
         </Box>
-      </Drawer>
+        <Box
+          sx={{
+            flex: 1,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
+        >
+          <StepContainer />
+        </Box>
+      </CustomDrawer>
     </ProgressReportContextProvider>
+  );
+};
+
+const CustomDrawer = ({
+  children,
+  open,
+  onClose,
+}: {
+  children: ReactNode;
+  open: boolean;
+  onClose?: () => void;
+}) => {
+  return (
+    <Drawer
+      anchor="right"
+      open={open}
+      onClose={onClose}
+      sx={{
+        '& .MuiDrawer-paper': {
+          width: '100%',
+        },
+      }}
+    >
+      {' '}
+      <Box sx={{ display: 'flex', height: 1, width: 'calc(100% - 300px)' }}>
+        <Box
+          sx={{
+            width: 1,
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            padding: 2,
+            pt: 4,
+          }}
+        >
+          {children}
+        </Box>
+        <ProgressReportSidebar />
+      </Box>
+    </Drawer>
   );
 };
