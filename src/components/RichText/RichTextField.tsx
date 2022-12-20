b178125585bc058d1f499f644e7b4dafcd9026f4
Carson Full <carson_full@tsco.org>
2022-12-19 19:06:24 -0600

    Make RichTextField background white

--- src/components/RichText/RichTextField.tsx
+++ src/components/RichText/RichTextField.tsx
@@ -247,6 +247,7 @@ export function RichTextField({
                             e.stopPropagation();
                           }
                         }}
+                        sx={{ bgcolor: 'background.paper' }}
                       />
 
                       <FormHelperText>
