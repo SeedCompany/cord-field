3a2fa4fe7ef86a4f3aa36a64df96a0c5688543f9
Andrew Murray <andrew_murray@tsco.org>
2022-12-21 20:58:08 +0200

    extended the `label` prop on `DataButton`

--- src/components/DataButton/DataButton.tsx
+++ src/components/DataButton/DataButton.tsx
@@ -1,8 +1,15 @@
-import { Button, ButtonProps, Skeleton, TooltipProps } from '@mui/material';
+import {
+  Box,
+  Button,
+  ButtonProps,
+  Skeleton,
+  TooltipProps,
+  Typography,
+} from '@mui/material';
 import { isFunction } from 'lodash';
-import { ReactNode } from 'react';
+import { cloneElement, isValidElement, ReactNode } from 'react';
 import { makeStyles } from 'tss-react/mui';
-import { SecuredProp } from '~/common';
+import { extendSx, SecuredProp, Sx } from '~/common';
 import { Redacted } from '../Redacted';
 
 const useStyles = makeStyles()(() => ({
@@ -11,6 +18,22 @@ const useStyles = makeStyles()(() => ({
   },
 }));
 
+const dataButtonStyles: Sx = {
+  alignItems: 'start',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  height: 1,
+};
+
+const dataButtonLabelStyles: Sx = {
+  fontSize: '0.75rem',
+};
+
+const iconStyles: Sx = {
+  marginBottom: -0.75,
+  marginRight: 0.5,
+};
+
 export const DataButton = <T extends any>({
   loading,
   secured,
@@ -18,6 +41,8 @@ export const DataButton = <T extends any>({
   redacted,
   children,
   startIcon,
+  sx,
+  label,
   ...props
 }: Omit<ButtonProps, 'children'> & {
   loading?: boolean;
@@ -25,6 +50,7 @@ export const DataButton = <T extends any>({
   redacted?: TooltipProps['title'];
   children: ((value: T) => ReactNode) | ReactNode;
   empty?: ReactNode;
+  label?: ReactNode;
 }) => {
   const { classes } = useStyles();
   const showData = !loading && (secured ? secured.canRead : true);
@@ -35,14 +61,29 @@ export const DataButton = <T extends any>({
       : empty
     : children ?? empty;
 
+  const iconWithStyles = isValidElement(startIcon)
+    ? cloneElement(startIcon, {
+        ...startIcon.props,
+        sx: [startIcon.props.sx, ...extendSx(iconStyles)],
+      })
+    : startIcon;
+
   const btn = (
     <Button
       variant="outlined"
       color="secondary"
       {...props}
-      startIcon={showData ? startIcon : undefined}
+      sx={[dataButtonStyles, ...extendSx(sx)]}
     >
-      {data || <>&nbsp;</>}
+      {label && (
+        <Typography component="span" sx={dataButtonLabelStyles}>
+          {label}
+        </Typography>
+      )}
+      <Box sx={{ flexDirection: 'row' }}>
+        {iconWithStyles}
+        {data || <>&nbsp;</>}
+      </Box>
     </Button>
   );
 
