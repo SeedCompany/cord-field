5c60206f73cd1a9b9baa5e4c5d1bc5a76475b80d
Carson Full <carson_full@tsco.org>
2023-02-15 15:05:51 -0600

    Disable change password while impersonating to not cause confusion

--- src/scenes/Root/Header/ProfileMenu/ProfileMenu.tsx
+++ src/scenes/Root/Header/ProfileMenu/ProfileMenu.tsx
@@ -1,6 +1,8 @@
 import { Divider, Menu, MenuItem, MenuProps, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import { useContext } from 'react';
 import { makeStyles } from 'tss-react/mui';
+import { ImpersonationContext } from '~/api/client/ImpersonationContext';
 import { useDialog } from '../../../../components/Dialog';
 import { MenuItemLink } from '../../../../components/Routing';
 import { useSession } from '../../../../components/Session';
@@ -23,6 +25,8 @@ export const ProfileMenu = (props: Partial<MenuProps>) => {
   const { classes } = useStyles();
   const { spacing } = useTheme();
   const { session } = useSession();
+  const impersonation = useContext(ImpersonationContext);
+
   const [changePasswordState, changePassword] = useDialog();
   const userId = session?.id;
 
@@ -51,14 +55,16 @@ export const ProfileMenu = (props: Partial<MenuProps>) => {
         {userId && (
           <MenuItemLink to={`/users/${userId}`}>View Profile</MenuItemLink>
         )}
-        <MenuItem
-          onClick={(event) => {
-            changePassword();
-            props.onClose?.(event, 'backdropClick');
-          }}
-        >
-          Change Password
-        </MenuItem>
+        {!impersonation.enabled && (
+          <MenuItem
+            onClick={(event) => {
+              changePassword();
+              props.onClose?.(event, 'backdropClick');
+            }}
+          >
+            Change Password
+          </MenuItem>
+        )}
         <MenuItemLink to="/logout">Sign Out</MenuItemLink>
       </Menu>
       <ChangePassword {...changePasswordState} />
