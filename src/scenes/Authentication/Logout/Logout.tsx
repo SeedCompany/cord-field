db7d75f1872f805168073e5b9be5de2cdafc6da8
Carson Full <carson_full@tsco.org>
2023-02-16 13:34:27 -0600

    Clear impersonation on logout

--- src/scenes/Authentication/Logout/Logout.tsx
+++ src/scenes/Authentication/Logout/Logout.tsx
@@ -1,6 +1,9 @@
 import { useApolloClient, useMutation } from '@apollo/client';
 import { useMount } from 'ahooks';
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { ImpersonationContext } from '~/api/client/ImpersonationContext';
+import { sleep as delay } from '~/common';
 import { AuthWaiting } from '../AuthWaiting';
 import { LogoutDocument } from './logout.graphql';
 
@@ -8,10 +11,13 @@ export const Logout = () => {
   const navigate = useNavigate();
   const client = useApolloClient();
   const [logout] = useMutation(LogoutDocument);
+  const impersonation = useContext(ImpersonationContext);
 
   // NOTE: SSR logout is handled independently in server.ts
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   useMount(async () => {
+    impersonation.set(); // clear
+    await delay(100); // give time to reset store if needed.
     await logout();
     await client.resetStore();
     navigate('/login', { replace: true });
