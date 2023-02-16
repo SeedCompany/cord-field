d44b55041071b3ad95bef6fb721cd82474c6438a
Carson Full <carson_full@tsco.org>
2023-02-16 13:05:12 -0600

    Fix logout logic being triggered multiple times

--- src/scenes/Authentication/Logout/Logout.tsx
+++ src/scenes/Authentication/Logout/Logout.tsx
@@ -1,5 +1,5 @@
 import { useApolloClient, useMutation } from '@apollo/client';
-import { useEffect } from 'react';
+import { useMount } from 'ahooks';
 import { useNavigate } from 'react-router-dom';
 import { AuthWaiting } from '../AuthWaiting';
 import { LogoutDocument } from './logout.graphql';
@@ -10,13 +10,12 @@ export const Logout = () => {
   const [logout] = useMutation(LogoutDocument);
 
   // NOTE: SSR logout is handled independently in server.ts
-  useEffect(() => {
-    void logout()
-      .then(() => client.resetStore())
-      .then(() => {
-        navigate('/login', { replace: true });
-      });
-  }, [logout, client, navigate]);
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  useMount(async () => {
+    await logout();
+    await client.resetStore();
+    navigate('/login', { replace: true });
+  });
 
   return <AuthWaiting />;
 };
