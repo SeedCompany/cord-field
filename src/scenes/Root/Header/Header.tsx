9058714916da877bf4fa86460f20fd046595746d
Andrew Murray <andrew_murray@tsco.org>
2023-03-08 12:47:57 -0500

    cleaned up sx on Header

--- src/scenes/Root/Header/Header.tsx
+++ src/scenes/Root/Header/Header.tsx
@@ -5,18 +5,9 @@ import { ProfileToolbar } from './ProfileToolbar';
 const headerSx = (theme: Theme) => ({
   padding: theme.spacing(4, 4, 1, 4),
   justifyContent: 'space-between',
-  [theme.breakpoints.up('xs')]: {
-    display: 'none',
-    alignItems: 'stretch',
-    flexDirection: 'column',
-  },
-  [theme.breakpoints.up('sm')]: {
-    display: 'flex',
-  },
-  [theme.breakpoints.up('mobile')]: {
-    alignItems: 'flex-start',
-    flexDirection: 'row',
-  },
+  display: { xs: 'none', sm: 'flex' },
+  flexDirection: { xs: 'column', mobile: 'row' },
+  alignItems: { xs: 'strecth', mobile: 'flex-start' },
 });
 
 export const Header = () => {
