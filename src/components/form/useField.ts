SeedCompany/cord-field

commit 6780e91b098803d18317e14bd27054b35c1b4375

Don't disable fields while submitting while auto submit is enabled

src/components/form/useField.ts
@@ -1,9 +1,10 @@
 import { identity } from 'lodash';
-import { useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { UseFieldConfig, useField as useFinalField } from 'react-final-form';
 import { Except } from 'type-fest';
 import { callSome, Many, many, Nullable } from '~/common';
 import { useFirstMountState } from '~/hooks';
+import { AutoSubmitOptionsContext } from './AutoSubmit';
 import { useFieldName } from './FieldGroup';
 import { isEqualBy, isListEqualBy, useFocus, useIsSubmitting } from './util';
 import {
@@ -121,7 +122,9 @@ export const useField = <
   });
   const submitting = useIsSubmitting();
 
-  const disabled = disabledProp ?? meta.submitting;
+  const autoSubmit = useContext(AutoSubmitOptionsContext);
+
+  const disabled = disabledProp ?? autoSubmit ? false : meta.submitting;
 
   const firstRender = useFirstMountState();
   const focused = disabled

src/components/form/AutoSubmit.tsx
@@ -1,5 +1,5 @@
 import { useDebounceFn } from 'ahooks';
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { useForm, useFormState } from 'react-final-form';
 
 export interface AutoSubmitOptions {
@@ -61,3 +61,7 @@ export const AutoSubmit = (options: AutoSubmitOptions) => {
 
   return null;
 };
+
+export const AutoSubmitOptionsContext = createContext<
+  AutoSubmitOptions | undefined
+>(undefined);

src/components/form/Form.tsx
@@ -5,7 +5,12 @@ import {
 } from 'react-final-form';
 import { Promisable } from 'type-fest';
 import { ErrorHandlers, handleFormError } from '../../api';
-import { AutoSubmit, AutoSubmitOptions } from './AutoSubmit';
+import {
+  AutoSubmit,
+  AutoSubmitOptions,
+  AutoSubmitOptionsContext,
+  defaultAutoSubmitOptions,
+} from './AutoSubmit';
 import { FieldGroup } from './FieldGroup';
 
 export interface FormProps<
@@ -98,8 +103,15 @@ export function Form<
       }}
     >
       {(renderProps) => {
-        const renderedForm =
+        let renderedForm =
           typeof children === 'function' ? children(renderProps) : children;
+        renderedForm = autoSubmitOptions ? (
+          <AutoSubmitOptionsContext.Provider value={autoSubmitOptions}>
+            {renderedForm}
+          </AutoSubmitOptionsContext.Provider>
+        ) : (
+          renderedForm
+        );
 
         return (
           <FieldGroup replace prefix={fieldsPrefix}>

commit 972b0e56dfd85e35db0611bd46770fbedcea4cdf

Fix disabled prop on fields applying incorrectly
Regression from 6780e91

src/components/form/useField.ts
@@ -124,7 +124,7 @@ export const useField = <
 
   const autoSubmit = useContext(AutoSubmitOptionsContext);
 
-  const disabled = disabledProp ?? autoSubmit ? false : meta.submitting;
+  const disabled = disabledProp ?? (autoSubmit ? false : meta.submitting);
 
   const firstRender = useFirstMountState();
   const focused = disabled