0642c1deb687a1446eecddd591ea2a071c6cd8e0
adam-soltech <adamlang@splaqbook-m1-2.lan>
2023-08-01 18:03:34 -0700

    chore: redo formatter with immutability and ease of reading in mind

--- src/components/RichText/ms-word-helpers.ts
+++ src/components/RichText/ms-word-helpers.ts
@@ -1,24 +1,75 @@
 import { OutputData as RichTextData } from '@editorjs/editorjs';
 
-const hasWordListMarkers = (text: string) => {
-  return /•\t(.*)/.test(text) || /^\d+\.\s(.*)/m.test(text);
+interface BlockState {
+  blocks: any[];
+  paragraphText: string;
+  listItems: string[];
+  listType: 'unordered' | 'ordered' | null;
+}
+
+const initialBlockState: BlockState = {
+  blocks: [],
+  paragraphText: '',
+  listItems: [],
+  listType: null,
 };
 
-const isUnorderedList = (text: string) => {
-  return /•\t(.*)/.test(text);
-};
+export const handleMsPasteFormatting = (
+  event: ClipboardEvent
+): RichTextData | undefined => {
+  const text = event.clipboardData?.getData('text');
+  // If there is no text, or the text has no list return undefined and continue native paste propagation
+  if (!text || !hasWordListMarkers(text)) {
+    return;
+  }
 
-const isOrderedList = (text: string) => {
-  return /^\d+\.\s(.*)/.test(text);
-};
-
-const createListBlock = (items: string[], style: 'unordered' | 'ordered') => {
-  return {
-    type: 'list',
-    data: {
-      style: style,
-      items: items,
+  const lines = text.split('\n');
+  // split the text by newline character and start the reduction loop
+  const blocksState: BlockState = lines.reduce(
+    (acc: BlockState, line: string): BlockState => {
+      // if we have a paragraph and the line is empty, create a paragraph block and reset the paragraph text
+      if (!line.trim() && acc.paragraphText) {
+        return {
+          ...acc,
+          blocks: [...acc.blocks, createParagraphBlock(acc.paragraphText)],
+          paragraphText: '',
+        };
+        // if we have an unordered list item add it to the list items array from which the block will be constructed
+      } else if (isUnorderedList(line)) {
+        return {
+          ...acc,
+          listType: 'unordered',
+          listItems: [...acc.listItems, line.replace(/•\t/, '')],
+        };
+        // Same thing here but for an ordered list
+      } else if (isOrderedList(line)) {
+        return {
+          ...acc,
+          listType: 'ordered',
+          listItems: [...acc.listItems, line.replace(/^\d+\.\s/, '')],
+        };
+        // if we have a completed list build the block and reset the list items array
+      } else if (acc.listItems.length) {
+        return {
+          listType: acc.listType,
+          listItems: [],
+          blocks: [...acc.blocks, createListBlock(acc.listItems, acc.listType)],
+          paragraphText: acc.paragraphText + line + ' ',
+        };
+        // Otherwise just add the line to the paragraph text
+      } else {
+        return {
+          ...acc,
+          paragraphText: acc.paragraphText + line + ' ',
+        };
+      }
     },
+    initialBlockState
+  );
+
+  return {
+    time: Date.now(),
+    blocks: blocksState.blocks,
   };
 };
 
@@ -31,50 +82,27 @@ const createParagraphBlock = (text: string) => {
   };
 };
 
-export const handleMsPasteFormatting = (
-  event: ClipboardEvent
-): RichTextData | undefined => {
-  const text = event.clipboardData?.getData('text');
-
-  if (!text || !hasWordListMarkers(text)) {
-    return undefined;
-  }
-
-  const lines = text.split('\n');
-  const blocks = [];
-  let listItems: string[] = [];
-  let listType: 'unordered' | 'ordered' = 'unordered';
-  let paragraphText = '';
-
-  lines.forEach((line) => {
-    if (!line.trim() && paragraphText) {
-      blocks.push(createParagraphBlock(paragraphText));
-      paragraphText = '';
-    } else if (isUnorderedList(line)) {
-      listType = 'unordered';
-      listItems.push(line.replace(/•\t/, ''));
-    } else if (isOrderedList(line)) {
-      listType = 'ordered';
-      listItems.push(line.replace(/^\d+\.\s/, ''));
-    } else {
-      if (listItems.length) {
-        blocks.push(createListBlock(listItems, listType));
-        listItems = [];
-      }
-      paragraphText += line + ' ';
-    }
-  });
-
-  if (listItems.length) {
-    blocks.push(createListBlock(listItems, listType));
-  }
-
-  if (paragraphText.trim()) {
-    blocks.push(createParagraphBlock(paragraphText));
-  }
-
+const createListBlock = (
+  items: string[],
+  style: 'unordered' | 'ordered' | null
+) => {
   return {
-    time: Date.now(),
-    blocks: blocks,
+    type: 'list',
+    data: {
+      style: style,
+      items: items,
+    },
   };
+};
+
+const hasWordListMarkers = (text: string) => {
+  return /•\t(.*)/.test(text) || /^\d+\.\s(.*)/m.test(text);
+};
+
+const isUnorderedList = (text: string) => {
+  return /•\t(.*)/.test(text);
+};
+
+const isOrderedList = (text: string) => {
+  return /^\d+\.\s(.*)/.test(text);
 };
