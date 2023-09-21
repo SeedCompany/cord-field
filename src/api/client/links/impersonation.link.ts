1f687bda036ad91fd8208e24969b149c5ad94000
Carson Full <carson_full@tsco.org>
2023-09-21 14:45:20 -0500

    Ignore current impersonation for sensitive operations
    Normal user flow doesn't allow this.
    But there are edge cases where this could come up.

--- src/api/client/links/impersonation.link.ts
+++ src/api/client/links/impersonation.link.ts
@@ -1,18 +1,26 @@
 import { setContext } from '@apollo/client/link/context';
 import { pickBy } from 'lodash';
 import { RefObject } from 'react';
+import { GqlSensitiveOperations } from '../../operationsList';
 import { Impersonation } from '../ImpersonationContext';
 
 export const createImpersonationLink = (
   ref?: RefObject<Impersonation | undefined>
 ) =>
-  setContext((req, prev) => ({
-    ...prev,
-    headers: {
-      ...prev.headers,
-      ...pickBy({
-        'x-cord-impersonate-user': ref?.current?.user,
-        'x-cord-impersonate-role': ref?.current?.roles?.join(','),
-      }),
-    },
-  }));
+  setContext((req, prev) => {
+    const isSensitiveOp = req.operationName
+      ? GqlSensitiveOperations.has(req.operationName)
+      : false;
+    const impersonation =
+      ref?.current && !isSensitiveOp ? ref.current : undefined;
+    return {
+      ...prev,
+      headers: {
+        ...prev.headers,
+        ...pickBy({
+          'x-cord-impersonate-user': impersonation?.user,
+          'x-cord-impersonate-role': impersonation?.roles?.join(','),
+        }),
+      },
+    };
+  });
