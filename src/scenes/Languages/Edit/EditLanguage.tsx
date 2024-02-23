SeedCompany/cord-field

commit e6f1b2c6b2f6706f0493a8cfc3679202e93813eb

Remove null coercion that's no longer needed

src/scenes/Languages/Edit/EditLanguage.tsx
@@ -53,29 +53,16 @@ export const EditLanguage = (props: EditLanguageProps) => {
       title="Edit Language"
       {...props}
       initialValues={initialValues}
-      onSubmit={async ({
-        language: {
-          populationOverride,
-          sponsorEstimatedEndFY,
-          ethnologue,
-          ...rest
-        },
-      }) => {
+      onSubmit={async ({ language }) => {
         const result = await updateLanguage({
           variables: {
             input: {
               language: {
-                populationOverride: populationOverride ?? null,
+                ...language,
                 sponsorEstimatedEndDate:
                   CalendarDate.fiscalYearEndToCalendarDate(
-                    sponsorEstimatedEndFY
+                    language.sponsorEstimatedEndFY
                   ),
-                ethnologue: {
-                  ...ethnologue,
-                  code: ethnologue?.code ?? null,
-                  provisionalCode: ethnologue?.provisionalCode ?? null,
-                },
-                ...rest,
               },
             },
           },

src/scenes/Locations/Edit/EditLocation.tsx
@@ -36,7 +36,7 @@ export const EditLocation = (props: EditLocationProps) => {
   );
 
   const onSubmit: FormProps['onSubmit'] = async ({
-    location: { isoAlpha3, fundingAccountId, mapImage: mapImages, ...rest },
+    location: { fundingAccountId, mapImage: mapImages, ...rest },
   }) => {
     const [uploadedImageInfo, finalizeUpload] = await uploadFile(
       mapImages?.[0]
@@ -44,7 +44,6 @@ export const EditLocation = (props: EditLocationProps) => {
 
     const input: UpdateLocation = {
       ...rest,
-      isoAlpha3: isoAlpha3 ?? null,
       fundingAccountId: fundingAccountId?.id ?? null,
       mapImage: uploadedImageInfo,
     };

src/scenes/Partners/Edit/EditPartner.tsx
@@ -157,14 +157,12 @@ export const EditPartner = ({
       decorators={decorators}
       initialValues={initialValues}
       onSubmit={async ({ partner, organization }) => {
-        const { pointOfContactId, pmcEntityCode, address, ...partnerRest } =
-          partner;
+        const { pointOfContactId, pmcEntityCode, ...partnerRest } = partner;
 
         await updatePartner({
           variables: {
             partner: {
               ...partnerRest,
-              address: address ?? null,
               pointOfContactId: pointOfContactId?.id,
               pmcEntityCode: pmcEntityCode?.toUpperCase(),
             },

commit b537d70a7a350816ca4cf34f706d890eee243f05

Fix passing field value `sponsorEstimatedEndFY` to API. Regressed in e6f1b2c

src/scenes/Languages/Edit/EditLanguage.tsx
@@ -53,7 +53,9 @@ export const EditLanguage = (props: EditLanguageProps) => {
       title="Edit Language"
       {...props}
       initialValues={initialValues}
-      onSubmit={async ({ language }) => {
+      onSubmit={async ({
+        language: { sponsorEstimatedEndFY, ...language },
+      }) => {
         const result = await updateLanguage({
           variables: {
             input: {
@@ -61,7 +63,7 @@ export const EditLanguage = (props: EditLanguageProps) => {
                 ...language,
                 sponsorEstimatedEndDate:
                   CalendarDate.fiscalYearEndToCalendarDate(
-                    language.sponsorEstimatedEndFY
+                    sponsorEstimatedEndFY
                   ),
               },
             },