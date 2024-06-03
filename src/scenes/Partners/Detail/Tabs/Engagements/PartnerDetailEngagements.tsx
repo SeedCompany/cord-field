6e839e03b1f650d571ef6c2afe4cb797ceb59f32
Carson Full <carson_full@tsco.org>
2024-06-03 17:02:36 -0500

    Finish off implementation

--- src/scenes/Partners/Detail/Tabs/Engagements/PartnerDetailEngagements.tsx
+++ src/scenes/Partners/Detail/Tabs/Engagements/PartnerDetailEngagements.tsx
@@ -1,10 +1,17 @@
-import { Box, Chip, Typography } from '@mui/material';
+import { Box, Chip, Tooltip, Typography } from '@mui/material';
 import { DataGrid, GridColDef, GridLocaleText } from '@mui/x-data-grid';
-import { cmpBy, simpleSwitch } from '@seedcompany/common';
-import { useNavigate, useParams } from 'react-router-dom';
+import {
+  cleanJoin,
+  cmpBy,
+  mapEntries,
+  simpleSwitch,
+} from '@seedcompany/common';
+import { useParams } from 'react-router-dom';
 import {
   EngagementStatusLabels,
   ProgressReportStatusLabels,
+  ProgressReportStatusList,
+  ProjectStepLabels,
   ProjectTypeLabels,
 } from '~/api/schema.graphql';
 import { Sensitivity } from '~/api/schema/schema.graphql';
@@ -12,14 +19,14 @@ import { labelFrom } from '~/common';
 import { FormattedDate } from '~/components/Formatters';
 import { SensitivityIcon } from '~/components/Sensitivity';
 import { useTable } from '~/hooks';
+import { Link } from '../../../../../components/Routing';
 import {
-  PartnerDetailEngagmentsTableListItemFragment as Engagement,
+  PartnerDetailEngagementsTableListItemFragment as Engagement,
   PartnerDetailEngagementsDocument,
 } from './PartnerDetailEngagements.graphql';
 
 export const PartnerDetailEngagements = () => {
   const { partnerId = '' } = useParams();
-  const navigate = useNavigate();
 
   const [props] = useTable({
     query: PartnerDetailEngagementsDocument,
@@ -27,154 +34,9 @@ export const PartnerDetailEngagements = () => {
     listAt: 'partner.engagements',
     initialInput: {
       count: 20,
-      sort: 'status',
+      sort: 'nameProjectFirst',
     },
   });
-
-  const columns: Array<GridColDef<Engagement>> = [
-    {
-      headerName: 'Name',
-      field: 'project',
-      flex: 3.5,
-      sortable: true,
-      renderCell: ({ row }) => {
-        const name =
-          row.__typename === 'LanguageEngagement'
-            ? `${row.project.name.value} - ${row.language.value?.name.value}`
-            : row.project.name.value;
-        return (
-          <Typography
-            color="primary.main"
-            variant="body2"
-            onClick={() => navigate(`/projects/${row.project.id}`)}
-          >
-            {name}
-          </Typography>
-        );
-      },
-    },
-    {
-      headerName: 'Type',
-      field: 'type',
-      flex: 1,
-      valueGetter: ({ row }) => labelFrom(ProjectTypeLabels)(row.project.type),
-      renderCell: ({ value }) => <Chip label={value} variant="outlined" />,
-    },
-    {
-      headerName: 'Status',
-      field: 'status',
-      flex: 2,
-      valueGetter: ({ row }) =>
-        labelFrom(EngagementStatusLabels)(row.status.value),
-    },
-    {
-      headerName: 'Country',
-      field: 'primaryLocation',
-      flex: 1,
-      valueGetter: ({ row }) => row.project.primaryLocation.value?.name.value,
-    },
-    {
-      headerName: 'ISO',
-      field: 'ethnologue',
-      flex: 1,
-      valueGetter: ({ row }) =>
-        row.__typename === 'LanguageEngagement'
-          ? row.language.value?.ethnologue.code.value?.toUpperCase()
-          : '',
-    },
-    {
-      headerName: 'ROD',
-      field: 'registryOfDialectsCode',
-      flex: 1,
-      valueGetter: ({ row }) =>
-        row.__typename === 'LanguageEngagement'
-          ? row.language.value?.registryOfDialectsCode.value
-          : '',
-    },
-    {
-      headerName: 'MOU Start',
-      field: 'mouStart',
-      flex: 1,
-      valueGetter: ({ row }) => row.project.mouStart.value,
-      renderCell: ({ value }) => {
-        return (
-          <Typography variant="body2" color="secondaryText">
-            <FormattedDate date={value} />
-          </Typography>
-        );
-      },
-    },
-    {
-      headerName: 'MOU End',
-      field: 'mouEnd',
-      flex: 1,
-      valueGetter: ({ row }) => row.project.mouEnd.value,
-      renderCell: ({ value }) => {
-        return (
-          <Typography variant="body2" color="secondaryText">
-            <FormattedDate date={value} />
-          </Typography>
-        );
-      },
-    },
-    {
-      headerName: 'QR Status',
-      field: 'currentProgressReportDue',
-      flex: 1,
-      renderCell({ row }) {
-        const qrStatus =
-          row.__typename === 'LanguageEngagement'
-            ? labelFrom(ProgressReportStatusLabels)(
-                row.currentProgressReportDue.value?.status.value
-              )
-            : '';
-        return (
-          <Typography
-            color="primary.main"
-            variant="body2"
-            onClick={() => navigate(`/engagements/${row.id}/reports/progress`)}
-          >
-            {qrStatus}
-          </Typography>
-        );
-      },
-    },
-    {
-      headerName: 'Sensitivity',
-      field: 'sensitivity',
-      flex: 1,
-      sortComparator: cmpBy<Sensitivity>((v) =>
-        simpleSwitch(v, { Low: 0, Medium: 1, High: 2 })
-      ),
-      renderCell: ({ value }) => (
-        <Box
-          display="flex"
-          alignItems="center"
-          gap={1}
-          textTransform="uppercase"
-        >
-          <SensitivityIcon value={value} disableTooltip />
-          {value}
-        </Box>
-      ),
-    },
-    {
-      headerName: 'Files',
-      field: '',
-      flex: 1,
-      renderCell({ row }) {
-        return (
-          <Typography
-            color="primary.main"
-            variant="body2"
-            onClick={() => navigate(`/projects/${row.project.id}/files`)}
-          >
-            View Files
-          </Typography>
-        );
-      },
-    },
-  ];
 
   return (
     <DataGrid<Engagement>
@@ -187,8 +49,6 @@ export const PartnerDetailEngagements = () => {
       sx={{
         border: 'none',
         pt: 1,
-        // TODO Somehow change to using Link component
-        '& .MuiDataGrid-row:hover': { cursor: 'pointer' },
         '& .MuiDataGrid-cell, & .MuiDataGrid-columnHeader': {
           '&:focus, &:focus-within': { outline: 'none' },
         },
@@ -199,5 +59,146 @@ export const PartnerDetailEngagements = () => {
 };
 
 const localeText: Partial<GridLocaleText> = {
-  noRowsLabel: 'This partner is not engaged in any engagements',
+  noRowsLabel: 'This partner does not have any engagements',
 };
+
+const columns: Array<GridColDef<Engagement>> = [
+  {
+    headerName: 'Name',
+    field: 'nameProjectFirst',
+    minWidth: 200,
+    valueGetter: ({ row }) =>
+      cleanJoin(' - ', [
+        row.project.name.value,
+        row.__typename === 'LanguageEngagement'
+          ? row.language.value?.name.value
+          : row.__typename === 'InternshipEngagement'
+          ? row.intern.value?.fullName
+          : null,
+      ]),
+    renderCell: ({ value, row }) => (
+      <Link to={`/projects/${row.project.id}`}>{value}</Link>
+    ),
+  },
+  {
+    headerName: 'Type',
+    field: 'project.type',
+    width: 130,
+    valueGetter: ({ row }) => labelFrom(ProjectTypeLabels)(row.project.type),
+    renderCell: ({ value }) => <Chip label={value} variant="outlined" />,
+  },
+  {
+    headerName: 'Project Step',
+    field: 'project.step',
+    width: 250,
+    valueGetter: ({ row }) =>
+      labelFrom(ProjectStepLabels)(row.project.step.value),
+  },
+  {
+    headerName: 'Engagement Status',
+    field: 'status',
+    width: 190,
+    valueGetter: ({ row }) =>
+      labelFrom(EngagementStatusLabels)(row.status.value),
+  },
+  {
+    headerName: 'Country',
+    field: 'project.primaryLocation.name',
+    valueGetter: ({ row }) => row.project.primaryLocation.value?.name.value,
+  },
+  {
+    headerName: 'Ethnologue Code',
+    field: 'language.ethnologue.code',
+    width: 75,
+    valueGetter: ({ row }) =>
+      row.__typename === 'LanguageEngagement'
+        ? row.language.value?.ethnologue.code.value?.toUpperCase()
+        : '',
+    renderHeader: () => (
+      <Tooltip title="Ethnologue Code">
+        <Typography variant="inherit">ISO</Typography>
+      </Tooltip>
+    ),
+  },
+  {
+    headerName: 'Registry of Dialects',
+    field: 'language.registryOfDialectsCode',
+    width: 80,
+    valueGetter: ({ row }) =>
+      row.__typename === 'LanguageEngagement'
+        ? row.language.value?.registryOfDialectsCode.value
+        : '',
+    renderHeader: () => (
+      <Tooltip title="Registry of Dialects">
+        <Typography variant="inherit">ROD</Typography>
+      </Tooltip>
+    ),
+  },
+  {
+    headerName: 'MOU Start',
+    field: 'startDate',
+    valueGetter: ({ value }) => value.value,
+    renderCell: ({ value }) => <FormattedDate date={value} />,
+  },
+  {
+    headerName: 'MOU End',
+    field: 'endDate',
+    valueGetter: ({ value }) => value.value,
+    renderCell: ({ value }) => <FormattedDate date={value} />,
+  },
+  {
+    headerName: 'QR Status',
+    field: 'currentProgressReportDue.status',
+    valueGetter: ({ row }) =>
+      row.__typename === 'LanguageEngagement' &&
+      row.currentProgressReportDue.value
+        ? labelFrom(ProgressReportStatusLabels)(
+            row.currentProgressReportDue.value.status.value
+          )
+        : null,
+    sortComparator: cmpBy((v: string | null) =>
+      ProgressReportStatusIndex.get(v)
+    ),
+    renderHeader: () => (
+      <Tooltip title="Status of Querterly Report Currently Due">
+        <Typography variant="inherit">QR Status</Typography>
+      </Tooltip>
+    ),
+    renderCell({ value, row }) {
+      const report =
+        row.__typename === 'LanguageEngagement'
+          ? row.currentProgressReportDue.value
+          : undefined;
+      if (!report) {
+        return null;
+      }
+      return <Link to={`/progress-reports/${report.id}`}>{value}</Link>;
+    },
+  },
+  {
+    headerName: 'Sensitivity',
+    field: 'sensitivity',
+    sortComparator: cmpBy<Sensitivity>((v) =>
+      simpleSwitch(v, { Low: 0, Medium: 1, High: 2 })
+    ),
+    renderCell: ({ value }) => (
+      <Box display="flex" alignItems="center" gap={1} textTransform="uppercase">
+        <SensitivityIcon value={value} disableTooltip />
+        {value}
+      </Box>
+    ),
+  },
+  {
+    headerName: 'Files',
+    field: 'files',
+    sortable: false,
+    renderCell: ({ row }) => (
+      <Link to={`/projects/${row.project.id}/files`}>View Files</Link>
+    ),
+  },
+];
+
+const ProgressReportStatusIndex = mapEntries(
+  [null, ...ProgressReportStatusList].entries(),
+  ([i, v]) => [!v ? null : ProgressReportStatusLabels[v], i - 1]
+).asMap;
