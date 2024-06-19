SeedCompany/cord-field

commit d8b6a68f621e83d437746311071ab50b43c44fd2

Migrate withStyles to styled

src/components/CardGroup/CardGroup.tsx
@@ -1,6 +1,6 @@
 import { Card, CardProps } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import { Children, ComponentType, Fragment } from 'react';
-import { withStyles } from 'tss-react/mui';
 import { applyBreakpoint, BreakpointAt } from '~/common';
 import { ResponsiveDivider } from '../ResponsiveDivider';
 
@@ -8,13 +8,12 @@ export interface CardGroupProps extends CardProps {
   horizontal?: BreakpointAt;
 }
 
-const CardGroupRoot = withStyles(
-  Card as ComponentType<CardGroupProps>,
-  ({ breakpoints }, props: CardGroupProps) => ({
+const CardGroupRoot = styled(Card as ComponentType<CardGroupProps>)(
+  ({ horizontal, theme }) => ({
     root: {
       display: 'flex',
       flexDirection: 'column',
-      ...applyBreakpoint(breakpoints, props.horizontal, {
+      ...applyBreakpoint(theme.breakpoints, horizontal, {
         flexDirection: 'row',
       }),
     },

src/components/PaperTooltip/PaperTooltip.tsx
@@ -1,14 +1,12 @@
 import { Tooltip } from '@mui/material';
-import { CSSObject } from 'tss-react';
-import { withStyles } from 'tss-react/mui';
+import { styled } from '@mui/material/styles';
 
-export const PaperTooltip = withStyles(
-  Tooltip,
-  ({ palette, spacing, typography, shadows }) => ({
+export const PaperTooltip = styled(Tooltip)(
+  ({ theme: { palette, spacing, typography, shadows } }) => ({
     tooltip: {
       backgroundColor: palette.background.paper,
       color: palette.text.primary,
-      ...(typography.body1 as CSSObject),
+      ...typography.body1,
       padding: spacing(1),
       boxShadow: shadows[8],
     },

src/components/ResponsiveDivider/ResponsiveDivider.tsx
@@ -1,6 +1,6 @@
 import { Box, Divider, DividerProps } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import { ComponentType } from 'react';
-import { withStyles } from 'tss-react/mui';
 import { applyBreakpoint, BreakpointAt, extendSx, StyleProps } from '~/common';
 
 export interface ResponsiveDividerProps extends DividerProps {
@@ -8,26 +8,25 @@ export interface ResponsiveDividerProps extends DividerProps {
   spacing?: number;
 }
 
-export const ResponsiveDivider = withStyles(
-  Divider as ComponentType<ResponsiveDividerProps>,
-  ({ spacing, breakpoints }, props) => ({
-    root: {
-      width: `calc(100% - ${spacing(props.spacing ?? 0)} * 2)`,
-      margin: spacing(0, props.spacing ?? 0),
-      '.MuiGrid-container > &': {
-        marginRight: -1,
-      },
-      ...applyBreakpoint(breakpoints, props.vertical, {
-        margin: spacing(props.spacing ?? 0, 0),
-        borderLeftWidth: 'thin',
-        // Divider orientation=vertical & flexItem
-        width: 1,
-        height: 'auto',
-        alignSelf: 'stretch',
-      }),
+export const ResponsiveDivider = styled(
+  Divider as ComponentType<ResponsiveDividerProps>
+)(({ spacing, vertical, theme }) => ({
+  root: {
+    width: `calc(100% - ${theme.spacing(spacing ?? 0)} * 2)`,
+    margin: theme.spacing(0, spacing ?? 0),
+    '.MuiGrid-container > &': {
+      marginRight: -1,
     },
-  })
-);
+    ...applyBreakpoint(theme.breakpoints, vertical, {
+      margin: theme.spacing(spacing ?? 0, 0),
+      borderLeftWidth: 'thin',
+      // Divider orientation=vertical & flexItem
+      width: 1,
+      height: 'auto',
+      alignSelf: 'stretch',
+    }),
+  },
+}));
 
 /**
  * Horizontal divider until `verticalWhen` css query is matched.

commit 8de22b710107212c2d1569ee78a5a1f908e7a60c

Fix CardGroup regression from d8b6a68

src/components/CardGroup/CardGroup.tsx
@@ -1,37 +1,33 @@
 import { Card, CardProps } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { Children, ComponentType, Fragment } from 'react';
-import { applyBreakpoint, BreakpointAt } from '~/common';
+import { Children, Fragment } from 'react';
+import { applyBreakpoint, BreakpointAt, extendSx } from '~/common';
 import { ResponsiveDivider } from '../ResponsiveDivider';
 
 export interface CardGroupProps extends CardProps {
   horizontal?: BreakpointAt;
 }
 
-const CardGroupRoot = styled(Card as ComponentType<CardGroupProps>)(
-  ({ horizontal, theme }) => ({
-    root: {
-      display: 'flex',
-      flexDirection: 'column',
-      ...applyBreakpoint(theme.breakpoints, horizontal, {
-        flexDirection: 'row',
+export const CardGroup = ({ horizontal, ...props }: CardGroupProps) => (
+  <Card
+    {...props}
+    sx={[
+      (theme) => ({
+        display: 'flex',
+        flexDirection: 'column',
+        ...applyBreakpoint(theme.breakpoints, horizontal, {
+          flexDirection: 'row',
+        }),
       }),
-    },
-  })
+      ...extendSx(props.sx),
+    ]}
+  >
+    {Children.map(props.children, (child, index) => (
+      <Fragment key={index}>
+        {index % 2 ? (
+          <ResponsiveDivider vertical={horizontal} spacing={2} />
+        ) : null}
+        {child}
+      </Fragment>
+    ))}
+  </Card>
 );
-
-export const CardGroup = (props: CardGroupProps) => {
-  const { children, ...rest } = props;
-  return (
-    <CardGroupRoot {...rest}>
-      {Children.map(children, (child, index) => (
-        <Fragment key={index}>
-          {index % 2 ? (
-            <ResponsiveDivider vertical={props.horizontal} spacing={2} />
-          ) : null}
-          {child}
-        </Fragment>
-      ))}
-    </CardGroupRoot>
-  );
-};