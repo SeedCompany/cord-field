27096c33dc4530cab7a7735a8a4cb770ce81336e
Carson Full <carson_full@tsco.org>
2024-06-28 21:15:15 -0500

    Fix the tab list mistakenly being included in container size

--- src/components/Tabs/TabsContainer.tsx
+++ src/components/Tabs/TabsContainer.tsx
@@ -4,11 +4,11 @@ import { styled } from '@mui/material/styles';
 export const TabsContainer = styled(Stack)(() => ({
   flex: 1,
   minHeight: 375,
-  containerType: 'size',
   '& .MuiTabPanel-root': {
     flex: 1,
     padding: 0,
     '&:not([hidden])': {
+      containerType: 'size',
       display: 'flex',
       flexDirection: 'column',
     },
