022c6d51b0422e3af9a27767abec881404bb2719
Carson Full <carson_full@tsco.org>
2024-07-02 11:45:55 -0500

    Fix padding

--- src/components/Layout/ContentContainer.tsx
+++ src/components/Layout/ContentContainer.tsx
@@ -1,21 +1,8 @@
-import { makeStyles } from 'tss-react/mui';
-import { ChildrenProp } from '~/common';
+import { Stack } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
-const useStyles = makeStyles()(({ spacing }) => ({
-  root: {
-    flex: 1,
-    overflow: 'hidden',
-    padding: spacing(4, 0, 0, 4),
-    display: 'flex',
-    flexDirection: 'column',
-  },
+export const ContentContainer = styled(Stack)(({ theme }) => ({
+  flex: 1,
+  overflow: 'hidden',
+  padding: theme.spacing(4, 0, 0, 4),
 }));
-
-export const ContentContainer = (
-  props: { className?: string } & ChildrenProp
-) => {
-  const { classes, cx } = useStyles();
-  return (
-    <div className={cx(classes.root, props.className)}>{props.children}</div>
-  );
-};
