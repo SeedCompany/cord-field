c5516b5437200fbaaeb1f25d4850ab7687e426d6
Andrew Murray <andrew_murray@tsco.org>
2024-07-30 14:50:52 -0400

    align chips more to design

--- src/scenes/Projects/StatusHistory/ProjectStatusChip.tsx
+++ src/scenes/Projects/StatusHistory/ProjectStatusChip.tsx
@@ -1,10 +1,21 @@
-import { Box, Chip, Tooltip } from '@mui/material';
+import { Box, Chip, Tooltip, Typography } from '@mui/material';
 
 export const ProjectStatusChip = ({ status }: { status: string }) => {
   return (
     <Box display="flex" width={1} flex={1} justifyContent="center">
       <Tooltip title={status}>
-        <Chip label={status} sx={{ maxWidth: 154, textOverflow: 'ellipsis' }} />
+        <Chip
+          label={
+            <Typography variant="button" color="textSecondary" fontSize="80%">
+              {status}
+            </Typography>
+          }
+          sx={{
+            maxWidth: 154,
+            textOverflow: 'ellipsis',
+            borderRadius: 1,
+          }}
+        />
       </Tooltip>
     </Box>
   );
