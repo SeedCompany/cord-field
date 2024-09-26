b8900296a30984435ebeb999aa39d50722d2c32f
Carson Full <carson_full@tsco.org>
2024-09-26 14:54:10 -0500

    Behind feature flag

--- src/scenes/Projects/WorkflowEvents/WorkflowEventsIcon.tsx
+++ src/scenes/Projects/WorkflowEvents/WorkflowEventsIcon.tsx
@@ -1,11 +1,14 @@
 import { History as HistoryIcon } from '@mui/icons-material';
 import { Tooltip } from '@mui/material';
 import { IconButton, IconButtonProps } from '~/components/IconButton';
+import { Feature } from '../../../components/Feature';
 
 export const WorkflowEventsIcon = (props: IconButtonProps) => (
-  <Tooltip title="View Status History Log">
-    <IconButton {...props}>
-      <HistoryIcon />
-    </IconButton>
-  </Tooltip>
+  <Feature flag="project-workflow-history" match={true}>
+    <Tooltip title="View Status History Log">
+      <IconButton {...props}>
+        <HistoryIcon />
+      </IconButton>
+    </Tooltip>
+  </Feature>
 );
