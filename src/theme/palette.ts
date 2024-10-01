0932f67965ca04f79096dfde01b844990cbef99a
Andre Turner <andre_turner@tsco.org>
2024-10-01 12:22:48 -0500

    Change to color palette

--- src/theme/palette.ts
+++ src/theme/palette.ts
@@ -4,7 +4,7 @@ import { lighten, PaletteOptions } from '@mui/material/styles';
 import { Role } from '~/api/schema/schema.graphql';
 
 export const createPalette = ({ dark }: { dark?: boolean }) => {
-  const mainGreen = '#467f3b';
+  const mainGreen = '#1EA973';
   const roleLuminance = dark ? 32 : 84;
   const palette: PaletteOptions = {
     mode: dark ? 'dark' : 'light',
