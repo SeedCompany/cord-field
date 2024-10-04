9107881fc2041e311b46ece492fc890c34a57389
Carson Full <carson_full@tsco.org>
2024-10-04 09:37:53 -0500

    Allow report quarter to be selected

--- src/scenes/Dashboard/ProgressReportsWidget/ProgressReportsWidget.tsx
+++ src/scenes/Dashboard/ProgressReportsWidget/ProgressReportsWidget.tsx
@@ -1,3 +1,5 @@
+import { Autocomplete, TextField } from '@mui/material';
+import { DateTime } from 'luxon';
 import { useState } from 'react';
 import { CalendarDate } from '~/common';
 import { flexLayout } from '~/components/Grid';
@@ -15,8 +17,18 @@ export const ProgressReportsWidget = ({
   expanded,
   ...props
 }: WidgetProps & { expanded: boolean }) => {
-  // Set the current due date to 2 quarters ago for testing purposes
-  const [currentDue] = useState(() => CalendarDate.now().minus({ quarter: 2 }));
+  const [availableQuarters] = useState(() => {
+    const start = DateTime.local(2023, 7) as DateTime<true>;
+    const end = DateTime.now().minus({ quarter: 1 });
+    return start
+      .until(end)
+      .splitBy({ quarters: 1 })
+      .map((i) => CalendarDate.fromDateTime(i.start!) as CalendarDate<true>)
+      .reverse();
+  });
+  const [currentQuarter, setCurrentQuarter] = useState(
+    () => availableQuarters[0]!
+  );
 
   const Grid = expanded
     ? ProgressReportsExpandedGrid
@@ -24,17 +36,31 @@ export const ProgressReportsWidget = ({
   return (
     <Widget {...props}>
       <WidgetHeader
-        title={`Quarterly Reports - Q${currentDue.fiscalQuarter} FY${currentDue.fiscalYear}`}
+        title="Quarterly Reports"
         expandAction={
           <ExpanderButton
             expanded={expanded}
             to={expanded ? '/dashboard' : '/dashboard/progress-reports'}
           />
         }
-      />
+      >
+        <Autocomplete
+          disablePortal
+          options={availableQuarters}
+          getOptionLabel={(q) => `Q${q.fiscalQuarter} FY${q.fiscalYear}`}
+          value={currentQuarter}
+          onChange={(_, q) => setCurrentQuarter(q)}
+          disableClearable
+          size="small"
+          renderInput={(params) => (
+            <TextField variant="outlined" margin="none" {...params} />
+          )}
+          sx={{ width: 137 }}
+        />
+      </WidgetHeader>
       <TableWidget>
         <Grid
-          quarter={currentDue}
+          quarter={currentQuarter}
           hideFooter
           sx={[
             {
