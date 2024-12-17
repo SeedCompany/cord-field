6a02c30c2b12c1145824566746480b393b97cf1e
Carson Full <carson_full@tsco.org>
2024-12-17 17:06:48 -0600

    Bump common lib

--- src/scenes/ProgressReports/PnpValidation/PnPExtractionProblems.tsx
+++ src/scenes/ProgressReports/PnpValidation/PnPExtractionProblems.tsx
@@ -80,7 +80,7 @@ export const ProblemList = memo(function ProblemList({
 }) {
   return [...groupToMapBy(problems, (p) => p.groups[groupIndex])].map(
     ([group, problems]) => {
-      const { severity } = problems[0]!;
+      const { severity } = problems[0];
       if (group && (groupIndex === 1 || problems.length > 1)) {
         const currentNode =
           groupIndex === 1 ? (
