20e976b8ef8694eacb3da6079145c1caea0b6e6e
Rob Donigian <rob_donigian@tsco.org>
2024-12-20 15:43:09 -0500

    Remove language milestone reached grid filter

--- src/components/EngagementDataGrid/EngagementColumns.tsx
+++ src/components/EngagementDataGrid/EngagementColumns.tsx
@@ -98,13 +98,11 @@ export const EngagementColumns: Array<GridColDef<Engagement>> = [
     field: 'milestoneReached',
     ...enumColumn(LanguageMilestoneList, LanguageMilestoneLabels),
     width: 130,
-    valueGetter: (_, row) => {
-      if (row.__typename !== 'LanguageEngagement') {
-        return null;
-      }
-
-      return row.milestoneReached.value;
-    },
+    valueGetter: (_, row) =>
+      row.__typename === 'LanguageEngagement'
+        ? row.milestoneReached.value
+        : null,
+    filterable: false,
   },
   {
     headerName: 'Type',
