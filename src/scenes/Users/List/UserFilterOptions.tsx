449d8f2d52e50ccd8e7026dae4342879d36fed1a
Rob Donigian <rob_donigian@tsco.org>
2025-02-19 14:11:32 -0500

    remove unnecessary sort and filter files

--- src/scenes/Users/List/UserFilterOptions.tsx
+++
@@ -1,5 +0,0 @@
-import { EnumParam, makeQueryHandler, withDefault } from '../../../hooks';
-
-export const useUserFilters = makeQueryHandler({
-  tab: withDefault(EnumParam(['all', 'pinned']), 'all'),
-});
