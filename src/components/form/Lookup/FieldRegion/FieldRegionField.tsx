d29b9a5de6fd7fbb1e3277154740b319fcc8ff5a
Rob Donigian <rob_donigian@tsco.org>
2025-02-21 11:01:33 -0500

    Create FieldRegion form

--- src/components/form/Lookup/FieldRegion/FieldRegionField.tsx
+++ src/components/form/Lookup/FieldRegion/FieldRegionField.tsx
@@ -1,10 +1,21 @@
-import { DisplayFieldRegionFragment as FieldRegionLookupItem } from '~/common';
+import { DisplayFieldRegionFragment as FieldRegion } from '~/common';
+import { CreateFieldRegion, FieldRegionFormValues } from '../../../FieldRegion';
 import { LookupField } from '../LookupField';
 import { FieldRegionLookupDocument } from './FieldRegionLookup.graphql';
 
-export const FieldRegionField = LookupField.createFor<FieldRegionLookupItem>({
+export const FieldRegionField = LookupField.createFor<
+  FieldRegion,
+  FieldRegionFormValues
+>({
   resource: 'FieldRegion',
   lookupDocument: FieldRegionLookupDocument,
   label: 'Field Region',
   placeholder: 'Search for a field region by name',
+  CreateDialogForm: CreateFieldRegion,
+  // @ts-expect-error don't need to pass through entire initialValues
+  getInitialValues: (val) => ({
+    fieldRegion: {
+      name: val,
+    },
+  }),
 });
