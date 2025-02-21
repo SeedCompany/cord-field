8ea5c7cc849f44a89d144cad17a7754af9a67de4
Rob Donigian <rob_donigian@tsco.org>
2025-02-21 11:01:49 -0500

    Create FieldZone form

--- src/components/form/Lookup/FieldZone/FieldZoneField.tsx
+++ src/components/form/Lookup/FieldZone/FieldZoneField.tsx
@@ -1,10 +1,24 @@
-import { DisplayFieldZoneFragment as FieldZoneLookupItem } from '~/common';
+import { DisplayFieldZoneFragment as FieldZone } from '~/common';
+import {
+  CreateFieldZone,
+  FieldZoneFormValues,
+} from '../../../../components/FieldZone';
 import { LookupField } from '../LookupField';
 import { FieldZoneLookupDocument } from './FieldZoneLookup.graphql';
 
-export const FieldZoneField = LookupField.createFor<FieldZoneLookupItem>({
+export const FieldZoneField = LookupField.createFor<
+  FieldZone,
+  FieldZoneFormValues
+>({
   resource: 'FieldZone',
   lookupDocument: FieldZoneLookupDocument,
   label: 'Field Zone',
   placeholder: 'Search for a field zone by name',
+  CreateDialogForm: CreateFieldZone,
+  // @ts-expect-error don't need to pass through entire initialValues
+  getInitialValues: (val) => ({
+    fieldZone: {
+      name: val,
+    },
+  }),
 });
