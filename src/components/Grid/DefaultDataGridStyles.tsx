SeedCompany/cord-field

commit 0ae8336731f9b8cc635b0ddce523cf4ef628bef2

Rename grid filter "Operator" -> "Condition"

src/components/Grid/DefaultDataGridStyles.tsx
@@ -1,10 +1,13 @@
 import { Box, FormControl, TextFieldProps } from '@mui/material';
-import type {
+import {
   DataGridProProps as DataGridProps,
+  GRID_DEFAULT_LOCALE_TEXT,
   GridColDef,
 } from '@mui/x-data-grid-pro';
 import { mapEntries } from '@seedcompany/common';
 import { Sx } from '../../common';
+
+GRID_DEFAULT_LOCALE_TEXT.filterPanelOperator = 'Condition';
 
 const scrollIntoView: DataGridProps['onMenuOpen'] = ({ target }) => {
   target?.closest('.MuiDataGrid-root')?.scrollIntoView({ block: 'center' });

commit 6c376c4b6b60984092febf6425996bf848473c29

Fix regression in hiding the filter icon on column headers
Regressed with 0ae8336731f9b8cc635b0ddce523cf4ef628bef2
as it changed the CSS selector

src/components/Grid/DefaultDataGridStyles.tsx
@@ -137,12 +137,12 @@ export const flexLayout = {
   },
 } satisfies Sx;
 
-// Hide filter operator button - useful when there is only one operator
+// Hide the filter operator button - not useful when there is only one operator
 export const noHeaderFilterButtons = {
   '.MuiDataGrid-headerFilterRow .MuiDataGrid-columnHeader': {
     [[
       // Hide the operator button
-      'button[title="Operator"]',
+      `button[title="${GRID_DEFAULT_LOCALE_TEXT.filterPanelOperator}"]`,
       // Hide the clear icon button for select inputs
       // It seems cluttered, and it is only a couple clicks to clear it.
       '.MuiFormControl-root:has(.MuiSelect-select) + button:has([data-testid="ClearIcon"])',