bbfe53949f41aa45770c6985451bab3e5d61502a
Carson Full <carson_full@tsco.org>
2025-07-02 10:50:41 -0500

    Inline FieldGroup & Remove unneeded Stack from form

--- src/components/FieldZone/FieldZoneForm/FieldZoneForm.tsx
+++ src/components/FieldZone/FieldZoneForm/FieldZoneForm.tsx
@@ -1,9 +1,7 @@
-import { Stack } from '@mui/material';
 import { Merge } from 'type-fest';
 import { CreateFieldZone, UpdateFieldZone } from '~/api/schema.graphql';
 import { DialogForm, DialogFormProps } from '../../Dialog/DialogForm';
 import { SecuredField, SubmitError, TextField } from '../../form';
-import { FieldGroup } from '../../form/FieldGroup';
 import { UserField } from '../../form/Lookup/User/UserField';
 import { UserLookupItemFragment } from '../../form/Lookup/User/UserLookup.graphql';
 import { FieldZoneFormFragment } from './FieldZoneForm.graphql';
@@ -30,24 +28,20 @@ export const FieldZoneForm = <Mutation extends FieldZoneMutation, R>({
   fieldZone,
   ...rest
 }: FieldZoneFormProps<Mutation, R>) => (
-  <DialogForm {...rest}>
+  <DialogForm {...rest} fieldsPrefix="fieldZone">
     <SubmitError />
-    <FieldGroup prefix="fieldZone">
-      <Stack>
-        <SecuredField obj={fieldZone} name="name">
-          {(props) => (
-            <TextField
-              {...props}
-              label="Field Zone Name"
-              placeholder="Enter Field Zone Name"
-              required
-            />
-          )}
-        </SecuredField>
-        <SecuredField obj={fieldZone} name="directorId">
-          {(props) => <UserField {...props} label="Director" required />}
-        </SecuredField>
-      </Stack>
-    </FieldGroup>
+    <SecuredField obj={fieldZone} name="name">
+      {(props) => (
+        <TextField
+          {...props}
+          label="Field Zone Name"
+          placeholder="Enter Field Zone Name"
+          required
+        />
+      )}
+    </SecuredField>
+    <SecuredField obj={fieldZone} name="directorId">
+      {(props) => <UserField {...props} label="Director" required />}
+    </SecuredField>
   </DialogForm>
 );
