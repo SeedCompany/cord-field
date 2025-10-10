afdbd0a1b8cc2606bf1ce3f6c05e8ad9fdcbf798
Carson Full <carson_full@tsco.org>
2025-10-10 14:04:35 -0500

    Polish

--- src/scenes/Engagement/LanguageEngagement/Milestone/Milestone.tsx
+++ src/scenes/Engagement/LanguageEngagement/Milestone/Milestone.tsx
@@ -1,6 +1,10 @@
 import { useMutation } from '@apollo/client';
 import { Check, CheckCircle, Clear, Edit } from '@mui/icons-material';
 import { CircularProgress, Stack, Tooltip, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { setOf } from '@seedcompany/common';
+import { Decorator } from 'final-form';
+import { difference } from 'lodash';
 import { useMemo, useState } from 'react';
 import { Form } from 'react-final-form';
 import {
@@ -8,6 +12,7 @@ import {
   LanguageMilestoneLabels,
   LanguageMilestoneList,
 } from '~/api/schema.graphql';
+import { labelFrom } from '~/common';
 import { SelectField } from '~/components/form';
 import { TriStateBooleanField } from '~/components/form/TriStateBooleanField';
 import { IconButton } from '../../../../components/IconButton';
@@ -22,6 +27,31 @@ interface FormShape {
   milestonePlanned: LanguageMilestone;
   milestoneReached: boolean | null;
 }
+
+const actualMilestones = setOf<LanguageMilestone | undefined>(
+  difference(LanguageMilestoneList, ['None', 'Unknown'])
+);
+
+const clearReached: Decorator<FormShape> = (form) => {
+  let prevValues: Partial<FormShape> | undefined;
+  return form.subscribe(
+    ({ values, initialValues }) => {
+      if (prevValues === undefined || prevValues !== initialValues) {
+        prevValues = initialValues;
+      }
+      if (
+        actualMilestones.has(prevValues.milestonePlanned) &&
+        !actualMilestones.has(values.milestonePlanned)
+      ) {
+        form.change('milestoneReached', null);
+      }
+      prevValues = values;
+    },
+    { values: true, initialValues: true }
+  );
+};
+
+const decorators = [clearReached];
 
 export const LanguageEngagementMilestone = ({ engagement }: Props) => {
   const { milestonePlanned, milestoneReached } = engagement;
@@ -45,6 +75,7 @@ export const LanguageEngagementMilestone = ({ engagement }: Props) => {
 
   return (
     <Form<FormShape>
+      decorators={decorators}
       initialValues={initialValues}
       onSubmit={async (values, form) => {
         if (form.getState().dirty) {
@@ -63,104 +94,136 @@ export const LanguageEngagementMilestone = ({ engagement }: Props) => {
 
         setIsEditing(false);
       }}
-      subscription={{ submitting: true }}
+      subscription={{ submitting: true, values: true }}
     >
-      {({ handleSubmit, form, submitting }) => (
+      {({ handleSubmit, form, submitting, values }) => (
         <Stack
-          spacing={3}
-          sx={{ mb: 4 }}
+          spacing={1}
           component={isEditing ? 'form' : 'div'}
           onSubmit={handleSubmit}
         >
-          {/* Milestone Planned Section */}
-          {milestonePlanned.canRead && (
-            <Stack spacing={1}>
+          {/* Header */}
+          <Stack direction="row" alignItems="center" spacing={1}>
+            <Typography variant="h3">Milestone Planned</Typography>
+            {!isEditing ? (
+              milestonePlanned.canEdit || milestoneReached.canEdit ? (
+                <Tooltip title="Edit">
+                  <IconButton
+                    onClick={() => setIsEditing(true)}
+                    aria-label="edit milestone"
+                  >
+                    <Edit />
+                  </IconButton>
+                </Tooltip>
+              ) : null
+            ) : (
+              <>
+                <Tooltip title="Save">
+                  <IconButton
+                    color="success"
+                    disabled={submitting}
+                    type="submit"
+                  >
+                    {submitting ? <CircularProgress size={24} /> : <Check />}
+                  </IconButton>
+                </Tooltip>
+                <Tooltip title="Cancel">
+                  <IconButton
+                    color="error"
+                    disabled={submitting}
+                    onClick={() => {
+                      setIsEditing(false);
+                      form.reset();
+                    }}
+                  >
+                    <Clear />
+                  </IconButton>
+                </Tooltip>
+              </>
+            )}
+          </Stack>
+
+          <Typography variant="body2" color="textSecondary">
+            Completion of milestone translation goals within this MOU phase
+          </Typography>
+
+          <Stack direction="row" spacing={3} flexWrap="wrap">
+            {/* Planned Field / View Display */}
+            {isEditing && milestonePlanned.canEdit ? (
+              <SelectField
+                name="milestonePlanned"
+                label="Milestone"
+                options={LanguageMilestoneList}
+                getOptionLabel={labelFrom(LanguageMilestoneLabels)}
+                required
+                fullWidth={false}
+                sx={{ width: 175 }}
+              />
+            ) : milestonePlanned.value &&
+              milestonePlanned.value !== 'Unknown' ? (
               <Stack direction="row" alignItems="center" spacing={1}>
-                <Typography variant="h3">Milestone Planned</Typography>
-                {!isEditing ? (
-                  milestonePlanned.canEdit || milestoneReached.canEdit ? (
-                    <Tooltip title="Edit">
-                      <IconButton
-                        onClick={() => setIsEditing(true)}
-                        aria-label="edit milestone"
-                      >
-                        <Edit />
-                      </IconButton>
-                    </Tooltip>
-                  ) : null
-                ) : (
-                  <>
-                    <Tooltip title="Save">
-                      <IconButton
-                        color="success"
-                        disabled={submitting}
-                        type="submit"
-                      >
-                        {submitting ? (
-                          <CircularProgress size={24} />
-                        ) : (
-                          <Check />
-                        )}
-                      </IconButton>
-                    </Tooltip>
-                    <Tooltip title="Cancel">
-                      <IconButton
-                        color="error"
-                        disabled={submitting}
-                        onClick={() => {
-                          setIsEditing(false);
-                          form.reset();
-                        }}
-                      >
-                        <Clear />
-                      </IconButton>
-                    </Tooltip>
-                  </>
-                )}
+                {(() => {
+                  const label = (
+                    <Typography>
+                      {LanguageMilestoneLabels[milestonePlanned.value]}
+                    </Typography>
+                  );
+                  return milestoneReached.value === false ? (
+                    <Strikethrough>{label}</Strikethrough>
+                  ) : (
+                    label
+                  );
+                })()}
+                {milestoneReached.value === true ? (
+                  <Tooltip title="Milestone has been completed">
+                    <CheckCircle color="success" fontSize="small" />
+                  </Tooltip>
+                ) : milestoneReached.value === false ? (
+                  <Tooltip title="Milestone was not completed">
+                    <Clear color="error" />
+                  </Tooltip>
+                ) : null}
               </Stack>
-              <Typography variant="body2" color="textSecondary">
-                Completion of milestone translation goals within this MOU phase
+            ) : (
+              <Typography color="textSecondary">
+                Unknown{' '}
+                {milestonePlanned.canEdit &&
+                  '— click the edit pencil to declare'}
               </Typography>
-              {isEditing && milestonePlanned.canEdit ? (
-                <SelectField
-                  name="milestonePlanned"
-                  label="Milestone Planned"
-                  options={LanguageMilestoneList}
-                  getOptionLabel={(option) => LanguageMilestoneLabels[option]}
-                  defaultValue={milestonePlanned.value}
-                  required
-                  sx={{ width: 'fit-content' }}
-                />
-              ) : milestonePlanned.value &&
-                milestonePlanned.value !== 'Unknown' ? (
-                <Stack direction="row" alignItems="center" spacing={1}>
-                  <Typography>
-                    {LanguageMilestoneLabels[milestonePlanned.value]}
-                  </Typography>
-                  {milestoneReached.value === true && (
-                    <Tooltip title="Milestone has been completed in this engagement">
-                      <CheckCircle color="success" fontSize="large" />
-                    </Tooltip>
-                  )}
-                </Stack>
-              ) : (
-                <Typography>
-                  Unknown{' '}
-                  {milestonePlanned.canEdit &&
-                    '— click the edit pencil to declare'}
-                </Typography>
-              )}
-              {isEditing && milestoneReached.canEdit && (
+            )}
+
+            {/* Reached Field */}
+            {isEditing &&
+              milestoneReached.canEdit &&
+              actualMilestones.has(values.milestonePlanned) && (
                 <TriStateBooleanField
                   name="milestoneReached"
-                  label="Was the milestone reached in this engagement?"
+                  label="Was it actually reached/completed?"
+                  helperText={false}
                   sx={{ width: 'fit-content' }}
                 />
               )}
-            </Stack>
-          )}
+          </Stack>
         </Stack>
       )}
     </Form>
   );
 };
+
+const Strikethrough = styled('s')(({ theme }) => ({
+  display: 'inline-block',
+  textDecoration: 'none',
+  position: 'relative',
+  '&:after': {
+    color: theme.palette.error.main,
+    pointerEvents: 'none',
+    content: '""',
+    display: 'block',
+    width: '100%',
+    height: '60%', // manually positioned off vertical center to look better with the font
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    borderBottom: 'thin solid',
+  },
+}));
