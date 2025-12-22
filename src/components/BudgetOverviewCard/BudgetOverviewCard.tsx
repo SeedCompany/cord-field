0c2c279f6f9ba50b565acd184ac7c64ee05e9c72
Rob Donigian <rob_donigian@tsco.org>
2025-12-22 16:55:47 -0500

    conditionally display pre approved icon

--- src/components/BudgetOverviewCard/BudgetOverviewCard.tsx
+++ src/components/BudgetOverviewCard/BudgetOverviewCard.tsx
@@ -1,4 +1,6 @@
-import { AccountBalance } from '@mui/icons-material';
+import { AccountBalance, Warning } from '@mui/icons-material';
+import { Box, Skeleton, Tooltip, Typography } from '@mui/material';
+import { ProjectStatus } from '~/api/schema.graphql';
 import {
   FieldOverviewCard,
   FieldOverviewCardProps,
@@ -8,19 +10,33 @@ import { BudgetOverviewFragment } from './BudgetOverview.graphql';
 
 export interface BudgetOverviewCardProps extends FieldOverviewCardProps {
   budget?: BudgetOverviewFragment | null;
+  status?: ProjectStatus;
 }
 
 export const BudgetOverviewCard = ({
   className,
   budget,
   loading,
+  status,
 }: BudgetOverviewCardProps) => {
   const formatCurrency = useCurrencyFormatter();
 
   return (
     <FieldOverviewCard
       className={className}
-      title="Field Budget"
+      title={
+        <Box display="flex" alignItems="center" gap={1}>
+          <Typography variant="h4">
+            {loading ? <Skeleton width="80%" /> : 'Field Budget'}
+          </Typography>
+          {budget?.recordRollup.preApprovedExceeded &&
+            status === 'InDevelopment' && (
+              <Tooltip title="Pre-approved Item Exceeded">
+                <Warning color="error" />
+              </Tooltip>
+            )}
+        </Box>
+      }
       viewLabel="Budget Details"
       loading={loading}
       data={{
