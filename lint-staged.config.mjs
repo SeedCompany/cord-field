d84be93311a8578abb4e3d37543975755aedac08
brentkulwicki <brentkulwicki@gmail.com>
2026-04-07 16:58:56 -0400

    fixing the non-blocking nature of the setup

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,11 +2,8 @@
 /** @type {import('lint-staged').Configuration} */
 export default {
   '**/*.{js,mjs,ts,tsx}': (files) => {
-    // auto fix & format
-    const lint = `LINT_STAGED=1 yarn eslint --fix --report-unused-disable-directives-severity=off ${files.join(' ')}`;
-    // but don't block the commit
-    const lintSafe = `sh -c '${lint} || true'`;
+    const lint = `yarn eslint --fix --max-warnings 0 --report-unused-disable-directives-severity=off ${files.join(' ')}`;
     const prettier = `yarn prettier --write ${files.join(' ')}`;
-    return [prettier, lintSafe];
+    return [prettier, lint];
   },
 };
